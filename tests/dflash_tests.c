23dba52bb69f7a0e94fd97a2a41b884bf558c873
Javier Gonzalez <javier@javigon.com>
2015-11-17 13:44:24 +0100

    test: add append - read tests accross pages

--- tests/dflash_tests.c
+++ tests/dflash_tests.c
@@ -21,6 +21,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <liblightnvm.h>
 #include <unistd.h>
 
@@ -92,7 +93,6 @@ static void create_file(CuTest *ct)
 static void file_close_ungrac(CuTest *ct)
 {
 	int tgt_id, fd;
-	int i;
 
 	/* Not close fd; delete file -> Blocks are returned to MM */
 	create_tgt(ct);
@@ -126,9 +126,20 @@ static void file_close_ungrac(CuTest *ct)
 	remove_tgt(ct);
 }
 
+static void init_data_test(char *buf, int len)
+{
+	int i;
+	char payload = 'a';
+
+	for (i = 0; i < len; i++) {
+		buf[i] = (payload + i) % 28;
+	}
+}
+
 /*
  * Append and read back from file.
- *	- payload < PAGE_SIZE
+ *	- Append: payload < PAGE_SIZE
+ *	- Read: All
  *	- open - append - close - open - read - close
  */
 static void file_ar1(CuTest *ct)
@@ -137,7 +148,6 @@ static void file_ar1(CuTest *ct)
 	char test[100] = "Hello World\n";
 	char test2[100];
 	int tgt_id, fd;
-	int i;
 
 	create_tgt(ct);
 	tgt_id = nvm_target_open("test1", 0);
@@ -169,6 +179,73 @@ static void file_ar1(CuTest *ct)
 	remove_tgt(ct);
 }
 
+/*
+ * Append and read back from file.
+ *	- Append: PAGE_SIZE < payload < BLOCK_SIZE
+ *	- Read: All, 1000 byte chunks, 1 byte chunks
+ *	- open - append - close - open - read - close
+ */
+static void file_ar2(CuTest *ct)
+{
+	size_t written, read;
+	char test[5000];
+	char test2[5000];
+	int tgt_id, fd;
+	int i;
+
+	init_data_test(test, 5000);
+
+	create_tgt(ct);
+	tgt_id = nvm_target_open("test1", 0);
+	CuAssertTrue(ct, tgt_id > 0);
+
+	file_id = nvm_file_create(tgt_id, 0, 0);
+	CuAssertTrue(ct, file_id > 0);
+
+	fd = nvm_file_open(file_id, 0);
+	CuAssertTrue(ct, fd > 0);
+
+	written = nvm_file_append(fd, test, 5000);
+	CuAssertIntEquals(ct, 5000, written);
+
+	nvm_file_close(fd, 0);
+
+	fd = nvm_file_open(file_id, 0);
+	CuAssertTrue(ct, fd > 0);
+
+	read = nvm_file_read(fd, test2, written, 0, 0);
+	CuAssertIntEquals(ct, written, read);
+
+	CuAssertByteArrayEquals(ct, test, test2, 5000, 5000);
+
+	/* read in chunks */
+	memset(test2, 0, 5000);
+
+	for (i = 0; i < 5000; i++) {
+		read = nvm_file_read(fd, test2 + i, 1, i, 0);
+		CuAssertIntEquals(ct, 1, read);
+	}
+
+	CuAssertByteArrayEquals(ct, test, test2, 5000, 5000);
+
+	/* read in chunks */
+	memset(test2, 0, 5000);
+
+	for (i = 0; i < 5; i++) {
+		int offset = i * 1000;
+		read = nvm_file_read(fd, test2 + offset, 1000, offset, 0);
+		CuAssertIntEquals(ct, 1000, read);
+	}
+
+	CuAssertByteArrayEquals(ct, test, test2, 5000, 5000);
+
+	nvm_file_close(fd, 0);
+	nvm_file_delete(file_id, 0);
+	nvm_target_close(tgt_id);
+
+	remove_tgt(ct);
+}
+
 static void fini_lib(CuTest *ct)
 {
 	nvm_fini();
@@ -184,6 +261,7 @@ CuSuite *dflash_GetSuite()
 	SUITE_ADD_TEST(per_test_suite, create_file);
 	SUITE_ADD_TEST(per_test_suite, file_close_ungrac);
 	SUITE_ADD_TEST(per_test_suite, file_ar1);
+	SUITE_ADD_TEST(per_test_suite, file_ar2);
 	SUITE_ADD_TEST(per_test_suite, fini_lib);
 }
 
