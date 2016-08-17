7a506622e579cb7600e92c7429f8e094f33975d4
Simon A. F. Lund <slund@cnexlabs.com>
2016-08-17 14:27:45 +0200

    tests: Added tests of nvm_vblock_[write|read]

--- tests/test_vblock.c
+++ tests/test_vblock.c
@@ -327,6 +327,82 @@ clean:
 	CU_ASSERT(0==ret);
 }
 
+void __test_VBLOCK_WRITE_READ_N(int iterations)
+{
+	NVM_DEV dev;
+	NVM_FPAGE fpage;
+	uint32_t sec_size, pln_pg_size;
+
+	NVM_TGT tgt;
+	NVM_VBLOCK vblock;
+	int i, ret;
+						/* Create target */
+	ret = nvm_mgmt_tgt_create(nvm_tgt_name, nvm_tgt_type, nvm_dev_name, 0, 0);
+	CU_ASSERT(0==ret);
+
+	dev = nvm_dev_open(nvm_dev_name);		/* Open device */
+	CU_ASSERT_PTR_NOT_NULL(dev);
+
+	fpage = nvm_dev_get_fpage(dev);		/* Get geometry */
+	sec_size = nvm_fpage_get_sec_size(fpage);
+	pln_pg_size = nvm_fpage_get_pln_pg_size(fpage);
+
+	tgt = nvm_tgt_open(nvm_tgt_name, 0x0);	/* Open target */
+	CU_ASSERT_PTR_NOT_NULL(tgt);
+
+	for(i=0; i<iterations; ++i) {
+		int read, written;
+		char *wbuf = NULL;
+		char *rbuf = NULL;
+
+		vblock = nvm_vblock_new();		/* Allocate the vblock */
+		CU_ASSERT_PTR_NOT_NULL(vblock);
+
+		ret = nvm_vblock_gets(vblock, tgt, 0);	/* Reserve from tgt on lun 0 */
+		CU_ASSERT(0==ret)
+
+							/* Write to media */
+		ret = posix_memalign((void**)&wbuf, sec_size, pln_pg_size);
+		CU_ASSERT(0==ret)
+		strcpy(wbuf, "Hello World of NVM");
+
+		written = nvm_vblock_write(vblock, fpage, 0, 1, tgt, wbuf, 0x0);
+		CU_ASSERT(1==written)
+
+							/* Read from media */
+		ret = posix_memalign((void**)&rbuf, sec_size, pln_pg_size);
+		CU_ASSERT(0==ret);
+
+		read = nvm_vblock_read(vblock, fpage, 0, 1, tgt, rbuf, 0x0);
+		CU_ASSERT(1==read);
+
+		CU_ASSERT_STRING_EQUAL(wbuf, rbuf);
+
+		ret = nvm_vblock_put(vblock, tgt);	/* Release vblock from tgt */
+		CU_ASSERT(0==ret);
+
+		free(wbuf);
+		free(rbuf);
+
+		nvm_vblock_free(&vblock);		/* De-allocate vblock */
+	}
+
+	nvm_tgt_close(tgt);				/* Close the target */
+
+	ret = nvm_mgmt_tgt_remove(nvm_tgt_name);
+	CU_ASSERT(0==ret);
+}
+
+void test_VBLOCK_WRITE_READ_01(void)
+{
+	__test_VBLOCK_WRITE_READ_N(1);
+}
+
+void test_VBLOCK_WRITE_READ_02(void)
+{
+	__test_VBLOCK_WRITE_READ_N(1000);
+}
+
 int main(int argc, char **argv)
 {
 	if (argc > 1) {
@@ -353,6 +429,8 @@ int main(int argc, char **argv)
 	(NULL == CU_add_test(pSuite, "nvm_vblock_[get|put] 1", test_VBLOCK_GET_PUT_01)) ||
 	(NULL == CU_add_test(pSuite, "nvm_vblock_[gets|put] 1", test_VBLOCK_GETS_PUT_01)) ||
 	(NULL == CU_add_test(pSuite, "nvm_vblock_[gets|put] 2", test_VBLOCK_GETS_PUT_02)) ||
+	(NULL == CU_add_test(pSuite, "nvm_vblock_[write|read] 1", test_VBLOCK_WRITE_READ_01)) ||
+	(NULL == CU_add_test(pSuite, "nvm_vblock_[write|read] 2", test_VBLOCK_WRITE_READ_02)) ||
 	0)
 	{
 		CU_cleanup_registry();
