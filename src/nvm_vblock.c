b901675993841e303c08b2e2d3ab9c3f3b9fa74a
Simon A. F. Lund <slund@cnexlabs.com>
2016-08-29 14:45:37 +0200

    vblock: Fix to ppa-list creation in nvm_block_erase

--- src/nvm_vblock.c
+++ src/nvm_vblock.c
@@ -212,44 +212,36 @@ int nvm_vblock_erase(struct nvm_vblock *vblock)
 {
 	struct nvm_dev *dev = vblock->tgt->dev;
 	const int NPLANES = nvm_dev_get_nplanes(dev);
-	const int NPAGES = nvm_dev_get_npages(dev);
-	const int NPPAS_MAX = NPLANES * nvm_dev_get_nsectors(dev);
 
-	int pg_off;
+	struct NVM_ADDR ppas[NPLANES];
+	struct nvm_ioctl_io ctl;
+	int i, ret;
 
-	for (pg_off = 0; pg_off < NPAGES; ++pg_off) {
-		struct NVM_ADDR ppas[NPPAS_MAX];
-		struct nvm_ioctl_io ctl;
-		int i, ret;
+	for (i = 0; i < NPLANES; i++) {
+		struct NVM_ADDR ppa;
 
-		for (i = 0; i < NPPAS_MAX; i++) {
-			struct NVM_ADDR ppa;
+		ppa.ppa = vblock->ppa;
+		ppa.g.pl = i;
 
-			ppa.ppa = vblock->ppa;
-			ppa.g.pg = pg_off;
-			ppa.g.sec = i % NPLANES;
-			ppa.g.pl = i / NPLANES;
+		ppas[i] = ppa;
+	}
 
-			ppas[i] = ppa;
-		}
+	memset(&ctl, 0, sizeof(ctl));
+	ctl.opcode = 0x90;	/* MAGIC NUMBER -- NVM_OP_PERASE */
+	ctl.flags = 0x2;	/* MAGIC NUMBER -- NVM_IO_QUAD_ACCESS */
+	ctl.nppas = NPLANES;
 
-		memset(&ctl, 0, sizeof(ctl));
-		ctl.opcode = 0x90;	/* MAGIC NUMBER -- NVM_OP_PERASE */
-		ctl.flags = 0x2;	/* MAGIC NUMBER -- NVM_IO_QUAD_ACCESS */
-		ctl.nppas = NPPAS_MAX;
+	ctl.ppas = (uint64_t)ppas;
 
-		ctl.ppas = (uint64_t)ppas;
+	ret = ioctl(vblock->tgt->fd, NVM_PIO, &ctl);
+	if (ret) {
+		NVM_DEBUG("failed ret(%d)\n", ret);
+		return ret;
+	}
 
-		ret = ioctl(vblock->tgt->fd, NVM_PIO, &ctl);
-		if (ret) {
-			NVM_DEBUG("failed ret(%d)\n", ret);
-			return ret;
-		}
-
-		if (ctl.result) {
-			NVM_DEBUG("result(%u)\n", ctl.result);
-			NVM_DEBUG("status(%llu)\n", (unsigned long long)ctl.status);
-		}
+	if (ctl.result) {
+		NVM_DEBUG("result(%u)\n", ctl.result);
+		NVM_DEBUG("status(%llu)\n", (unsigned long long)ctl.status);
 	}
 
 	return 0;
