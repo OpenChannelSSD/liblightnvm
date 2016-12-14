079a3715a1e3c709ac8424a80eb185cad79d3e42
Simon A. F. Lund <slund@cnexlabs.com>
2016-12-14 15:31:59 +0100

    nvm_sblk: Added a "chunked write" CLI example

--- cli/sblk.c
+++ cli/sblk.c
@@ -58,6 +58,51 @@ int write(NVM_CLI_CMD_ARGS *args, int flags)
 	return ret < 0;
 }
 
+int chunked_write(NVM_CLI_CMD_ARGS *args, int flags)
+{
+	ssize_t ret = 0;
+	char *buf;
+	size_t buf_sz = args->sblk_geo.nchannels * args->sblk_geo.nluns \
+			* args->sblk_geo.nplanes * args->sblk_geo.nsectors \
+			* args->sblk_geo.nbytes;
+	size_t nbytes_written = 0;
+
+	printf("** nvm_sblk_write(...): sblk_tbytes(%lu), buf_sz(%lu)\n",
+	       args->sblk_geo.tbytes, buf_sz);
+	nvm_sblk_pr(args->sblk);
+	
+	nvm_timer_start();
+	buf = nvm_buf_alloc(args->sblk_geo, buf_sz);
+	if (!buf) {
+		perror("nvm_buf_alloc");
+		return errno;
+	}
+	nvm_timer_stop();
+	nvm_timer_pr("nvm_buf_alloc");
+
+	nvm_timer_start();
+	nvm_buf_fill(buf, buf_sz);
+	nvm_timer_stop();
+	nvm_timer_pr("nvm_buf_fill");
+
+	nvm_timer_start();
+	
+	while(nbytes_written < args->sblk_geo.tbytes) {
+		ret = nvm_sblk_write(args->sblk, buf, buf_sz);
+		if (ret < 0) {
+			perror("nvm_sblk_write");
+			break;
+		}
+		nbytes_written += ret;
+	}
+	nvm_timer_stop();
+	nvm_timer_pr("nvm_sblk_write");
+
+	free(buf);
+
+	return ret < 0;
+}
+
 int pad(NVM_CLI_CMD_ARGS *args, int flags)
 {
 	ssize_t ret;
@@ -114,6 +159,7 @@ static NVM_CLI_CMD cmds[] = {
 	{"write", write, NVM_CLI_ARG_SBLK, 0x0},
 	{"pad", pad, NVM_CLI_ARG_SBLK, 0x0},
 	{"read", read, NVM_CLI_ARG_SBLK, 0x0},
+	{"chunked_w", chunked_write, NVM_CLI_ARG_SBLK, 0x0},
 };
 
 static int ncmds = sizeof(cmds) / sizeof(cmds[0]);
