757eb2ea93adcf374c06abf92ae3e088fdcf1800
Simon A. F. Lund <slund@cnexlabs.com>
2017-02-16 15:52:34 +0100

    nvm_addr: Re-implemented using `nvm_be` and `nvm_spec`

--- src/nvm_addr.c
+++ src/nvm_addr.c
@@ -27,20 +27,35 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
 #include <stdio.h>
-#include <linux/lightnvm.h>
+#include <errno.h>
 #include <liblightnvm.h>
-#include <nvm.h>
+#include <nvm_be.h>
+#include <nvm_dev.h>
 #include <nvm_debug.h>
+#include <nvm_utils.h>
 
 void nvm_ret_pr(struct nvm_ret *ret)
 {
 	printf("nvm_ret { result(0x%x), status(%lu) }\n", ret->result,
 	       ret->status);
+}
+
+void nvm_addr_pr(struct nvm_addr addr)
+{
+	printf("(0x%016lx){ ch(%02d), lun(%02d), pl(%d), ",
+	       addr.ppa, addr.g.ch, addr.g.lun, addr.g.pl);
+	printf("blk(%04d), pg(%03d), sec(%d) }\n",
+	       addr.g.blk, addr.g.pg, addr.g.sec);
+}
+
+void nvm_addr_prn(struct nvm_addr *addr, unsigned int naddrs)
+{
+	printf("naddrs(%d) {\n", naddrs);
+	for (int i = 0; i < naddrs; ++i) {
+		printf(" %02d: ", i); nvm_addr_pr(addr[i]);
+	}
+	printf("}\n");
 }
 
 int nvm_addr_check(struct nvm_addr addr, const struct nvm_geo *geo)
@@ -73,12 +88,12 @@ inline uint64_t nvm_addr_gen2dev(struct nvm_dev *dev, struct nvm_addr addr)
 {
 	uint64_t d_addr = 0;
 
-	d_addr |= ((uint64_t)addr.g.ch) << dev->fmt.n.ch_ofz;
-	d_addr |= ((uint64_t)addr.g.lun) << dev->fmt.n.lun_ofz;
-	d_addr |= ((uint64_t)addr.g.pl) << dev->fmt.n.pl_ofz;
-	d_addr |= ((uint64_t)addr.g.blk) << dev->fmt.n.blk_ofz;
-	d_addr |= ((uint64_t)addr.g.pg) << dev->fmt.n.pg_ofz;
-	d_addr |= ((uint64_t)addr.g.sec) << dev->fmt.n.sec_ofz;
+	d_addr |= ((uint64_t)addr.g.ch) << dev->ppaf.n.ch_off;
+	d_addr |= ((uint64_t)addr.g.lun) << dev->ppaf.n.lun_off;
+	d_addr |= ((uint64_t)addr.g.pl) << dev->ppaf.n.pl_off;
+	d_addr |= ((uint64_t)addr.g.blk) << dev->ppaf.n.blk_off;
+	d_addr |= ((uint64_t)addr.g.pg) << dev->ppaf.n.pg_off;
+	d_addr |= ((uint64_t)addr.g.sec) << dev->ppaf.n.sec_off;
 
 	return d_addr;
 }
@@ -98,12 +113,12 @@ inline struct nvm_addr nvm_addr_dev2gen(struct nvm_dev *dev, uint64_t addr)
 	struct nvm_addr gen;
 
 	gen.ppa = 0;
-	gen.g.ch = (addr & dev->mask.n.ch) >> dev->fmt.n.ch_ofz;
-	gen.g.lun |= (addr & dev->mask.n.lun) >> dev->fmt.n.lun_ofz;
-	gen.g.pl|= (addr & dev->mask.n.pl) >> dev->fmt.n.pl_ofz;
-	gen.g.blk |= (addr & dev->mask.n.blk) >> dev->fmt.n.blk_ofz;
-	gen.g.pg |= (addr & dev->mask.n.pg) >> dev->fmt.n.pg_ofz;
-	gen.g.sec |= (addr & dev->mask.n.sec) >> dev->fmt.n.sec_ofz;
+	gen.g.ch = (addr & dev->mask.n.ch) >> dev->ppaf.n.ch_off;
+	gen.g.lun |= (addr & dev->mask.n.lun) >> dev->ppaf.n.lun_off;
+	gen.g.pl|= (addr & dev->mask.n.pl) >> dev->ppaf.n.pl_off;
+	gen.g.blk |= (addr & dev->mask.n.blk) >> dev->ppaf.n.blk_off;
+	gen.g.pg |= (addr & dev->mask.n.pg) >> dev->ppaf.n.pg_off;
+	gen.g.sec |= (addr & dev->mask.n.sec) >> dev->ppaf.n.sec_off;
 
 	return gen;
 }
@@ -118,11 +133,12 @@ struct nvm_addr nvm_addr_lba2gen(struct nvm_dev *dev, uint64_t off)
 	return nvm_addr_off2gen(dev, off << NVM_UNIVERSAL_SECT_SH);
 }
 
-static ssize_t nvm_addr_cmd(struct nvm_dev *dev, struct nvm_addr addrs[],
-			    int naddrs, void *data, void *meta, uint16_t flags,
-			    uint16_t opcode, struct nvm_ret *ret)
+static inline ssize_t nvm_addr_cmd(struct nvm_dev *dev, struct nvm_addr addrs[],
+				   int naddrs, void *data, void *meta,
+				   uint16_t flags, uint16_t opcode,
+				   struct nvm_ret *ret)
 {
-	struct nvm_user_vio ctl;
+	struct nvm_cmd cmd = {};
 	uint64_t dev_addrs[naddrs];
 	int i, err;
 
@@ -131,40 +147,39 @@ static ssize_t nvm_addr_cmd(struct nvm_dev *dev, struct nvm_addr addrs[],
 		return -1;
 	}
 
-	memset(&ctl, 0, sizeof(ctl));
-	ctl.opcode = opcode;
-	ctl.control = flags | NVM_FLAG_DEFAULT;
+	cmd.vuser.opcode = opcode;
+	cmd.vuser.control = flags | NVM_FLAG_DEFAULT;
 
-	for (i = 0; i < naddrs; ++i) {	// Setup PPAs: Convert address format
+	// Setup PPAs: Convert address format from generic to device specific
+	for (i = 0; i < naddrs; ++i)
 		dev_addrs[i] = nvm_addr_gen2dev(dev, addrs[i]);
-	}
-	ctl.nppas = naddrs - 1;		// Unnatural numbers: counting from zero
-	ctl.ppa_list = naddrs == 1 ? dev_addrs[0] : (uint64_t)dev_addrs;
 
-	ctl.addr = (uint64_t)data;	// Setup data
-	ctl.data_len = data ? dev->geo.sector_nbytes * naddrs : 0;
+	// Unnatural numbers: counting from zero
+	cmd.vuser.nppas = naddrs - 1;
+	cmd.vuser.ppa_list = naddrs == 1 ? dev_addrs[0] : (uint64_t)dev_addrs;
 
-	ctl.metadata = (uint64_t)meta;	// Setup metadata
-	ctl.metadata_len = meta ? dev->geo.meta_nbytes * naddrs : 0;
+	// Setup data
+	cmd.vuser.addr = (uint64_t)data;
+	cmd.vuser.data_len = data ? dev->geo.sector_nbytes * naddrs : 0;
 
-	err = ioctl(dev->fd, NVME_NVM_IOCTL_SUBMIT_VIO, &ctl);
+	// Setup metadata
+	cmd.vuser.metadata = (uint64_t)meta;
+	cmd.vuser.metadata_len = meta ? dev->geo.meta_nbytes * naddrs : 0;
+
+	err = dev->be->vuser(dev, &cmd, ret);
 #ifdef NVM_DEBUG_ENABLED
-	if (err || ctl.result || ctl.status) {
-		printf("opcode(0x%02x), err(%d), ctl.result(%u), ctl.status(%llu)\n",
-		       opcode, err, ctl.result, ctl.status);
+	if (err || cmd.vuser.result || cmd.vuser.status) {
+		printf("opcode(0x%02x), err(%d), result(%u), status(%llu)\n",
+		       opcode, err, cmd.vuser.result, cmd.vuser.status);
 		nvm_addr_prn(addrs, naddrs);
 	}
 #endif
-	if (ret) {
-		ret->result = ctl.result;
-		ret->status = ctl.status;
-	}
-	if (err) {	// Give up on IOCTL errors
+	if (err) {	// Give up on errors
 		errno = EIO;
 		return -1;
 	}
 
-	switch (ctl.result) {
+	switch (cmd.vuser.result) {
 	case 0x0:	// All good
 		return 0;
 	case 0x4700:	// As good as it gets..
@@ -200,53 +215,5 @@ ssize_t nvm_addr_read(struct nvm_dev *dev, struct nvm_addr addrs[], int naddrs,
 {
 	return nvm_addr_cmd(dev, addrs, naddrs, data, meta, flags,
 			    S12_OPC_READ, ret);
-}
-
-void nvm_addr_fmt_pr(struct nvm_addr_fmt *fmt)
-{
-	printf("fmt {\n");
-	printf("  ch_ofz(%02d),  ch_len(%02d), \n",
-	       fmt->n.ch_ofz, fmt->n.ch_len);
-	printf(" lun_ofz(%02d), lun_len(%02d), \n",
-	       fmt->n.lun_ofz, fmt->n.lun_len);
-	printf("  pl_ofz(%02d),  pl_len(%02d), \n",
-	       fmt->n.pl_ofz, fmt->n.pl_len);
-	printf(" blk_ofz(%02d), blk_len(%02d), \n",
-	       fmt->n.blk_ofz, fmt->n.blk_len);
-	printf("  pg_ofz(%02d),  pg_len(%02d), \n",
-	       fmt->n.pg_ofz, fmt->n.pg_len);
-	printf(" sec_ofz(%02d), sec_len(%02d), \n",
-	       fmt->n.sec_ofz, fmt->n.sec_len);
-	printf("}\n");
-}
-
-
-void nvm_addr_fmt_mask_pr(struct nvm_addr_fmt_mask *mask)
-{
-	printf("fmt-mask {\n");
-	printf("  ch("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.ch));
-	printf(" lun("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.lun));
-	printf("  pl("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.pl));
-	printf(" blk("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.blk));
-	printf("  pg("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.pg));
-	printf(" sec("NVM_I64_FMT")\n", NVM_I64_TO_STR(mask->n.sec));
-	printf("}\n");
-}
-
-void nvm_addr_pr(struct nvm_addr addr)
-{
-	printf("(0x%016lx){ ch(%02d), lun(%02d), pl(%d), ",
-	       addr.ppa, addr.g.ch, addr.g.lun, addr.g.pl);
-	printf("blk(%04d), pg(%03d), sec(%d) }\n",
-	       addr.g.blk, addr.g.pg, addr.g.sec);
-}
-
-void nvm_addr_prn(struct nvm_addr *addr, unsigned int naddrs)
-{
-	printf("naddrs(%d) {\n", naddrs);
-	for (int i = 0; i < naddrs; ++i) {
-		printf(" %02d: ", i); nvm_addr_pr(addr[i]);
-	}
-	printf("}\n");
 }
 
