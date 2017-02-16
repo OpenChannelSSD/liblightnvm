b5984c6de70b5f23bfa418f9de0fd291d7b8577c
Simon A. F. Lund <slund@cnexlabs.com>
2017-02-16 16:53:34 +0100

    nvm_be.h: Added header defining interface for device backends

---
+++ include/nvm_be.h
@@ -0,0 +1,67 @@
+/*
+ * nvm_be - internal header for library backends
+ *
+ * Copyright (C) 2015 Javier González <javier@cnexlabs.com>
+ * Copyright (C) 2015 Matias Bjørling <matias@cnexlabs.com>
+ * Copyright (C) 2016 Simon A. F. Lund <slund@cnexlabs.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __INTERNAL_NVM_BE_H
+#define __INTERNAL_NVM_BE_H
+
+/**
+ * Backend interface
+ */
+struct nvm_be {
+	/**
+	 * Open a device
+	 */
+	struct nvm_dev *(*open)(const char *, int);
+
+	/**
+	 * Close the given device
+	 */
+	void (*close)(struct nvm_dev *);
+
+	/**
+	 * Send an user command to device
+	 */
+	int (*user)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+
+	/**
+	 * Send an admin command to device
+	 */
+	int (*admin)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+
+	/**
+	 * Send a vector user command to device
+	 */
+	int (*vuser)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+
+	/**
+	 * Send a vector admin command to device
+	 */
+	int (*vadmin)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+};
+
+#endif /* __INTERNAL_NVM_BE_H */
