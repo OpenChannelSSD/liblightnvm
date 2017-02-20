793751b8d7218feeda72a3b1baac32a188806f81
Simon A. F. Lund <slund@cnexlabs.com>
2017-02-21 00:04:46 +0100

    cli: nvm_bbt: Changed sub-command interface

--- cli/bbt.c
+++ cli/bbt.c
@@ -8,10 +8,10 @@
 #include <liblightnvm.h>
 #include "nvm_cli.h"
 
-int get(NVM_CLI_CMD_ARGS *args, int flags)
+int get(NVM_CLI_CMD_ARGS *args)
 {
 	const struct nvm_bbt* bbt;
-	struct nvm_ret ret = {0,0};
+	struct nvm_ret ret = {0};
 
 	printf("** nvm_bbt_get(...):\n");
 
@@ -27,10 +27,10 @@ int get(NVM_CLI_CMD_ARGS *args, int flags)
 	return 0;
 }
 
-int set(NVM_CLI_CMD_ARGS *args, int flags)
+int _set(NVM_CLI_CMD_ARGS *args, enum nvm_bbt_state state)
 {
 	struct nvm_bbt* bbt;
-	struct nvm_ret ret = {0,0};
+	struct nvm_ret ret = {0};
 	int nupdates;
 
 	printf("** nvm_bbt_set(...):\n");
@@ -45,8 +45,8 @@ int set(NVM_CLI_CMD_ARGS *args, int flags)
 
 	printf("Current state:\n"); nvm_bbt_pr(bbt);
 
-	for (int i = 0; i < bbt->nblks; ++i) {
-		bbt->blks[i] = flags;
+	for (uint64_t i = 0; i < bbt->nblks; ++i) {
+		bbt->blks[i] = state;
 	}
 
 	printf("New state:\n"); nvm_bbt_pr(bbt);
@@ -64,16 +64,41 @@ int set(NVM_CLI_CMD_ARGS *args, int flags)
 	return 0;
 }
 
-int mark(NVM_CLI_CMD_ARGS *args, int flags)
+int set_f(NVM_CLI_CMD_ARGS *args)
+{
+	return _set(args, NVM_BBT_FREE);
+}
+
+int set_b(NVM_CLI_CMD_ARGS *args)
+{
+	return _set(args, NVM_BBT_BAD);
+}
+
+int set_g(NVM_CLI_CMD_ARGS *args)
+{
+	return _set(args, NVM_BBT_GBAD);
+}
+
+int set_d(NVM_CLI_CMD_ARGS *args)
+{
+	return _set(args, NVM_BBT_DMRK);
+}
+
+int set_h(NVM_CLI_CMD_ARGS *args)
+{
+	return _set(args, NVM_BBT_HMRK);
+}
+
+int _mark(NVM_CLI_CMD_ARGS *args, enum nvm_bbt_state state)
 {
 	ssize_t err = 0;
-	struct nvm_ret ret = {0,0};
+	struct nvm_ret ret = {0};
 
 	printf("** nvm_bbt_mark(...):\n");
 	for (int i = 0; i < args->naddrs; ++i)
 		nvm_addr_pr(args->addrs[i]);
 
-	err = nvm_bbt_mark(args->dev, args->addrs, args->naddrs, flags, &ret);
+	err = nvm_bbt_mark(args->dev, args->addrs, args->naddrs, state, &ret);
 	if (err) {
 		perror("nvm_bbt_mark");
 		nvm_ret_pr(&ret);
@@ -82,21 +107,46 @@ int mark(NVM_CLI_CMD_ARGS *args, int flags)
 	return err ? 1 : 0;
 }
 
+int mark_f(NVM_CLI_CMD_ARGS *args)
+{
+	return _mark(args, NVM_BBT_FREE);
+}
+
+int mark_b(NVM_CLI_CMD_ARGS *args)
+{
+	return _mark(args, NVM_BBT_BAD);
+}
+
+int mark_g(NVM_CLI_CMD_ARGS *args)
+{
+	return _mark(args, NVM_BBT_GBAD);
+}
+
+int mark_h(NVM_CLI_CMD_ARGS *args)
+{
+	return _mark(args, NVM_BBT_HMRK);
+}
+
+int mark_d(NVM_CLI_CMD_ARGS *args)
+{
+	return _mark(args, NVM_BBT_DMRK);
+}
+
 //
 // Remaining code is CLI boiler-plate
 //
 static NVM_CLI_CMD cmds[] = {
-	{"get", get, NVM_CLI_ARG_CH_LUN, 0x0},
-	{"set_f", set, NVM_CLI_ARG_CH_LUN, NVM_BBT_FREE},
-	{"set_b", set, NVM_CLI_ARG_CH_LUN, NVM_BBT_BAD},
-	{"set_g", set, NVM_CLI_ARG_CH_LUN, NVM_BBT_GBAD},
-	{"set_d", set, NVM_CLI_ARG_CH_LUN, NVM_BBT_DMRK},
-	{"set_h", set, NVM_CLI_ARG_CH_LUN, NVM_BBT_HMRK},
-	{"mark_f", mark, NVM_CLI_ARG_ADDRLIST, NVM_BBT_FREE},
-	{"mark_b", mark, NVM_CLI_ARG_ADDRLIST, NVM_BBT_BAD},
-	{"mark_g", mark, NVM_CLI_ARG_ADDRLIST, NVM_BBT_GBAD},
-	{"mark_d", mark, NVM_CLI_ARG_ADDRLIST, NVM_BBT_DMRK},
-	{"mark_h", mark, NVM_CLI_ARG_ADDRLIST, NVM_BBT_HMRK},
+	{"get",		get,	NVM_CLI_ARG_CH_LUN,	NULL},
+	{"set_f",	set_f,	NVM_CLI_ARG_CH_LUN,	NULL},
+	{"set_b",	set_b,	NVM_CLI_ARG_CH_LUN,	NULL},
+	{"set_g",	set_g,	NVM_CLI_ARG_CH_LUN,	NULL},
+	{"set_d",	set_d,	NVM_CLI_ARG_CH_LUN,	NULL},
+	{"set_h",	set_h,	NVM_CLI_ARG_CH_LUN,	NULL},
+	{"mark_f",	mark_f,	NVM_CLI_ARG_ADDRLIST,	NULL},
+	{"mark_b",	mark_b,	NVM_CLI_ARG_ADDRLIST,	NULL},
+	{"mark_g",	mark_g,	NVM_CLI_ARG_ADDRLIST,	NULL},
+	{"mark_d",	mark_d,	NVM_CLI_ARG_ADDRLIST,	NULL},
+	{"mark_h",	mark_h,	NVM_CLI_ARG_ADDRLIST,	NULL},
 };
 
 static int ncmds = sizeof(cmds) / sizeof(cmds[0]);
@@ -108,7 +158,7 @@ int main(int argc, char **argv)
 
 	cmd = nvm_cli_setup(argc, argv, cmds, ncmds);
 	if (cmd) {
-		ret = cmd->func(&cmd->args, cmd->flags);
+		ret = cmd->func(cmd->args);
 	} else {
 		nvm_cli_usage(argv[0], "NVM bad-block-table (nvm_bbt_*)", cmds,
 			      ncmds);
