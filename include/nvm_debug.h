e7ab29ae6122e3e7b42bc0b87f2d5af85547e58c
Simon A. F. Lund <slund@cnexlabs.com>
2017-02-20 21:05:16 +0100

    nvm_debug.h: Changed to standards compliant NVM_DEBUG

--- include/nvm_debug.h
+++ include/nvm_debug.h
@@ -30,24 +30,28 @@
 #ifndef __INTERNAL_NVM_DEBUG_H
 #define __INTERNAL_NVM_DEBUG_H
 
+#include <string.h>
 #include <stdio.h>
 
-#define NVM_ASSERT(c, x, ...) if(!(c)){printf("%s:%s - %d %s" x "\n", \
-		__FILE__, __FUNCTION__, __LINE__, strerror(errno), \
-		##__VA_ARGS__); fflush(stdout); exit(EXIT_FAILURE); }
-
-#define NVM_ERROR(x, ...) printf("%s:%s - %d %s" x "\n", __FILE__, \
-		__FUNCTION__, __LINE__, strerror(errno), ##__VA_ARGS__); \
-		fflush(stdout);
-
-#define NVM_FATAL(x, ...) printf("%s:%s - %d %s" x "\n", __FILE__, \
-		__FUNCTION__, __LINE__, strerror(errno), ##__VA_ARGS__); \
-		fflush(stdout);exit(EXIT_FAILURE)
-
 #ifdef NVM_DEBUG_ENABLED
-	#define NVM_DEBUG(x, ...) printf("%s:%s-%d: " x "\n", __FILE__, \
-		__FUNCTION__, __LINE__, ##__VA_ARGS__);fflush(stdout);
+
+#define NVM_DEBUG(...) printf("%s:%s-%d: " FIRST(__VA_ARGS__) "\n" , \
+	__FILE__, __func__, __LINE__ REST(__VA_ARGS__)); fflush(stdout);
+
+#define FIRST(...) FIRST_HELPER(__VA_ARGS__, throwaway)
+#define FIRST_HELPER(first, ...) first
+
+#define REST(...) REST_HELPER(NUM(__VA_ARGS__), __VA_ARGS__)
+#define REST_HELPER(qty, ...) REST_HELPER2(qty, __VA_ARGS__)
+#define REST_HELPER2(qty, ...) REST_HELPER_##qty(__VA_ARGS__)
+#define REST_HELPER_ONE(first)
+#define REST_HELPER_TWOORMORE(first, ...) , __VA_ARGS__
+#define NUM(...) \
+	SELECT_10TH(__VA_ARGS__, TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE,\
+		TWOORMORE, TWOORMORE, TWOORMORE, TWOORMORE, ONE, throwaway)
+#define SELECT_10TH(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, ...) a10
+
 #else
-	#define NVM_DEBUG(x, ...)
+	#define NVM_DEBUG(...)
 #endif
 #endif /* __INTERNAL_NVM_DEBUG_H */
