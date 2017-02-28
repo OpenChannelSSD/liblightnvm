34648f20a621f6552af1deca6462e9bb8a832e2c
Simon A. F. Lund <slund@cnexlabs.com>
2017-02-28 17:08:19 +0100

    public: Added header for third-party reuse of nvm_cli_*

--- include/liblightnvm_cli.h
+++ include/liblightnvm_cli.h
@@ -1,3 +1,41 @@
+/*
+ * nvm_cli - Command-line interface (CLI) utitities for liblightnvm
+ *
+ * Copyright (C) 2015-2017 Javier González <javier@cnexlabs.com>
+ * Copyright (C) 2015-2017 Matias Bjørling <matias@cnexlabs.com>
+ * Copyright (C) 2015-2017 Simon A. F. Lund <slund@cnexlabs.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/**
+ * @file liblightnvm.h
+ */
+#ifndef __LIBLIGHTNVM_CLI_H
+#define __LIBLIGHTNVM_CLI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <liblightnvm.h>
 
 #define NVM_CLI_CMD_LEN 50
@@ -114,3 +152,9 @@ int nvm_cli_pmode(struct nvm_dev *dev);
  * errno set to indicate the error.
  */
 int nvm_cli_meta_mode(struct nvm_dev *dev);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LIBLIGHTNVM_CLI.H */
