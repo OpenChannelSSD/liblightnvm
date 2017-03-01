7d06c315b83f5797e4ebf5c8e8c9c397113d3c51
Simon A. F. Lund <slund@cnexlabs.com>
2017-03-01 12:16:42 +0100

    tests: nvm_dev: Added tests of nvm_dev_openf for specific backends

--- tests/test_dev.c
+++ tests/test_dev.c
@@ -12,11 +12,9 @@ void test_DEV_OPEN_CLOSE(void)
 	struct nvm_dev *dev;
 
 	dev = nvm_dev_open(nvm_dev_path);
-	CU_ASSERT_PTR_NOT_NULL(dev);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(dev);
 
 	nvm_dev_close(dev);
-        CU_ASSERT_PTR_NOT_NULL(dev);
-	// still dangling...
 }
 
 void test_DEV_OPEN_CLOSE_N(void)
@@ -27,13 +25,39 @@ void test_DEV_OPEN_CLOSE_N(void)
 	for (int i = 0; i < n; ++i) {
 		dev[i] = nvm_dev_open(nvm_dev_path);
 		CU_ASSERT_PTR_NOT_NULL(dev[i]);
+		if (!dev[i])
+			break;
 	}
 
 	for (int i = 0; i < n; ++i) {
+		if (!dev[i])
+			break;
 		nvm_dev_close(dev[i]);
-		CU_ASSERT_PTR_NOT_NULL(dev[i]);
-		// still dangling...
 	}
+}
+
+void test_DEV_OPENF_SYSFS_CLOSE(void)
+{
+	struct nvm_dev *dev;
+
+	dev = nvm_dev_openf(nvm_dev_path, NVM_BE_SYSFS);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(dev);
+
+	CU_ASSERT_EQUAL(nvm_dev_get_be_id(dev), NVM_BE_SYSFS);
+
+	nvm_dev_close(dev);
+}
+
+void test_DEV_OPENF_IOCTL_CLOSE(void)
+{
+	struct nvm_dev *dev;
+
+	dev = nvm_dev_openf(nvm_dev_path, NVM_BE_IOCTL);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(dev);
+
+	CU_ASSERT_EQUAL(nvm_dev_get_be_id(dev), NVM_BE_IOCTL);
+
+	nvm_dev_close(dev);
 }
 
 int main(int argc, char **argv)
@@ -63,6 +87,8 @@ int main(int argc, char **argv)
 	if (
 	(NULL == CU_add_test(pSuite, "nvm_dev_[open|close]", test_DEV_OPEN_CLOSE)) ||
 	(NULL == CU_add_test(pSuite, "nvm_dev_[open|close] n", test_DEV_OPEN_CLOSE_N)) ||
+	(NULL == CU_add_test(pSuite, "nvm_dev_[openf(sysfs)|close] ", test_DEV_OPENF_SYSFS_CLOSE)) ||
+	(NULL == CU_add_test(pSuite, "nvm_dev_[openf(ioctl)|close] ", test_DEV_OPENF_IOCTL_CLOSE)) ||
 	0
 	)
 	{
