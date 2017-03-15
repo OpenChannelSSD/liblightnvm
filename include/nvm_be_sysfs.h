393b8cac97a130fcde9bdc93dcf587d46434d320
Simon A. F. Lund <slund@cnexlabs.com>
2017-03-16 00:52:19 +0100

    nvm_be: Added internal header for nvm_be_sysfs

---
+++ include/nvm_be_sysfs.h
@@ -0,0 +1,48 @@
+/*
+ * nvm_be_sysfs - internal header for sysfs backend
+ *
+ * Copyright (C) 2015-2017 Javier Gonzáles <javier@cnexlabs.com>
+ * Copyright (C) 2015-2017 Matias Bjørling <matias@cnexlabs.com>
+ * Copyright (C) 2015-2017 Simon A. F. Lund <slund@cnexlabs.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __INTERNAL_NVM_BE_SYSFS_H
+#define __INTERNAL_NVM_BE_SYSFS_H
+
+struct nvm_dev *nvm_be_sysfs_open(const char *dev_path);
+
+void nvm_be_sysfs_close(struct nvm_dev *dev);
+
+int nvm_be_sysfs_user(struct nvm_dev *dev, struct nvm_cmd *cmd,
+		      struct nvm_ret *ret);
+
+int nvm_be_sysfs_admin(struct nvm_dev *dev, struct nvm_cmd *cmd,
+		       struct nvm_ret *ret);
+
+int nvm_be_sysfs_vuser(struct nvm_dev *dev, struct nvm_cmd *cmd,
+		       struct nvm_ret *ret);
+
+int nvm_be_sysfs_vadmin(struct nvm_dev *dev, struct nvm_cmd *cmd,
+			struct nvm_ret *ret);
+
+#endif /* __INTERNAL_NVM_BE_SYSFS */
