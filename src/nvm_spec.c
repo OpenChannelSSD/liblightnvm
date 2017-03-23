99060ef6d7adc21498a7757dcf90f287588f09b9
Simon A. F. Lund <slund@cnexlabs.com>
2017-03-23 22:38:47 +0100

    nvm_spec: Changed printer output to yaml

--- src/nvm_spec.c
+++ src/nvm_spec.c
@@ -40,68 +40,77 @@ void _s12_identify_pr(struct spec_identify *identify)
 {
 	struct s12_identify idf = identify->s12;
 
-	printf("spec_identify {\n");
-	printf(" verid(0x%x), vnvmt(%u), cgroups(%u),\n",
-		idf.verid, idf.vnvmt, idf.cgroups);
-	printf(" cap("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idf.cap));
-	printf(" dom("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idf.dom));
+	printf("spec_identify:\n");
+	printf("  verid: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idf.verid));
+	printf("  vnvmt(%u)\n", idf.vnvmt);
+	printf("  cgroups(%u),\n", idf.cgroups);
+	printf("  cap("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idf.cap));
+	printf("  dom("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idf.dom));
+
+	printf("spec_identify_cgrps:\n");
 
 	for (int i = 0; i < idf.cgroups; ++i) {
 		struct spec_cgrp grp = idf.grp[i];
 
-		printf(" cgrp(%d) {\n", i);
-		printf("  mtype(0x%02x),\n", grp.mtype);
-		if (!grp.mtype) {
-			printf("  fmtype("NVM_I8_FMT"),\n",
-				NVM_I8_TO_STR(grp.fmtype));
-			printf("  num_ch(%u), num_luns(%u), num_pln(%u),",
-				grp.num_ch, grp.num_lun, grp.num_pln);
-			printf(" num_blk(%u), num_pg(%u),\n  fpg_sz(%u),",
-				grp.num_blk, grp.num_pg, grp.fpg_sz);
-			printf(" csecs(%u), sos(%u),\n",
-				grp.csecs, grp.sos);
-			printf("  trdt(%d), trdm(%d),\n", grp.trdt, grp.trdm);
-			printf("  tprt(%d), tprm(%d),\n", grp.tprt, grp.tprm);
-			printf("  tbet(%d), tbem(%d),\n", grp.tbet, grp.tbem);
-			printf("  mpos("NVM_I32_FMT"),\n",
-				NVM_I32_TO_STR(grp.mpos));
-			printf("  mccap("NVM_I32_FMT"),\n",
-				NVM_I32_TO_STR(grp.mccap));
-			printf("  cpar(%d),\n", grp.cpar);
-			printf("  mts(NOT IMPLEMENTED)\n");
-		}
-		printf(" }\n");
+		printf("  mtype: 0x%02x)\n", grp.mtype);
+		if (grp.mtype)
+			continue;
+
+		printf("  fmtype: "NVM_I8_FMT"\n", NVM_I8_TO_STR(grp.fmtype));
+		printf("  num_ch: %u\n", grp.num_ch);
+		printf("  num_luns: %u\n", grp.num_lun);
+		printf("  num_pln: %u\n", grp.num_pln);
+		printf("  num_blk: %u\n", grp.num_blk);
+		printf("  num_pg: %u\n", grp.num_pg);
+		printf("  fpg_sz: %u\n", grp.fpg_sz);
+		printf("  csecs: %u\n", grp.csecs);
+		printf("  sos: %u\n", grp.sos);
+		printf("  trdt: %d\n", grp.trdt);
+		printf("  trdm: %d\n", grp.trdm);
+		printf("  tprt: %d\n", grp.tprt);
+		printf("  tprm: %d\n", grp.tprm);
+		printf("  tbet: %d\n", grp.tbet);
+		printf("  tbem: %d\n", grp.tbem);
+		printf("  mpos: "NVM_I32_FMT"\n", NVM_I32_TO_STR(grp.mpos));
+		printf("  mccap: "NVM_I32_FMT"\n", NVM_I32_TO_STR(grp.mccap));
+		printf("  cpar: %d\n", grp.cpar);
+		printf("  mts: NOT_IMPLEMENTED\n");
 	}
-	printf("}\n");
 }
 
 void _s20_identify_pr(struct spec_identify *identify)
 {
 	struct s20_identify idf = identify->s20;
 
-	printf("spec_identify {\n");
-	printf(" verid("NVM_I8_FMT"),\n", NVM_I8_TO_STR(idf.verid));
-	printf(" mccap("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idf.mccap));
-	printf(" num_ch(%u),\n", idf.num_ch);
-	printf(" num_lun(%u),\n", idf.num_lun);
-	printf(" num_chk(%u),\n", idf.num_chk);
-	printf(" clba(%u),\n", idf.clba);
-	printf(" csecs(%u),\n", idf.csecs);
-	printf(" sos(%u),\n", idf.sos);
-	printf(" mw_min(%u),\n", idf.mw_min);
-	printf(" mw_opt(%u),\n", idf.mw_opt);
-	printf(" mw_cunits(%u),\n", idf.mw_cunits);
-	printf(" trdt(%d), trdm(%d),\n", idf.trdt, idf.trdm);
-	printf(" tprt(%d), tprm(%d),\n", idf.twrt, idf.twrm);
-	printf(" tbet(%d), tbem(%d),\n", idf.tcet, idf.tcem);
-	printf("}\n");
-	printf("spec_identify-");spec_lbaf_pr(&idf.lbaf);
-	printf("spec_identify-");spec_ppaf_nand_pr(&idf.ppaf);
-
+	printf("spec_identify:\n");
+	printf("  verid: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idf.verid));
+	printf("  mccap: "NVM_I32_FMT"\n", NVM_I32_TO_STR(idf.mccap));
+	printf("  num_ch: %u\n", idf.num_ch);
+	printf("  num_lun: %u\n", idf.num_lun);
+	printf("  num_chk: %u\n", idf.num_chk);
+	printf("  clba: %u\n", idf.clba);
+	printf("  csecs: %u\n", idf.csecs);
+	printf("  sos: %u\n", idf.sos);
+	printf("  mw_min: %u\n", idf.mw_min);
+	printf("  mw_opt: %u\n", idf.mw_opt);
+	printf("  mw_cunits: %u\n", idf.mw_cunits);
+	printf("  trdt: %d\n", idf.trdt);
+	printf("  trdm: %d\n", idf.trdm);
+	printf("  twrt: %d\n", idf.twrt);
+	printf("  twrm: %d\n", idf.twrm);
+	printf("  tcet: %d\n", idf.tcet);
+	printf("  tcem: %d\n", idf.tcem);
+	printf("spec_identify_"); spec_lbaf_pr(&idf.lbaf);
+	printf("spec_identify_"); spec_ppaf_nand_pr(&idf.ppaf);
 }
 
 void spec_identify_pr(struct spec_identify *identify)
 {
+	if (!identify) {
+		printf("spec_identify: ~\n");
+		return;
+	}
+
 	switch(identify->s.verid) {
 	case SPEC_VERID_12:
 		_s12_identify_pr(identify);
@@ -109,44 +118,48 @@ void spec_identify_pr(struct spec_identify *identify)
 	case SPEC_VERID_20:
 		_s20_identify_pr(identify);
 		break;
+
 	default:
-		printf("verid(%d:UNSUPPORTED)\n", identify->s.verid);
+		printf("spec_identify:\n");
+		printf("  verid("NVM_I8_FMT"),\n", NVM_I8_TO_STR(identify->s.verid));
 	}
 }
 
 void spec_ppaf_nand_pr(struct spec_ppaf_nand *ppaf)
 {
-
-	printf("ppaf {\n");
-	printf("  ch_off(%02u),  ch_len(%02u),\n", ppaf->n.ch_off, ppaf->n.ch_len);
-	printf(" lun_off(%02u), lun_len(%02u),\n", ppaf->n.lun_off, ppaf->n.lun_len);
-	printf("  pl_off(%02u),  pl_len(%02u),\n", ppaf->n.pl_off, ppaf->n.pl_len);
-	printf(" blk_off(%02u), blk_len(%02u),\n", ppaf->n.blk_off, ppaf->n.blk_len);
-	printf("  pg_off(%02u),  pg_len(%02u),\n", ppaf->n.pg_off, ppaf->n.pg_len);
-	printf(" sec_off(%02u), sec_len(%02u),\n", ppaf->n.sec_off, ppaf->n.sec_len);
-	printf("}\n");
+	printf("ppaf:\n");
+	printf("  ch_off: %02u\n", ppaf->n.ch_off);
+	printf("  ch_len: %02u\n", ppaf->n.ch_len);
+	printf("  lun_off: %02u\n", ppaf->n.lun_off);
+	printf("  lun_len: %02u\n", ppaf->n.lun_len);
+	printf("  pl_off: %02u\n", ppaf->n.pl_off);
+	printf("  pl_len: %02u\n", ppaf->n.pl_len);
+	printf("  blk_off: %02u\n", ppaf->n.blk_off);
+	printf("  blk_len: %02u\n", ppaf->n.blk_len);
+	printf("  pg_off: %02u\n", ppaf->n.pg_off);
+	printf("  pg_len: %02u\n", ppaf->n.pg_len);
+	printf("  sec_off: %02u\n", ppaf->n.sec_off);
+	printf("  sec_len: %02u\n", ppaf->n.sec_len);
 }
 
 void spec_ppaf_nand_mask_pr(struct spec_ppaf_nand_mask *mask)
 {
-	printf("ppaf_mask {\n");
-	printf("  ch("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.ch));
-	printf(" lun("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.lun));
-	printf("  pl("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.pl));
-	printf(" blk("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.blk));
-	printf("  pg("NVM_I64_FMT"),\n", NVM_I64_TO_STR(mask->n.pg));
-	printf(" sec("NVM_I64_FMT")\n", NVM_I64_TO_STR(mask->n.sec));
-	printf("}\n");
+	printf("ppaf_mask:\n");
+	printf("  ch:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.ch));
+	printf("  lun: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.lun));
+	printf("  pl:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.pl));
+	printf("  blk: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.blk));
+	printf("  pg:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.pg));
+	printf("  sec: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.sec));
 }
 
 void spec_lbaf_pr(struct spec_lbaf *lbaf)
 {
-	printf("lbaf {\n");
-	printf(" ch_len(%u),\n", lbaf->ch_len);
-	printf(" lun_len(%u),\n", lbaf->lun_len);
-	printf(" cnk_len(%u),\n", lbaf->cnk_len);
-	printf(" sec_len(%u),\n", lbaf->sec_len);
-	printf("}\n");
+	printf("lbaf:\n");
+	printf("  ch_len: %u\n", lbaf->ch_len);
+	printf("  lun_len: %u\n", lbaf->lun_len);
+	printf("  cnk_len: %u\n", lbaf->cnk_len);
+	printf("  sec_len: %u\n", lbaf->sec_len);
 }
 
 struct spec_bbt *spec_bbt_get(struct nvm_dev *dev, struct nvm_addr addr,
@@ -244,29 +257,25 @@ int spec_bbt_set(struct nvm_dev *dev, struct nvm_addr addrs[],
 void spec_bbt_pr(struct spec_bbt *bbt)
 {
 	if (!bbt) {
-		printf("spec_bbt(NULL)\n");
+		printf("spec_bbt: ~\n");
 		return;
 	}
 
-	printf("tblkid {%c, %c, %c, %c}\n",
-	       bbt->tblid[0], bbt->tblid[1], bbt->tblid[2], bbt->tblid[3]);
-	printf("verid(%u)\n", bbt->verid);
-	printf("revid(%u)\n", bbt->revid);
-	printf("rvsd1(%u)\n", bbt->rvsd1);
-	printf("tblks(%u)\n", bbt->tblks);
-	printf("tfact(%u)\n", bbt->tfact);
-	printf("tgrown(%u)\n", bbt->tgrown);
+	printf("spec_bbt:\n");
+	printf("  tblkid: %c%c%c%c\n",
+		   bbt->tblid[0], bbt->tblid[1], bbt->tblid[2], bbt->tblid[3]);
+	printf("  verid: %u\n", bbt->verid);
+	printf("  revid: %u\n", bbt->revid);
+	printf("  rvsd1: %u\n", bbt->rvsd1);
+	printf("  tblks: %u\n", bbt->tblks);
+	printf("  tfact: %u\n", bbt->tfact);
+	printf("  tgrown: %u\n", bbt->tgrown);
+	printf("  tdresv: %u\n", bbt->tdresv);
+	printf("  thresv: %u\n", bbt->thresv);
+	printf("  rsvd2: ~\n");
 
-	printf("tdresv(%u)\n", bbt->tdresv);
-	printf("thresv(%u)\n", bbt->thresv);
-	printf("rsvd2(..)\n");
-
-	printf("blk[] (notgood) {\n");
+	printf("  tblks:\n");
 	for (uint32_t i = 0; i < bbt->tblks; ++i) {
-		if (i)
-			printf("\n");
-		printf("i(%d) = %u", i, bbt->blk[i]);
+		printf("    - %u\n", bbt->blk[i]);
 	}
-	printf("}\n");
 }
-
