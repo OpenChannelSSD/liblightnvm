56d691c2bef5e8965a915173b233904d2b515897
Simon A. F. Lund <slund@cnexlabs.com>
2017-06-06 12:08:08 +0200

    Ensure that errno is set when nvm_dev_open[f] fails

--- src/nvm_dev.c
+++ src/nvm_dev.c
@@ -308,8 +308,10 @@ struct nvm_dev * nvm_dev_openf(const char *dev_path, int flags) {
 		}
 	}
 
-	if (!dev)
+	if (!dev) {
+		errno = errno ? errno : EIO;
 		return NULL;
+	}
 
 	dev->bbts_cached = 0;
 	dev->nbbts = dev->geo.nchannels * dev->geo.nluns;
