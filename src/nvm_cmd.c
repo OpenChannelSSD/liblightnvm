c29885ebba2ea736ac83f51641caeba491c6b545
Simon A. F. Lund <slund@cnexlabs.com>
2017-08-21 10:12:27 +0200

    nvm_cmd: Changed print format to use inttype MACROs

--- src/nvm_cmd.c
+++ src/nvm_cmd.c
@@ -26,6 +26,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include <inttypes.h>
 #include <stdio.h>
 #include <liblightnvm.h>
 #include <nvm_be.h>
@@ -55,27 +56,27 @@ int nvm_cmd_admin(struct nvm_dev *dev, struct nvm_cmd *cmd, struct nvm_ret *ret)
 void nvm_cmd_vuser_pr(struct nvm_cmd *cmd)
 {
 	printf("cmd.vuser:\n");
-	printf(" opcode: 0x%02x\n", cmd->vuser.opcode);
-	printf(" flags: 0x%02x\n", cmd->vuser.flags);
-	printf(" control: 0x%04x\n", cmd->vuser.control);
-	printf(" nppas: %u\n", cmd->vuser.nppas);
-	printf(" metadata: %lu\n", cmd->vuser.metadata);
-	printf(" addr: %lu\n", cmd->vuser.addr);
+	printf(" opcode: 0x%02"PRIx8"\n", cmd->vuser.opcode);
+	printf(" flags: 0x%02"PRIx8"\n", cmd->vuser.flags);
+	printf(" control: 0x%04"PRIx16"\n", cmd->vuser.control);
+	printf(" nppas: %"PRIu16"\n", cmd->vuser.nppas);
+	printf(" metadata: %"PRIu64"\n", cmd->vuser.metadata);
+	printf(" addr: %"PRIu64"\n", cmd->vuser.addr);
 	/*
 	printf(" ppa_list {");
 	for (int i = 0; i <= cmd->vuser.nppas; ++i) {
 		printf("  0x%016lx,\n", ((uint64_t*)cmd->vuser.ppa_list)[i]);
 	}*/
-	printf(" metadata_len: %u\n", cmd->vuser.metadata_len);
-	printf(" data_len: %u\n", cmd->vuser.data_len);
-	printf(" status: 0x%016lx\n", cmd->vuser.status);
-	printf(" result: 0x%016x\n", cmd->vuser.result);
+	printf(" metadata_len: %"PRIu32"\n", cmd->vuser.metadata_len);
+	printf(" data_len: %"PRIu32"\n", cmd->vuser.data_len);
+	printf(" status: 0x%016"PRIx64"\n", cmd->vuser.status);
+	printf(" result: 0x%016"PRIx32"\n", cmd->vuser.result);
 }
 
 void nvm_cmd_pr(struct nvm_cmd *cmd)
 {
 	printf("nvm_cmd:\n");
-	for (int i = 0; i < 16; ++i)
-		printf("  cdw%02d: "NVM_I32_FMT"\n", i, NVM_I32_TO_STR(cmd->cdw[i]));
+	for (int32_t i = 0; i < 16; ++i)
+		printf("  cdw%02"PRIi32": "NVM_I32_FMT"\n", i, NVM_I32_TO_STR(cmd->cdw[i]));
 }
 
