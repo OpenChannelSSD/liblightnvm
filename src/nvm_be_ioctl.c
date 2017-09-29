11f45fd65255ca92d49eadb060992b357bbe0996
Simon A. F. Lund <slund@cnexlabs.com>
2017-09-29 11:19:44 +0200

    nvm_be_ioctl: Switched to using 'nvm_be_populate'

--- src/nvm_be_ioctl.c
+++ src/nvm_be_ioctl.c
@@ -57,16 +57,6 @@ struct nvm_be nvm_be_ioctl = {
 #include <nvm_dev.h>
 #include <nvm_debug.h>
 
-static inline uint64_t _ilog2(uint64_t x)
-{
-	uint64_t val = 0;
-
-	while (x >>= 1)
-		val++;
-
-	return val;
-}
-
 int nvm_be_ioctl_vuser(struct nvm_dev *dev, struct nvm_cmd *cmd,
 		       struct nvm_ret *ret)
 {
@@ -146,130 +136,9 @@ int nvm_be_ioctl_admin(struct nvm_dev *dev, struct nvm_cmd *cmd,
 	return 0;
 }
 
-static inline void _construct_ppaf_mask(struct nvm_spec_ppaf_nand *ppaf,
-					struct nvm_spec_ppaf_nand_mask *mask)
-{
-	for (int i = 0 ; i < 12; ++i) {
-		if ((i % 2)) {
-			// i-1 = offset
-			// i = width
-			mask->a[i/2] = (((uint64_t)1<< ppaf->a[i])-1) << ppaf->a[i-1];
-		}
-	}
-}
-
-static inline int _ioctl_fill_geo(struct nvm_dev *dev, struct nvm_ret *ret)
-{
-	struct nvm_cmd cmd = {.cdw={0}};
-	struct nvm_spec_identify *idf;
-	int err;
-
-	idf = nvm_buf_alloca(4096, sizeof(*idf));
-	if (!idf) {
-		errno = ENOMEM;
-		return -1;
-	}
-	memset(idf, 0, sizeof(*idf));
-
-	cmd.vadmin.opcode = NVM_S12_OPC_IDF;	// Setup command
-	cmd.vadmin.addr = (uint64_t)idf;
-	cmd.vadmin.data_len = sizeof(*idf);
-
-	err = nvm_be_ioctl_vadmin(dev, &cmd, ret);
-	if (err) {
-		nvm_buf_free(idf);
-		return -1;			// NOTE: Propagate errno
-	}
-
-	switch (idf->s.verid) {
-	case NVM_SPEC_VERID_12:
-		dev->geo.page_nbytes = idf->s12.grp[0].fpg_sz;
-		dev->geo.sector_nbytes = idf->s12.grp[0].csecs;
-		dev->geo.meta_nbytes = idf->s12.grp[0].sos;
-
-		dev->geo.nchannels = idf->s12.grp[0].num_ch;
-		dev->geo.nluns = idf->s12.grp[0].num_lun;
-		dev->geo.nplanes = idf->s12.grp[0].num_pln;
-		dev->geo.nblocks = idf->s12.grp[0].num_blk;
-		dev->geo.npages = idf->s12.grp[0].num_pg;
-
-		dev->ppaf = idf->s12.ppaf;
-		dev->mccap = idf->s12.grp[0].mccap;
-		break;
-
-	case NVM_SPEC_VERID_20:
-		dev->geo.sector_nbytes = idf->s20.csecs;
-		dev->geo.meta_nbytes = idf->s20.sos;
-		dev->geo.page_nbytes = idf->s20.mw_min * dev->geo.sector_nbytes;
-
-		dev->geo.nchannels = idf->s20.num_ch;
-		dev->geo.nluns = idf->s20.num_lun;
-		dev->geo.nplanes = idf->s20.mw_opt / idf->s20.mw_min;
-		dev->geo.nblocks = idf->s20.num_chk;
-		dev->geo.npages = ((idf->s20.clba * idf->s20.csecs) / dev->geo.page_nbytes) / dev->geo.nplanes;
-		dev->geo.nsectors = dev->geo.page_nbytes / dev->geo.sector_nbytes;
-
-		dev->ppaf = idf->s20.ppaf;
-		dev->mccap = idf->s20.mccap;
-		break;
-
-	default:
-		NVM_DEBUG("Unsupported Version ID(%d)", idf->s.verid);
-		errno = ENOSYS;
-		nvm_buf_free(idf);
-		return -1;
-	}
-
-	dev->verid = idf->s.verid;
-	_construct_ppaf_mask(&dev->ppaf, &dev->mask);
-
-	// WARN: HOTFIX for reports of unrealisticly large OOB area
-	if (dev->geo.meta_nbytes > (dev->geo.sector_nbytes * 0.1)) {
-		dev->geo.meta_nbytes = 16;	// Naively hope this is right
-	}
-	dev->geo.nsectors = dev->geo.page_nbytes / dev->geo.sector_nbytes;
-
-	/* Derive total number of bytes on device */
-	dev->geo.tbytes = dev->geo.nchannels * dev->geo.nluns * \
-			  dev->geo.nplanes * dev->geo.nblocks * \
-			  dev->geo.npages * dev->geo.nsectors * \
-			  dev->geo.sector_nbytes;
-
-	/* Derive the sector-shift-width for LBA mapping */
-	dev->ssw = _ilog2(dev->geo.sector_nbytes);
-
-	/* Derive a default plane mode */
-	switch(dev->geo.nplanes) {
-	case 4:
-		dev->pmode = NVM_FLAG_PMODE_QUAD;
-		break;
-	case 2:
-		dev->pmode = NVM_FLAG_PMODE_DUAL;
-		break;
-	case 1:
-		dev->pmode = NVM_FLAG_PMODE_SNGL;
-		break;
-	default:
-		errno = EINVAL;
-		nvm_buf_free(idf);
-		return -1;
-	}
-
-	dev->erase_naddrs_max = NVM_NADDR_MAX;
-	dev->write_naddrs_max = NVM_NADDR_MAX;
-	dev->read_naddrs_max = NVM_NADDR_MAX;
-
-	dev->meta_mode = NVM_META_MODE_NONE;
-
-	nvm_buf_free(idf);
-
-	return 0;
-}
-
 struct nvm_dev *nvm_be_ioctl_open(const char *dev_path, int flags)
 {
 	struct nvm_dev *dev = NULL;
-	struct nvm_ret ret = {0,0};
 	int err;
 	
 	if (strlen(dev_path) > NVM_DEV_PATH_LEN) {
@@ -291,6 +160,7 @@ struct nvm_dev *nvm_be_ioctl_open(const char *dev_path, int flags)
 	case NVM_BE_IOCTL_WRITABLE:
 		dev->fd = open(dev->path, O_RDWR | O_DIRECT);
 		break;
+
 	default:
 		dev->fd = open(dev->path, O_RDONLY);
 		break;
@@ -304,7 +174,7 @@ struct nvm_dev *nvm_be_ioctl_open(const char *dev_path, int flags)
 		return NULL;	// Propagate errno from open
 	}
 
-	err = _ioctl_fill_geo(dev, &ret);
+	err = nvm_be_populate(dev, nvm_be_ioctl_vadmin);
 	if (err) {
 		NVM_DEBUG("FAILED: _ioctl_fill_geo, err(%d)", err);
 
