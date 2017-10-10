959f0f270d7fac2b5e8804baadcd330d16e05ebe
Simon A. F. Lund <slund@cnexlabs.com>
2017-10-10 16:21:48 +0200

    nvm_spec: Adjusted to changes in 2.0 spec. and removed bbt_{get|set}

--- src/nvm_spec.c
+++ src/nvm_spec.c
@@ -36,21 +36,125 @@
 #include <nvm_utils.h>
 #include <nvm_debug.h>
 
-void _nvm_spec_identify_s12_pr(const struct nvm_spec_identify *identify)
+void nvm_spec_lgeo_pr(const struct nvm_spec_lgeo *lgeo)
 {
-	struct nvm_spec_identify_s12 idf = identify->s12;
+	printf("lgeo:");
+	
+	if (!lgeo) {
+		printf(" ~\n");
+		return;
+	}
 
-	printf("nvm_spec_identify:\n");
-	printf("  verid: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idf.verid));
-	printf("  vnvmt(%u)\n", idf.vnvmt);
-	printf("  cgroups(%u),\n", idf.cgroups);
-	printf("  cap("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idf.cap));
-	printf("  dom("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idf.dom));
+	printf("\n");
+	printf("  npugrp: %u\n", lgeo->npugrp);
+	printf("  npunit: %u\n", lgeo->npunit);
+	printf("  nchunk: %u\n", lgeo->nchunk);
+	printf("  nsectr: %u\n", lgeo->nsectr);
+	printf("  nbytes: %u\n", lgeo->nbytes);
+	printf("  nbytes_oob: %u\n", lgeo->nbytes_oob);
+}
+
+void nvm_spec_wrt_pr(const struct nvm_spec_wrt *wrt)
+{
+	printf("wrt:");
+
+	if (!wrt) {
+		printf(" ~\n");
+		return;
+	}
+
+	printf(" {ws_min: %u, ws_opt: %u, mw_cunits: %u}\n",
+	       wrt->ws_min, wrt->ws_opt, wrt->mw_cunits);
+}
+
+void nvm_spec_perf_pr(const struct nvm_spec_perf *perf)
+{
+	printf("perf:");
+
+	if (!perf) {
+		printf(" ~\n");
+		return;
+	}
+
+	printf(" {trdt: %u, trdm: %u, twrt: %u,"
+	       " twrm: %u, tcet: %u, tcem: %u}\n",
+	       perf->trdt, perf->trdm, perf->twrt,
+	       perf->twrm, perf->tcet, perf->tcem);
+}
+
+void nvm_spec_ppaf_nand_pr(const struct nvm_spec_ppaf_nand *ppaf)
+{
+	printf("ppaf:");
+
+	if (!ppaf) {
+		printf(" ~\n");
+		return;
+	}
+
+	printf("\n");
+	printf("  ch_off: %02u\n", ppaf->n.ch_off);
+	printf("  ch_len: %02u\n", ppaf->n.ch_len);
+	printf("  lun_off: %02u\n", ppaf->n.lun_off);
+	printf("  lun_len: %02u\n", ppaf->n.lun_len);
+	printf("  pl_off: %02u\n", ppaf->n.pl_off);
+	printf("  pl_len: %02u\n", ppaf->n.pl_len);
+	printf("  blk_off: %02u\n", ppaf->n.blk_off);
+	printf("  blk_len: %02u\n", ppaf->n.blk_len);
+	printf("  pg_off: %02u\n", ppaf->n.pg_off);
+	printf("  pg_len: %02u\n", ppaf->n.pg_len);
+	printf("  sec_off: %02u\n", ppaf->n.sec_off);
+	printf("  sec_len: %02u\n", ppaf->n.sec_len);
+}
+
+void nvm_spec_ppaf_nand_mask_pr(const struct nvm_spec_ppaf_nand_mask *mask)
+{
+	printf("ppaf_mask:");
+
+	if (!mask) {
+		printf(" ~\n");
+		return;
+	}
+
+	printf("\n");
+	printf("  ch:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.ch));
+	printf("  lun: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.lun));
+	printf("  pl:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.pl));
+	printf("  blk: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.blk));
+	printf("  pg:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.pg));
+	printf("  sec: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.sec));
+}
+
+void nvm_spec_lbaf_pr(const struct nvm_spec_lbaf *lbaf)
+{
+	printf("lbaf:");
+
+	if (!lbaf) {
+		printf(" ~\n");
+		return;
+	}
+
+	printf("\n");
+	printf("  pugrp_len: %u\n", lbaf->pugrp_len);
+	printf("  punit_len: %u\n", lbaf->punit_len);
+	printf("  chunk_len: %u\n", lbaf->chunk_len);
+	printf("  sectr_len: %u\n", lbaf->sectr_len);
+}
+
+static void nvm_spec_idfy_s12_pr(const struct nvm_spec_idfy *identify)
+{
+	struct nvm_spec_idfy_s12 idfy = identify->s12;
+
+	printf("idfy:\n");
+	printf("  verid: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idfy.verid));
+	printf("  vnvmt(%u)\n", idfy.vnvmt);
+	printf("  cgroups(%u),\n", idfy.cgroups);
+	printf("  cap("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idfy.cap));
+	printf("  dom("NVM_I32_FMT"),\n", NVM_I32_TO_STR(idfy.dom));
 
 	printf("spec_identify_cgrps:\n");
 
-	for (int i = 0; i < idf.cgroups; ++i) {
-		struct nvm_spec_cgrp grp = idf.grp[i];
+	for (int i = 0; i < idfy.cgroups; ++i) {
+		struct nvm_spec_idfy_cgrp grp = idfy.grp[i];
 
 		printf("  mtype: 0x%02x)\n", grp.mtype);
 		if (grp.mtype)
@@ -78,100 +182,69 @@ void _nvm_spec_identify_s12_pr(const struct nvm_spec_identify *identify)
 	}
 }
 
-void _nvm_spec_identify_s20_pr(const struct nvm_spec_identify *identify)
+static void nvm_spec_idfy_s13_pr(const struct nvm_spec_idfy *identify)
 {
-	struct nvm_spec_identify_s20 idf = identify->s20;
+	struct nvm_spec_idfy_s13 idfy = identify->s13;
 
-	printf("nvm_spec_identify:\n");
-	printf("  verid: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idf.verid));
-	printf("  mccap: "NVM_I32_FMT"\n", NVM_I32_TO_STR(idf.mccap));
-	printf("  num_ch: %u\n", idf.num_ch);
-	printf("  num_lun: %u\n", idf.num_lun);
-	printf("  num_chk: %u\n", idf.num_chk);
-	printf("  clba: %u\n", idf.clba);
-	printf("  csecs: %u\n", idf.csecs);
-	printf("  sos: %u\n", idf.sos);
-	printf("  mw_min: %u\n", idf.mw_min);
-	printf("  mw_opt: %u\n", idf.mw_opt);
-	printf("  mw_cunits: %u\n", idf.mw_cunits);
-	printf("  trdt: %d\n", idf.trdt);
-	printf("  trdm: %d\n", idf.trdm);
-	printf("  twrt: %d\n", idf.twrt);
-	printf("  twrm: %d\n", idf.twrm);
-	printf("  tcet: %d\n", idf.tcet);
-	printf("  tcem: %d\n", idf.tcem);
-	printf("spec_identify_");
-	nvm_spec_lbaf_pr(&idf.lbaf);
-	printf("spec_identify_");
-	nvm_spec_ppaf_nand_pr(&idf.ppaf);
+	printf("idfy:\n");
+	printf("  verid: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idfy.verid));
+	printf("  verid_minor: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idfy.verid_minor));
+	printf("  mccap: "NVM_I32_FMT"\n", NVM_I32_TO_STR(idfy.mccap));
+	nvm_spec_ppaf_nand_pr(&idfy.ppaf);
+	nvm_spec_lbaf_pr(&idfy.lbaf);
+	nvm_spec_lgeo_pr(&idfy.lgeo);
+	nvm_spec_wrt_pr(&idfy.wrt);
+	nvm_spec_perf_pr(&idfy.perf);
 }
 
-void nvm_spec_identify_pr(const struct nvm_spec_identify *identify)
+static void nvm_spec_idfy_s20_pr(const struct nvm_spec_idfy *identify)
 {
-	if (!identify) {
-		printf("nvm_spec_identify: ~\n");
+	struct nvm_spec_idfy_s20 idfy = identify->s20;
+
+	printf("idfy:\n");
+	printf("  verid: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idfy.verid));
+	printf("  verid_minor: "NVM_I8_FMT"\n", NVM_I8_TO_STR(idfy.verid_minor));
+	printf("  mccap: "NVM_I32_FMT"\n", NVM_I32_TO_STR(idfy.mccap));
+	nvm_spec_lbaf_pr(&idfy.lbaf);
+	nvm_spec_lgeo_pr(&idfy.lgeo);
+	nvm_spec_wrt_pr(&idfy.wrt);
+	nvm_spec_perf_pr(&idfy.perf);
+}
+
+void nvm_spec_idfy_pr(const struct nvm_spec_idfy *idfy)
+{
+	if (!idfy) {
+		printf("nvm_spec_idfy: ~\n");
 		return;
 	}
 
-	switch(identify->s.verid) {
+	switch(idfy->s.verid) {
 	case NVM_SPEC_VERID_12:
-		_nvm_spec_identify_s12_pr(identify);
+		nvm_spec_idfy_s12_pr(idfy);
 		break;
+
+	case NVM_SPEC_VERID_13:
+		nvm_spec_idfy_s13_pr(idfy);
+		break;
+
 	case NVM_SPEC_VERID_20:
-		_nvm_spec_identify_s20_pr(identify);
+		nvm_spec_idfy_s20_pr(idfy);
 		break;
 
 	default:
-		printf("nvm_spec_identify:\n");
-		printf("  verid("NVM_I8_FMT"),\n", NVM_I8_TO_STR(identify->s.verid));
+		printf("nvm_spec_idfy:\n");
+		printf("  verid("NVM_I8_FMT"),\n", NVM_I8_TO_STR(idfy->s.verid));
 	}
-}
-
-void nvm_spec_ppaf_nand_pr(const struct nvm_spec_ppaf_nand *ppaf)
-{
-	printf("ppaf:\n");
-	printf("  ch_off: %02u\n", ppaf->n.ch_off);
-	printf("  ch_len: %02u\n", ppaf->n.ch_len);
-	printf("  lun_off: %02u\n", ppaf->n.lun_off);
-	printf("  lun_len: %02u\n", ppaf->n.lun_len);
-	printf("  pl_off: %02u\n", ppaf->n.pl_off);
-	printf("  pl_len: %02u\n", ppaf->n.pl_len);
-	printf("  blk_off: %02u\n", ppaf->n.blk_off);
-	printf("  blk_len: %02u\n", ppaf->n.blk_len);
-	printf("  pg_off: %02u\n", ppaf->n.pg_off);
-	printf("  pg_len: %02u\n", ppaf->n.pg_len);
-	printf("  sec_off: %02u\n", ppaf->n.sec_off);
-	printf("  sec_len: %02u\n", ppaf->n.sec_len);
-}
-
-void nvm_spec_ppaf_nand_mask_pr(const struct nvm_spec_ppaf_nand_mask *mask)
-{
-	printf("ppaf_mask:\n");
-	printf("  ch:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.ch));
-	printf("  lun: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.lun));
-	printf("  pl:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.pl));
-	printf("  blk: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.blk));
-	printf("  pg:  '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.pg));
-	printf("  sec: '"NVM_I64_FMT"'\n", NVM_I64_TO_STR(mask->n.sec));
-}
-
-void nvm_spec_lbaf_pr(const struct nvm_spec_lbaf *lbaf)
-{
-	printf("lbaf:\n");
-	printf("  ch_len: %u\n", lbaf->ch_len);
-	printf("  lun_len: %u\n", lbaf->lun_len);
-	printf("  cnk_len: %u\n", lbaf->cnk_len);
-	printf("  sec_len: %u\n", lbaf->sec_len);
 }
 
 void nvm_spec_bbt_pr(const struct nvm_spec_bbt *bbt)
 {
 	if (!bbt) {
-		printf("nvm_spec_bbt: ~\n");
+		printf("bbt: ~\n");
 		return;
 	}
 
-	printf("nvm_spec_bbt:\n");
+	printf("bbt:\n");
 	printf("  tblkid: %c%c%c%c\n",
 		   bbt->tblid[0], bbt->tblid[1], bbt->tblid[2], bbt->tblid[3]);
 	printf("  verid: %u\n", bbt->verid);
@@ -188,96 +261,5 @@ void nvm_spec_bbt_pr(const struct nvm_spec_bbt *bbt)
 	for (uint32_t i = 0; i < bbt->tblks; ++i) {
 		printf("    - %u\n", bbt->blk[i]);
 	}
-}
-
-struct nvm_spec_bbt *nvm_spec_bbt_get(struct nvm_dev *dev, struct nvm_addr addr,
-				      struct nvm_ret *ret)
-{
-	struct nvm_cmd cmd = {.cdw={0}};
-	struct nvm_spec_bbt *spec_bbt;
-	size_t spec_bbt_sz;
-	int err;
-
-	uint32_t nblks = dev->geo.nblocks * dev->geo.nplanes;
-	spec_bbt_sz = sizeof(*spec_bbt) + sizeof(*(spec_bbt->blk)) * nblks;
-	spec_bbt = nvm_buf_alloc(&dev->geo, spec_bbt_sz);
-	if (!spec_bbt) {
-		NVM_DEBUG("FAILED: malloc k_bbt failed");
-		errno = ENOMEM;
-		return NULL;
-	}
-
-	cmd.vadmin.opcode = NVM_S12_OPC_GET_BBT;
-	cmd.vadmin.addr = (uint64_t)spec_bbt;
-	cmd.vadmin.data_len = spec_bbt_sz;
-	cmd.vadmin.ppa_list = nvm_addr_gen2dev(dev, addr);
-	cmd.vadmin.nppas = 0;
-
-	err = dev->be->vadmin(dev, &cmd, ret);
-	if (err || (spec_bbt->tblks != nblks)) {
-		NVM_DEBUG("FAILED: be execution failed");
-		errno = EIO;
-		nvm_buf_free(spec_bbt);
-		return NULL;
-	}
-	if (!(spec_bbt->tblid[0] == 'B' && spec_bbt->tblid[1] == 'B' &&
-	      spec_bbt->tblid[2] == 'L' && spec_bbt->tblid[3] == 'T')) {
-		NVM_DEBUG("FAILED: invalid format of returned bbt");
-		errno = EIO;
-		nvm_buf_free(spec_bbt);
-		return NULL;
-	}
-
-	return spec_bbt;
-}
-
-int nvm_spec_bbt_set(struct nvm_dev *dev, struct nvm_addr *addrs,
-		     int naddrs, uint16_t flags, struct nvm_ret *ret)
-{
-	struct nvm_cmd cmd = {.cdw={0}};
-	uint64_t dev_addrs[naddrs];
-	int err;
-
-	switch(flags) {
-	case NVM_BBT_FREE:
-	case NVM_BBT_BAD:
-	case NVM_BBT_DMRK:
-	case NVM_BBT_GBAD:
-	case NVM_BBT_HMRK:
-		break;
-	default:
-		NVM_DEBUG("FAILED: invalid mark");
-		errno = EINVAL;
-		return -1;
-	}
-
-	if (naddrs > NVM_NADDR_MAX) {
-		NVM_DEBUG("FAILED: naddrs > NVM_NADDR_MAX");
-		errno = EINVAL;
-		return -1;
-	}
-
-	for (int i = 0; i < naddrs; ++i) {	// Setup PPAs: Convert format
-		if (nvm_addr_check(addrs[i], &dev->geo)) {
-			NVM_DEBUG("FAILED: invalid addrs[i]");
-			errno = EINVAL;
-			return -1;
-		}
-		dev_addrs[i] = nvm_addr_gen2dev(dev, addrs[i]);
-	}
-
-	cmd.vadmin.opcode = NVM_S12_OPC_SET_BBT;	// Construct command
-	cmd.vadmin.control = flags;
-	cmd.vadmin.nppas = naddrs - 1;		// Unnatural numbers: counting from zero
-	cmd.vadmin.ppa_list = naddrs == 1 ? dev_addrs[0] : (uint64_t)dev_addrs;
-
-	err = dev->be->vadmin(dev, &cmd, ret);
-	if (err) {
-		NVM_DEBUG("FAILED: be execution failed");
-		errno = EIO;
-		return -1;
-	}
-
-	return 0;
 }
 
