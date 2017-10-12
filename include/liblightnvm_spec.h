083f46b6243ea4a774ead89447782f161219689f
Simon A. F. Lund <slund@cnexlabs.com>
2017-10-12 12:11:56 +0200

    nvm_spec: public: Adjusted to changes in 2.0 spec.
    Renamed and/or redefined:
    
     * struct nvm_spec_identify -> nvm_spec_idfy
     * struct nvm_spec_identify_s20 -> struct nvm_spec_idfy_s20
     * struct nvm_spec_identify_s12 -> struct nvm_spec_idfy_s12
     * struct nvm_spec_lbaf
     * ...
    
    Added:
    
     * struct nvm_nvme_cmd
     * struct nvm_spec_identify_s13
     * struct nvm_spec_rprt
     * enum nvm_spec_opcodes
     * enum nvm_spec_s20_opcodes
     * enum nvm_spec_rptr_opts
     * ...

--- include/liblightnvm_spec.h
+++ include/liblightnvm_spec.h
@@ -32,6 +32,7 @@
 #ifndef __LIBLIGHTNVM_SPEC_H
 #define __LIBLIGHTNVM_SPEC_H
 
+#include <assert.h>
 #include <stdint.h>
 #include <sys/types.h>
 
@@ -43,8 +44,7 @@ enum nvm_spec_12_mccap {
 };
 
 enum nvm_spec_20_mccap {
-	NVM_SPEC_20_MCCAP_SUSPENSION = 0x1,
-	NVM_SPEC_20_MCCAP_VCOPY = 0x1 << 16,
+	NVM_SPEC_20_MCCAP_VCOPY = 0x1,
 };
 
 /**
@@ -91,11 +91,30 @@ enum nvm_spec_12_opcodes {
 	NVM_S12_OPC_READ = 0x92,
 };
 
+enum nvm_spec_20_opcodes {
+	NVM_S20_OPC_IDF = 0xE2,
+	NVM_S20_OPC_RPT = 0xF2,
+	NVM_S20_OPC_ERASE = 0x90,
+	NVM_S20_OPC_WRITE = 0x91,
+	NVM_S20_OPC_READ = 0x92,
+	NVM_S20_OPC_COPY = 0x93,
+};
+
+enum nvm_spec_opcodes {
+	NVM_OPC_IDFY = 0xE2,
+	NVM_OPC_STATE = 0xF2,
+	NVM_OPC_ERASE = 0x90,
+	NVM_OPC_WRITE = 0x91,
+	NVM_OPC_READ = 0x92,
+	NVM_OPC_COPY = 0x93
+};
+
 /**
  * Specification version identifier
  */
 enum nvm_spec_verid {
 	NVM_SPEC_VERID_12 = 0x1,
+	NVM_SPEC_VERID_13 = 0x2 + (0x1 << 7),
 	NVM_SPEC_VERID_20 = 0x2,
 };
 
@@ -127,14 +146,16 @@ struct nvm_spec_ppaf_nand_mask {
 };
 
 struct nvm_spec_lbaf {
-	uint8_t ch_len;
-	uint8_t lun_len;
-	uint8_t cnk_len;
-	uint8_t sec_len;
+	uint8_t pugrp_len;
+	uint8_t punit_len;
+	uint8_t chunk_len;
+	uint8_t sectr_len;
 	uint8_t rsvd[4];
 };
 
-struct nvm_spec_cgrp {
+void nvm_spec_lbaf_pr(const struct nvm_spec_lbaf *lbaf);
+
+struct nvm_spec_idfy_cgrp {
 	uint8_t mtype;
 	uint8_t fmtype;
 	uint8_t rsvd_2_3[2];
@@ -161,7 +182,7 @@ struct nvm_spec_cgrp {
 	uint8_t mts[896];
 };
 
-struct nvm_spec_identify_s12 {
+struct nvm_spec_idfy_s12 {
 	uint8_t verid;
 	uint8_t vnvmt;
 	uint8_t cgroups;
@@ -170,44 +191,75 @@ struct nvm_spec_identify_s12 {
 	uint32_t dom;
 	struct nvm_spec_ppaf_nand ppaf;
 	uint8_t rsdv_028_255[228];
-	struct nvm_spec_cgrp grp[4];
+	struct nvm_spec_idfy_cgrp grp[4];
 };
 
-struct nvm_spec_identify_s20 {
-	uint8_t verid;
-	uint8_t rsvd_001_003[3];
-	struct nvm_spec_lbaf lbaf;
-	struct nvm_spec_ppaf_nand ppaf;
-	uint8_t rsvd_028_031[4];
-	uint32_t mccap;
-	uint8_t rsvd_036_063[28];
-	uint16_t num_ch;
-	uint16_t num_lun;
-	uint32_t num_chk;
-	uint32_t clba;
-	uint32_t csecs;
-	uint32_t sos;
-	uint8_t rsvd_084_127[44];
-	uint32_t mw_min;
-	uint32_t mw_opt;
-	uint32_t mw_cunits;
-	uint8_t rsvd_140_191[52];
-	uint32_t trdt;
-	uint32_t trdm;
-	uint32_t twrt;
-	uint32_t twrm;
-	uint32_t tcet;
-	uint32_t tcem;
-	uint8_t rsvd_216_255[41];
+struct nvm_spec_lgeo {
+	uint16_t	npugrp;		///< # controller groups (GRP)
+	uint16_t	npunit;		///< # parallel units (PUN) per GRP
+	uint32_t	nchunk;		///< # chunks (CHK) per PUN
+	uint32_t	nsectr;		///< # sectors in chunk
+	uint32_t	nbytes;		///< # bytes in sector
+	uint32_t	nbytes_oob;	///< # bytes in sector out-of-band area
+	uint8_t		resv[44];
+};
+
+void nvm_spec_lgeo_pr(const struct nvm_spec_lgeo *lgeo);
+
+struct nvm_spec_wrt {
+	uint32_t	ws_min;
+	uint32_t	ws_opt;
+	uint32_t	mw_cunits;
+	uint8_t		resv[52];
+};
+
+void nvm_spec_wrt_pr(const struct nvm_spec_wrt *lbaf);
+
+struct nvm_spec_perf {
+	uint32_t	trdt;
+	uint32_t	trdm;
+	uint32_t	twrt;
+	uint32_t	twrm;
+	uint32_t	tcet;
+	uint32_t	tcem;
+	uint8_t		resv[40];
+};
+
+struct nvm_spec_idfy_s13 {
+	uint8_t				verid;
+	uint8_t				verid_minor;
+	uint8_t				rsvd1[2];
+	struct nvm_spec_lbaf		lbaf;
+	struct nvm_spec_ppaf_nand	ppaf;
+	uint8_t				rsvd2[4];
+	uint32_t			mccap;
+	uint8_t				rsvd3[28];
+	struct nvm_spec_lgeo		lgeo;
+	struct nvm_spec_wrt		wrt;
+	struct nvm_spec_perf	perf;
+	uint8_t				rsvd4;
+};
+
+struct nvm_spec_idfy_s20 {
+	uint8_t				verid;
+	uint8_t				verid_minor;
+	uint8_t				rsvd1[6];
+	struct nvm_spec_lbaf		lbaf;
+	uint64_t			mccap;
+	uint8_t				rsvd2[44];
+	struct nvm_spec_lgeo		lgeo;
+	struct nvm_spec_wrt		wrt;
+	struct nvm_spec_perf	perf;
 };
 
 /**
  * Identify command data structure
  */
-struct nvm_spec_identify {
+struct nvm_spec_idfy {
 	union {
-		struct nvm_spec_identify_s12 s12;
-		struct nvm_spec_identify_s20 s20;
+		struct nvm_spec_idfy_s12 s12;
+		struct nvm_spec_idfy_s13 s13;
+		struct nvm_spec_idfy_s20 s20;
 
 		struct {
 			uint8_t verid;
@@ -215,6 +267,8 @@ struct nvm_spec_identify {
 		} s;	///< Shared between the revisions
 	};
 };
+
+void nvm_spec_idfy_pr(const struct nvm_spec_idfy *idfy);
 
 struct nvm_spec_bbt {
 	uint8_t		tblid[4];
@@ -230,9 +284,24 @@ struct nvm_spec_bbt {
 	uint8_t		blk[];
 };
 
-void nvm_spec_identify_pr(const struct nvm_spec_identify *idf);
+enum nvm_spec_rptr_opts {
+	NVM_RPRT_LIST_ALL = 0x0,
+	NVM_RPRT_LIST_FREE = 0x1,
+	NVM_RPRT_LIST_FULL = 0x2,
+	NVM_RPRT_LIST_OPEN = 0x3,
+	NVM_RPRT_LIST_BAD = 0x4,
 
-void nvm_spec_lbaf_pr(const struct nvm_spec_lbaf *lbaf);
+	NVM_RPRT_LIST_SEQW = 0xA,
+	NVM_RPRT_LIST_ARBW = 0xB,
+};
+
+struct nvm_spec_rprt {
+	uint64_t nchunks;	///< #chunks in report
+	uint8_t rsvd[63];
+	uint8_t descr[];	///< Chunk state descriptor table
+};
+
+
 
 /**
  * Prints a humanly readable representation of the give address format mask
@@ -254,5 +323,61 @@ void nvm_spec_ppaf_nand_mask_pr(const struct nvm_spec_ppaf_nand_mask *mask);
  * @param ppaf The address format to print
  */
 void nvm_spec_bbt_pr(const struct nvm_spec_bbt *bbt);
+
+struct nvm_nvme_cmd {
+	/* cdw 00 */
+	uint16_t opcode	:  8;			/* opcode */
+	uint16_t fuse	:  2;			/* fused operation */
+	uint16_t rsvd	:  4;
+	uint16_t psdt	:  2;
+	uint16_t cid;				/* command identifier */
+
+	/* cdw 01 */
+	uint32_t nsid;				/* namespace identifier */
+
+	uint32_t cdw02;
+	uint32_t cdw03;
+
+	/* cdw 04-05 */
+	uint64_t mptr;				/* MPTR -- metadata pointer */
+
+	/* cdw 06-09: */			/* DPTR -- data pointer */
+	union {
+		struct {
+			uint64_t prp1;		/* PRP entry 1 */
+			uint64_t prp2;		/* PRP entry 2 */
+		} prp;
+	} dptr;
+
+	/* cdw 10-11 */
+	uint64_t addrs;				/* PPA / LBA list */
+
+	/* cdw 12 */
+	union {
+		struct {
+			uint32_t naddrs	: 16;	/* # addrs. in PPA list */
+			uint32_t control: 16;	/* PMODE, Scrabler, etc. */
+		} s12;
+
+		struct {
+			uint32_t naddrs	: 6;	/* # addrs. in LBA list */
+			uint32_t rsvd	: 24;
+			uint32_t fua	: 1;	/* Force unit access */
+			uint32_t lr	: 1;	/* Limited retry */
+		} ewrc;
+
+		struct {
+			uint32_t naddrs	: 16;	/* # chunks to report */
+			uint32_t ro	: 4;	/* Reporting options */
+			uint32_t rsvd	: 12;	/* Reporting options */
+		} rprt;
+	};
+
+	uint32_t cdw13;
+
+	/* cdw 14-15 */
+	uint64_t addrs_dst;			/* destination addresses */
+};
+static_assert(sizeof(struct nvm_nvme_cmd) == 64, "Incorrect size");
 
 #endif /* __LIBLIGHTNVM_SPEC_H */
