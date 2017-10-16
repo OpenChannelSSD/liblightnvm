84dbff91cbd59fac22238aef58ac063b1285a9ec
Simon A. F. Lund <slund@cnexlabs.com>
2017-10-16 16:30:34 +0200

    nvm_be: internal: Re-defined 'nvm_be' interface

--- include/nvm_be.h
+++ include/nvm_be.h
@@ -50,41 +50,93 @@ struct nvm_be {
 	void (*close)(struct nvm_dev *);
 
 	/**
-	 * Send an user command to device
+	 * Execute identify command
 	 */
-	int (*user)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+	struct nvm_spec_idfy *(*idfy)(struct nvm_dev *, struct nvm_ret *);
 
 	/**
-	 * Send an admin command to device
+	 * Execute report chunk command
 	 */
-	int (*admin)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+	struct nvm_spec_rprt *(*rprt)(struct nvm_dev *, struct nvm_addr,
+				      uint16_t, int, struct nvm_ret *);
 
 	/**
-	 * Send a vector user command to device
+	 * Execute get bad-block-table command
 	 */
-	int (*vuser)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+	struct nvm_spec_bbt *(*gbbt)(struct nvm_dev *, struct nvm_addr,
+				     struct nvm_ret *);
 
 	/**
-	 * Send a vector admin command to device
+	 * Execute set bad-block-table command
 	 */
-	int (*vadmin)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *);
+	int (*sbbt)(struct nvm_dev *, struct nvm_addr *, int, uint16_t,
+		    struct nvm_ret *);
+
+	/**
+	 * Execute erase command
+	 */
+	int (*erase)(struct nvm_dev *, struct nvm_addr *, int, uint16_t,
+		     struct nvm_ret *);
+
+	/**
+	 * Execute write command
+	 */
+	int (*write)(struct nvm_dev *dev, struct nvm_addr *, int naddrs, void *,
+		     void *, uint16_t, struct nvm_ret *);
+
+	/**
+	 * Execute read command
+	 */
+	int (*read)(struct nvm_dev *dev, struct nvm_addr *, int naddrs, void *,
+		    void *, uint16_t, struct nvm_ret *);
+
+	/**
+	 * Execute copy command
+	 */
+	int (*copy)(struct nvm_dev *, struct nvm_addr *, struct nvm_addr *, int,
+		    uint16_t, struct nvm_ret *);
 };
+
+/**
+ * Helper functions for not supported / not implemented commands
+ */
 
 struct nvm_dev* nvm_be_nosys_open(const char *dev_path, int flags);
 
 void nvm_be_nosys_close(struct nvm_dev *dev);
 
-int nvm_be_nosys_user(struct nvm_dev *dev, struct nvm_cmd *cmd,
+struct nvm_spec_idfy *nvm_be_nosys_idfy(struct nvm_dev *dev,
+					struct nvm_ret *ret);
+
+struct nvm_spec_rprt *nvm_be_nosys_rprt(struct nvm_dev *dev,
+					struct nvm_addr addr, uint16_t naddrs,
+					int flags, struct nvm_ret *ret);
+
+struct nvm_spec_bbt *nvm_be_nosys_gbbt(struct nvm_dev *dev,
+				       struct nvm_addr addr,
+				       struct nvm_ret *ret);
+
+int nvm_be_nosys_sbbt(struct nvm_dev *dev, struct nvm_addr *addrs, int naddrs,
+		      uint16_t flags, struct nvm_ret *ret);
+
+int nvm_be_nosys_erase(struct nvm_dev *dev, struct nvm_addr addrs[], int naddrs,
+		       uint16_t flags, struct nvm_ret *ret);
+
+int nvm_be_nosys_write(struct nvm_dev *dev, struct nvm_addr addrs[], int naddrs,
+		       void *data, void *meta, uint16_t flags,
+		       struct nvm_ret *ret);
+
+int nvm_be_nosys_read(struct nvm_dev *dev, struct nvm_addr addrs[], int naddrs,
+		      void *data, void *meta, uint16_t flags,
 		      struct nvm_ret *ret);
 
-int nvm_be_nosys_admin(struct nvm_dev *dev, struct nvm_cmd *cmd,
-		       struct nvm_ret *ret);
+int nvm_be_nosys_copy(struct nvm_dev *dev, struct nvm_addr src[],
+		      struct nvm_addr dst[], int naddrs, uint16_t flags,
+		      struct nvm_ret *ret);
 
-int nvm_be_nosys_vuser(struct nvm_dev *dev, struct nvm_cmd *cmd,
-		       struct nvm_ret *ret);
-
-int nvm_be_nosys_vadmin(struct nvm_dev *dev, struct nvm_cmd *cmd,
-			struct nvm_ret *ret);
+/**
+ * Auxilary helpers
+ */
 
 /**
  * Splits a dev_path such as "/dev/nvme0n1" into {nvme_name: nvme0, nsid: 1}
@@ -94,38 +146,24 @@ int nvm_be_nosys_vadmin(struct nvm_dev *dev, struct nvm_cmd *cmd,
 int nvm_be_split_dpath(const char *dev_path, char *nvme_name, int *nsid);
 
 /**
- * Check that the given NVMe device exists in sysfs
- *
- * When nsid > 0 /sys/class/nvme/{nvme_name}/{nvme_name}n{nsid}
- * is checked for existance, /sys/class/nvme/{nvme_name} otherwise.
- *
- * @returns 1 when it exists 0 otherwise
- */
-int nvm_be_sysfs_exists(const char *nvme_name, int nsid);
-
-/**
- * Read sysfs attributes from NVMe devices.
- *
- * When nsid > 0 /sys/class/nvme/{nvme_name}/{nvme_name}n{nsid}/lightnvm/{attr}
- * is read  /sys/class/nvme/{nvme_name}/{attr} otherwise.
- *
- * @returns 0 on success, -1 on error and errno set to indicate the error.
- */
-int nvm_be_sysfs_to_buf(const char *nvme_name, int nsid, const char *attr,
-			char *buf, int buf_len);
-
-/**
  * Fill out the geometry and other properties of the given device using the
- * given vadmin-backend function.
+ * idfy backend implementation
  */
-int nvm_be_populate(struct nvm_dev *dev,
-	int (*vadmin)(struct nvm_dev *, struct nvm_cmd *, struct nvm_ret *));
+int nvm_be_populate(struct nvm_dev *dev, struct nvm_be *be);
 
 /**
  * Fill out attribute values which are derived from those populated by
  * `nvm_be_populate` and determine device quirks
  */
 int nvm_be_populate_derived(struct nvm_dev *dev);
+
+/**
+ * Derives device quirks based on sysfs serial and device verid
+ *
+ * @param dev The device to determine quirks and assign quirk_mask
+ * @return 0 on success, 1 on error and errno set to indicate the error
+ */
+int nvm_be_populate_quirks(struct nvm_dev *dev, const char serial[]);
 
 extern struct nvm_be nvm_be_ioctl;
 extern struct nvm_be nvm_be_sysfs;
