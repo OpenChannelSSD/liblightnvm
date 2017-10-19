f0f42217f091e30736bc61e5ef2e0136d6eb99d0
Simon A. F. Lund <slund@cnexlabs.com>
2017-10-19 17:37:41 +0200

    tests: nvm_addr: conv: adjusted to changes in 'nvm_addr_check'

--- tests/test_addr_conv.c
+++ tests/test_addr_conv.c
@@ -1,100 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <unistd.h>
-#include <liblightnvm.h>
+#include "test_intf.c"
 
-#include <CUnit/Basic.h>
-
-// Parsed from CLI
-static char nvm_dev_path[NVM_DEV_PATH_LEN] = "/dev/nvme0n1";
-
-// Managed by setup/teardown and used by tests
-static struct nvm_dev *dev;
-static const struct nvm_geo *geo;
-
-size_t compare_buffers(char *expected, char *actual, size_t nbytes)
+static void conv(int func)
 {
-	size_t diff = 0;
+	size_t tsectr = 0;
+	
+	switch (nvm_dev_get_verid(dev)) {
+	case NVM_SPEC_VERID_12:
+		tsectr = geo->g.nchannels * geo->g.nluns * geo->g.nplanes *
+			geo->g.nblocks * geo->g.npages * geo->g.nsectors;
+		break;
+	
+	case NVM_SPEC_VERID_20:
+		tsectr = geo->l.npugrp * geo->l.npunit * geo->l.nchunk *
+			 geo->l.nsectr;
+		break;
 
-	for (size_t i = 0; i < nbytes; ++i) {
-		if (expected[i] != actual[i]) {
-			++diff;
-		}
+	default:
+		CU_FAIL("INVALID VERID");
+		return;
 	}
 
-	return diff;
-}
-
-int setup(void)
-{
-	dev = nvm_dev_open(nvm_dev_path);
-	if (!dev) {
-		perror("nvm_dev_open");
-		CU_ASSERT_PTR_NOT_NULL(dev);
-	}
-	geo = nvm_dev_get_geo(dev);
-
-	return 0;
-}
-
-int teardown(void)
-{
-	geo = NULL;
-	nvm_dev_close(dev);
-
-	return 0;
-}
-
-void _test_FMT_CONV(int func)
-{
-	size_t tsecs = geo->nchannels * geo->nluns * geo->nplanes *
-		       geo->nblocks * geo->npages * geo->nsectors;
-
-	for (size_t sec = 0; sec < tsecs; ++sec) {
-		struct nvm_addr expected;
-		struct nvm_addr actual;
+	for (size_t sectr = 0; sectr < tsectr; ++sectr) {
+		struct nvm_addr exp = { .val = 0 };
+		struct nvm_addr act = { .val = 0 };
 		uint64_t conv;
-		int res;
 
-		expected.ppa = 0;
-		expected.g.sec = sec % geo->nsectors;
-		expected.g.pg = (sec / geo->nsectors ) % geo->npages;
-		expected.g.blk = ((sec / geo->nsectors) / geo->npages ) % geo->nblocks;
-		expected.g.pl = (((sec / geo->nsectors) / geo->npages ) / geo->nblocks) % geo->nplanes;
-		expected.g.lun = ((((sec / geo->nsectors) / geo->npages ) / geo->nblocks) / geo->nplanes) % geo->nluns;
-		expected.g.ch = (((((sec / geo->nsectors) / geo->npages ) / geo->nblocks) / geo->nplanes) / geo->nluns) % geo->nchannels;
+		switch (nvm_dev_get_verid(dev)) {
+		case NVM_SPEC_VERID_12:
+			exp.g.sec = sectr % geo->nsectors;
+			exp.g.pg = (sectr / geo->nsectors ) % geo->npages;
+			exp.g.blk = ((sectr / geo->nsectors) / geo->npages ) % geo->nblocks;
+			exp.g.pl = (((sectr / geo->nsectors) / geo->npages ) / geo->nblocks) % geo->nplanes;
+			exp.g.lun = ((((sectr / geo->nsectors) / geo->npages ) / geo->nblocks) / geo->nplanes) % geo->nluns;
+			exp.g.ch = (((((sectr / geo->nsectors) / geo->npages ) / geo->nblocks) / geo->nplanes) / geo->nluns) % geo->nchannels;
+			break;
 
-		res = nvm_addr_check(expected, geo);
-		CU_ASSERT(!res)
+		case NVM_SPEC_VERID_20:
+			exp.l.sectr = sectr % geo->l.nsectr;
+			exp.l.chunk = (sectr / geo->l.nsectr ) % geo->l.nchunk;
+			exp.l.punit = ((sectr / geo->l.nsectr) / geo->l.nchunk ) % geo->l.npunit;
+			exp.l.pugrp = (((sectr / geo->l.nsectr) / geo->l.nchunk ) / geo->l.npunit) % geo->l.npugrp;
+			break;
+		}
+
+		CU_ASSERT(!nvm_addr_check(exp, dev));
 
 		switch (func) {
 		case 0:
-			conv = nvm_addr_gen2dev(dev, expected);
-			actual = nvm_addr_dev2gen(dev, conv);
+			conv = nvm_addr_gen2dev(dev, exp);
+			act = nvm_addr_dev2gen(dev, conv);
 			break;
 
 		case 1:
-			conv = nvm_addr_gen2off(dev, expected);
-			actual = nvm_addr_off2gen(dev, conv);
+			conv = nvm_addr_gen2off(dev, exp);
+			act = nvm_addr_off2gen(dev, conv);
 			break;
 
 		case 2:
-			conv = nvm_addr_gen2lba(dev, expected);
-			actual = nvm_addr_lba2gen(dev, conv);
+			conv = nvm_addr_gen2lba(dev, exp);
+			act = nvm_addr_lba2gen(dev, conv);
 			break;
 
 		case 3:
-			conv = nvm_addr_gen2dev(dev, expected);
+			conv = nvm_addr_gen2dev(dev, exp);
 			conv = nvm_addr_dev2lba(dev, conv);
-			actual = nvm_addr_lba2gen(dev, conv);
+			act = nvm_addr_lba2gen(dev, conv);
 			break;
 
 		case 4:
-			conv = nvm_addr_gen2dev(dev, expected);
+			conv = nvm_addr_gen2dev(dev, exp);
 			conv = nvm_addr_dev2off(dev, conv);
-			actual = nvm_addr_off2gen(dev, conv);
+			act = nvm_addr_off2gen(dev, conv);
 			break;
 
 		default:
@@ -102,94 +78,76 @@ void _test_FMT_CONV(int func)
 			return;
 		}
 
-		CU_ASSERT_EQUAL(actual.ppa, expected.ppa);
-		if (actual.ppa != expected.ppa) {
-			printf("Expected: "); nvm_addr_pr(expected);
-			printf("Got:      "); nvm_addr_pr(actual);
+		CU_ASSERT_EQUAL(act.val, exp.val);
+		if ((CU_BRM_VERBOSE == rmode) && (act.val != exp.val)) {
+			printf("Expected: "); nvm_addr_prn(&exp, 1, dev);
+			printf("Got:      "); nvm_addr_prn(&act, 1, dev);
 		}
 	}
 }
 
-/**
- * Tests: gen <-> dev
- */
 void test_FMT_GEN_DEV(void)
 {
-	_test_FMT_CONV(0);
+	conv(0);	///< gen -> dev -> gen
 }
 
-/**
- * Tests: gen <-> off
- */
 void test_FMT_GEN_OFF(void)
 {
-	_test_FMT_CONV(1);
+	conv(1);	///< gen -> off -> gen
 }
 
-/**
- * Tests: gen <-> LBA
- */
 void test_FMT_GEN_LBA(void)
 {
-	_test_FMT_CONV(2);
+	conv(2);	///< gen -> LBA -> gen
 }
 
-/**
- * Tests: gen <-> dev <-> LBA
- */
 void test_FMT_GEN_DEV_LBA(void)
 {
-	_test_FMT_CONV(3);
+	conv(3);	///< gen -> dev -> LBA -> gen
 }
 
-/**
- * Tests: gen <-> dev <-> off
- */
 void test_FMT_GEN_DEV_OFF(void)
 {
-	_test_FMT_CONV(4);
+	conv(4);	///< gen -> dev -> off -> gen
 }
 
 int main(int argc, char **argv)
 {
-	switch(argc) {
-	case 2:
-		if (strlen(argv[1]) > NVM_DEV_PATH_LEN) {
-			printf("ERR: len(dev_path) > %d characters\n",
-			       NVM_DEV_PATH_LEN);
-			return 1;
-                }
-		strncpy(nvm_dev_path, argv[1], NVM_DEV_PATH_LEN);
+	int err = 0;
+
+	CU_pSuite pSuite = suite_create("nvm_addr_*", argc, argv);
+	if (!pSuite)
+		goto out;
+
+	if (!CU_add_test(pSuite, "fmt gen -> dev -> gen", test_FMT_GEN_DEV))
+		goto out;
+	if (!CU_add_test(pSuite, "fmt gen -> lba -> gen", test_FMT_GEN_LBA))
+		goto out;
+	if (!CU_add_test(pSuite, "fmt gen -> off -> gen", test_FMT_GEN_OFF))
+		goto out;
+	if (!CU_add_test(pSuite, "fmt gen -> dev -> lba -> gen", test_FMT_GEN_DEV_LBA))
+		goto out;
+	if (!CU_add_test(pSuite, "fmt gen -> dev -> off -> gen", test_FMT_GEN_DEV_OFF))
+		goto out;
+
+	switch(rmode) {
+	case NVM_TEST_RMODE_AUTO:
+		CU_automated_run_tests();
+		break;
+
+	default:
+		CU_basic_set_mode(rmode);
+		CU_basic_run_tests();
 		break;
 	}
 
-	CU_pSuite pSuite = NULL;
+out:
+	err = CU_get_error() || \
+	      CU_get_number_of_suites_failed() || \
+	      CU_get_number_of_tests_failed() || \
+	      CU_get_number_of_failures();
 
-	if (CUE_SUCCESS != CU_initialize_registry())
-		return CU_get_error();
-
-	pSuite = CU_add_suite("nvm_addr_*", setup, teardown);
-	if (NULL == pSuite) {
-		CU_cleanup_registry();
-		return CU_get_error();
-	}
-
-	if (
-	(NULL == CU_add_test(pSuite, "fmt gen <-> dev", test_FMT_GEN_DEV)) ||
-	(NULL == CU_add_test(pSuite, "fmt gen <-> lba", test_FMT_GEN_LBA)) ||
-	(NULL == CU_add_test(pSuite, "fmt gen <-> off", test_FMT_GEN_OFF)) ||
-	(NULL == CU_add_test(pSuite, "fmt gen -> dev -> lba -> gen", test_FMT_GEN_DEV_LBA)) ||
-	(NULL == CU_add_test(pSuite, "fmt gen -> dev -> off -> gen", test_FMT_GEN_DEV_OFF)) ||
-	0)
-	{
-		CU_cleanup_registry();
-		return CU_get_error();
-	}
-
-	/* Run all tests using the CUnit Basic interface */
-	CU_basic_set_mode(CU_BRM_NORMAL);
-	CU_basic_run_tests();
 	CU_cleanup_registry();
 
-	return CU_get_error();
+	return err;
 }
