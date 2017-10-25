fa445aa2637f02b64f3665f02e182e4f36c1d179
Simon A. F. Lund <slund@cnexlabs.com>
2017-10-25 15:29:02 +0200

    nvm_vblk: adjusted to OCSSD2.0

--- src/nvm_vblk.c
+++ src/nvm_vblk.c
@@ -34,6 +34,7 @@
 #include <nvm_vblk.h>
 #include <nvm_omp.h>
 #include <nvm_utils.h>
+#include <nvm_debug.h>
 
 static inline int NVM_MIN(int x, int y) {
 	return x < y ? x : y;
@@ -44,6 +45,8 @@ struct nvm_vblk* nvm_vblk_alloc(struct nvm_dev *dev, struct nvm_addr addrs[],
 {
 	struct nvm_vblk *vblk;
 	const struct nvm_geo *geo;
+	const struct nvm_spec_lgeo *lgeo;
+	int verid;
 	
 	if (naddrs > 128) {
 		errno = EINVAL;
@@ -56,6 +59,12 @@ struct nvm_vblk* nvm_vblk_alloc(struct nvm_dev *dev, struct nvm_addr addrs[],
 		return NULL;
 	}
 
+	lgeo = nvm_dev_get_lgeo(dev);
+	if (!lgeo) {
+		errno = EINVAL;
+		return NULL;
+	}
+
 	vblk = malloc(sizeof(*vblk));
 	if (!vblk) {
 		errno = ENOMEM;
@@ -64,6 +73,7 @@ struct nvm_vblk* nvm_vblk_alloc(struct nvm_dev *dev, struct nvm_addr addrs[],
 
 	for (int i = 0; i < naddrs; ++i) {
 		if (nvm_addr_check(addrs[i], dev)) {
+			NVM_DEBUG("FAILED: nvm_addr_check");
 			errno = EINVAL;
 			free(vblk);
 			return NULL;
@@ -76,8 +86,25 @@ struct nvm_vblk* nvm_vblk_alloc(struct nvm_dev *dev, struct nvm_addr addrs[],
 	vblk->dev = dev;
 	vblk->pos_write = 0;
 	vblk->pos_read = 0;
-	vblk->nbytes = vblk->nblks * geo->nplanes * geo->npages *
-		       geo->nsectors * geo->sector_nbytes;
+
+	verid = nvm_dev_get_verid(dev);
+	switch (verid) {
+	case NVM_SPEC_VERID_12:
+	case NVM_SPEC_VERID_13:
+		vblk->nbytes = vblk->nblks * geo->nplanes * geo->npages *
+			       geo->nsectors * geo->sector_nbytes;
+		break;
+	
+	case NVM_SPEC_VERID_20:
+		vblk->nbytes = vblk->nblks * lgeo->nsectr * lgeo->nbytes;
+		break;
+
+	default:
+		NVM_DEBUG("FAILED: unsupported verid: %d", verid);
+		errno = ENOSYS;
+		free(vblk);
+		return NULL;
+	}
 
 	return vblk;
 }
@@ -86,25 +113,52 @@ struct nvm_vblk *nvm_vblk_alloc_line(struct nvm_dev *dev, int ch_bgn,
 				     int ch_end, int lun_bgn, int lun_end,
 				     int blk)
 {
-	struct nvm_vblk *vblk;
+	const int verid = nvm_dev_get_verid(dev);
 	const struct nvm_geo *geo = nvm_dev_get_geo(dev);
-	
+	const struct nvm_spec_lgeo *lgeo = nvm_dev_get_lgeo(dev);
+	struct nvm_vblk *vblk;
+
 	vblk = nvm_vblk_alloc(dev, NULL, 0);
 	if (!vblk)
 		return NULL;	// Propagate errno
 
-	for (int lun = lun_bgn; lun <= lun_end; ++lun) {
-		for (int ch = ch_bgn; ch <= ch_end; ++ch) {
-			vblk->blks[vblk->nblks].ppa = 0;
-			vblk->blks[vblk->nblks].g.ch = ch;
-			vblk->blks[vblk->nblks].g.lun = lun;
-			vblk->blks[vblk->nblks].g.blk = blk;
-			++(vblk->nblks);
+	switch (verid) {
+	case NVM_SPEC_VERID_12:
+	case NVM_SPEC_VERID_13:
+		for (int lun = lun_bgn; lun <= lun_end; ++lun) {
+			for (int ch = ch_bgn; ch <= ch_end; ++ch) {
+				vblk->blks[vblk->nblks].ppa = 0;
+				vblk->blks[vblk->nblks].g.ch = ch;
+				vblk->blks[vblk->nblks].g.lun = lun;
+				vblk->blks[vblk->nblks].g.blk = blk;
+				++(vblk->nblks);
+			}
 		}
-	}
 
-	vblk->nbytes = vblk->nblks * geo->nplanes * geo->npages *
-		       geo->nsectors * geo->sector_nbytes;
+		vblk->nbytes = vblk->nblks * geo->nplanes * geo->npages *
+			       geo->nsectors * geo->sector_nbytes;
+		break;
+
+	case NVM_SPEC_VERID_20:
+		for (int punit = lun_bgn; punit <= lun_end; ++punit) {
+			for (int pugrp = ch_bgn; pugrp <= ch_end; ++pugrp) {
+				vblk->blks[vblk->nblks].ppa = 0;
+				vblk->blks[vblk->nblks].l.pugrp = pugrp;
+				vblk->blks[vblk->nblks].l.punit = punit;
+				vblk->blks[vblk->nblks].l.chunk = blk;
+				++(vblk->nblks);
+			}
+		}
+
+		vblk->nbytes = vblk->nblks * lgeo->nsectr * lgeo->nbytes;
+		break;
+
+	default:
+		NVM_DEBUG("FAILED: unsupported verid: %d", verid);
+		nvm_buf_free(vblk);
+		errno = ENOSYS;
+		return NULL;
+	}
 
 	return vblk;
 }
@@ -114,24 +168,23 @@ void nvm_vblk_free(struct nvm_vblk *vblk)
 	free(vblk);
 }
 
-static inline int _cmd_nblks(int nblks, int cmd_nblks_max)
+static inline int cmd_nblks(int nblks, int cmd_nblks_max)
 {
 	int cmd_nblks = cmd_nblks_max;
 
-	while(nblks % cmd_nblks && cmd_nblks > 1) --cmd_nblks;
+	while (nblks % cmd_nblks && cmd_nblks > 1) --cmd_nblks;
 
 	return cmd_nblks;
 }
 
-ssize_t nvm_vblk_erase(struct nvm_vblk *vblk)
+static inline ssize_t vblk_erase_s12(struct nvm_vblk *vblk)
 {
 	size_t nerr = 0;
 	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
 
 	const int BLK_NADDRS = geo->nplanes;
-	const int CMD_NBLKS = _cmd_nblks(vblk->nblks,
+	const int CMD_NBLKS = cmd_nblks(vblk->nblks,
 				vblk->dev->erase_naddrs_max / BLK_NADDRS);
-	//const int NTHREADS = vblk->nblks < CMD_NBLKS ? 1 : vblk->nblks / CMD_NBLKS;
 	const int NTHREADS = 1;
 
 	#pragma omp parallel for num_threads(NTHREADS) schedule(static,1) reduction(+:nerr) ordered if (NTHREADS>1)
@@ -170,6 +223,61 @@ ssize_t nvm_vblk_erase(struct nvm_vblk *vblk)
 	return vblk->nbytes;
 }
 
+static inline ssize_t vblk_erase_s20(struct nvm_vblk *vblk)
+{
+	/*
+	const struct nvm_spec_lgeo *lgeo = nvm_dev_get_lgeo(vblk->dev);
+	size_t nerr = 0;
+
+	const int CMD_NBLKS = cmd_nblks(vblk->nblks, vblk->dev->erase_naddrs_max);
+
+	for (int off = 0; off < vblk->nblks; off += CMD_NBLKS) {
+		const int naddrs = NVM_MIN(CMD_NBLKS, vblk->nblks - off);
+		struct nvm_ret ret = {0,0};
+		ssize_t err;
+
+		struct nvm_addr addrs[naddrs];
+
+		for (int i = 0; i < naddrs; ++i)
+			addrs[i].ppa = vblk->blks[off + i].ppa;
+
+		if (nvm_addr_erase(vblk->dev, addrs, naddrs, 0, &ret))
+			++nerr;
+	}
+
+	if (nerr) {
+		errno = EIO;
+		return -1;
+	}
+
+	vblk->pos_write = 0;
+	vblk->pos_read = 0;
+
+	return vblk->nbytes;
+	*/
+
+	return -1;
+}
+
+ssize_t nvm_vblk_erase(struct nvm_vblk *vblk)
+{
+	const int verid = nvm_dev_get_verid(nvm_vblk_get_dev(vblk));
+
+	switch (verid) {
+	case NVM_SPEC_VERID_12:
+	case NVM_SPEC_VERID_13:
+		return vblk_erase_s12(vblk);
+
+	case NVM_SPEC_VERID_20:
+		return vblk_erase_s20(vblk);
+
+	default:
+		NVM_DEBUG("FAILED: unsupported verid: %d", verid);
+		errno = ENOSYS;
+		return -1;
+	}
+}
+
 static inline int _cmd_nspages(int nblks, int cmd_nspages_max)
 {
 	int cmd_nspages = cmd_nspages_max;
@@ -179,8 +287,8 @@ static inline int _cmd_nspages(int nblks, int cmd_nspages_max)
 	return cmd_nspages;
 }
 
-ssize_t nvm_vblk_pwrite(struct nvm_vblk *vblk, const void *buf, size_t count,
-			size_t offset)
+static inline ssize_t vblk_pwrite_s12(struct nvm_vblk *vblk, const void *buf,
+				      size_t count, size_t offset)
 {
 	size_t nerr = 0;
 	const int PMODE = nvm_dev_get_pmode(vblk->dev);
@@ -216,6 +324,7 @@ ssize_t nvm_vblk_pwrite(struct nvm_vblk *vblk, const void *buf, size_t count,
 
 		padding_buf = nvm_buf_alloc(geo, nbytes);
 		if (!padding_buf) {
+			NVM_DEBUG("FAILED: nvm_buf_alloc(padding)");
 			errno = ENOMEM;
 			return -1;
 		}
@@ -225,6 +334,7 @@ ssize_t nvm_vblk_pwrite(struct nvm_vblk *vblk, const void *buf, size_t count,
 	if (vblk->dev->meta_mode != NVM_META_MODE_NONE) {	// Meta
 		meta = nvm_buf_alloc(geo, meta_tbytes);		// Alloc buf
 		if (!meta) {
+			NVM_DEBUG("FAILED: nvm_buf_alloc(meta)");
 			errno = ENOMEM;
 			return -1;
 		}
@@ -288,6 +398,85 @@ ssize_t nvm_vblk_pwrite(struct nvm_vblk *vblk, const void *buf, size_t count,
 	return count;
 }
 
+#define WS_MIN 12
+
+static inline ssize_t vblk_pwrite_s20(struct nvm_vblk *vblk, const void *buf,
+				      size_t count, size_t offset)
+{
+	/*
+	size_t nerr = 0;
+	const struct nvm_spec_geo *lgeo = nvm_dev_get_lgeo(vblk->dev);
+	
+	size_t wun_nsectr = WS_MIN;
+	size_t wun_nbytes = wun_nsectr * lgeo->nbytes;
+
+	const size_t wun_bgn = offset / wun_nbytes;
+	const size_t wun_end = wun_bgn + (count / wun_nbytes);
+
+	const size_t cmd_nwun = vblk->dev->write_naddrs_max / wun_nsectr;
+
+	if (offset + count > vblk->nbytes) {
+		errno = EINVAL;
+		return -1;
+	}
+	if ((count % wun_nbytes) || (offset % wun_nbytes)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	// iterate over work units, increment with command
+	for (size_t wun_off = wun_bgn; wun_off < wun_end; wun_off += cmd_nwun) {
+		struct nvm_ret ret = {0, 0};
+
+		const int nwun = NVM_MIN(cmd_nwun, (int)(wun_end - wun_off));
+		const int naddrs = nwun * wun_nsectr;
+		struct nvm_addr addrs[naddrs];
+
+		for (size_t wun = wun_off; wun < wun_off + nwun; ++wun) {
+		
+
+		}
+
+		const char *buf_off;
+		buf_off = (const char*)buf + (off - bgn) * lgeo->nbytes;
+
+		const ssize_t err = nvm_addr_write(vblk->dev, addrs, naddrs,
+						   buf_off, NULL, 0x0, &ret);
+		if (err)
+			++nerr;
+	}
+
+	if (nerr) {
+		errno = EIO;
+		return -1;
+	}
+
+	return count;
+	*/
+
+	return -1;
+}
+
+ssize_t nvm_vblk_pwrite(struct nvm_vblk *vblk, const void *buf, size_t count,
+			size_t offset)
+{
+	const int verid = nvm_dev_get_verid(nvm_vblk_get_dev(vblk));
+
+	switch (verid) {
+	case NVM_SPEC_VERID_12:
+	case NVM_SPEC_VERID_13:
+		return vblk_pwrite_s12(vblk, buf, count, offset);
+
+	case NVM_SPEC_VERID_20:
+		return vblk_pwrite_s20(vblk, buf, count, offset);
+
+	default:
+		NVM_DEBUG("FAILED: unsupported verid: %d", verid);
+		errno = ENOSYS;
+		return -1;
+	}
+}
+
 ssize_t nvm_vblk_write(struct nvm_vblk *vblk, const void *buf, size_t count)
 {
 	ssize_t nbytes = nvm_vblk_pwrite(vblk, buf, count, vblk->pos_write);
@@ -305,8 +494,8 @@ ssize_t nvm_vblk_pad(struct nvm_vblk *vblk)
 	return nvm_vblk_write(vblk, NULL, vblk->nbytes - vblk->pos_write);
 }
 
-ssize_t nvm_vblk_pread(struct nvm_vblk *vblk, void *buf, size_t count,
-		       size_t offset)
+static inline ssize_t vblk_pread_s12(struct nvm_vblk *vblk, void *buf,
+				     size_t count, size_t offset)
 {
 	size_t nerr = 0;
 	const int PMODE = nvm_dev_get_pmode(vblk->dev);
@@ -372,6 +561,33 @@ ssize_t nvm_vblk_pread(struct nvm_vblk *vblk, void *buf, size_t count,
 	return count;
 }
 
+static inline ssize_t vblk_pread_s20(struct nvm_vblk *vblk, void *buf,
+				     size_t count, size_t offset)
+{
+	errno = EIO;
+	return -1;
+}
+
+ssize_t nvm_vblk_pread(struct nvm_vblk *vblk, void *buf, size_t count,
+		       size_t offset)
+{
+	const int verid = nvm_dev_get_verid(nvm_vblk_get_dev(vblk));
+
+	switch (verid) {
+	case NVM_SPEC_VERID_12:
+	case NVM_SPEC_VERID_13:
+		return vblk_pread_s12(vblk, buf, count, offset);
+
+	case NVM_SPEC_VERID_20:
+		return vblk_pread_s20(vblk, buf, count, offset);
+
+	default:
+		NVM_DEBUG("FAILED: unsupported verid: %d", verid);
+		errno = ENOSYS;
+		return -1;
+	}
+}
+
 ssize_t nvm_vblk_read(struct nvm_vblk *vblk, void *buf, size_t count)
 {
 	ssize_t nbytes = nvm_vblk_pread(vblk, buf, count, vblk->pos_read);
@@ -409,6 +625,14 @@ size_t nvm_vblk_get_pos_write(struct nvm_vblk *vblk)
 	return vblk->pos_write;
 }
 
+struct nvm_dev *nvm_vblk_get_dev(struct nvm_vblk *vblk)
+{
+	if (!vblk)
+		return NULL;
+
+	return vblk->dev;
+}
+
 int nvm_vblk_set_pos_read(struct nvm_vblk *vblk, size_t pos)
 {
 	if (pos > vblk->nbytes) {
@@ -441,7 +665,6 @@ void nvm_vblk_pr(struct nvm_vblk *vblk)
 	printf("  nmbytes: %zu\n", vblk->nbytes >> 20);
 	printf("  pos_write: %zu\n", vblk->pos_write);
 	printf("  pos_read: %zu\n", vblk->pos_read);
-	printf("  struct_nbytes: %zu\n", sizeof(*vblk));
         nvm_addr_prn(vblk->blks, vblk->nblks);
 }
 
