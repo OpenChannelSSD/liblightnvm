0598e6f7fefc4e719c67c0962ee163029ba0d37e
Simon A. F. Lund <slund@cnexlabs.com>
2017-12-06 13:40:09 +0100

    nvm_cmd: test: Added test for nvm_cmd_rprt

--- tests/test_cmd_rprt.c
+++ tests/test_cmd_rprt.c
@@ -38,9 +38,11 @@ void test_CMD_RPRT_PUNIT(void)
 {
 	struct nvm_addr punit_addr = { .val=0 };
 	struct nvm_addr chunk_addr = { .val=0 };
-	struct nvm_spec_rprt *rprt[10] = { NULL }; // Array of pointers
+	struct nvm_spec_rprt *rprt[10] = { NULL }; ///> Report pointers
 	int rprt_cur = 0;
 	struct nvm_vblk *vblk = NULL;
+	size_t buf_len = 0;
+	char *buf = NULL;
 	struct nvm_ret ret = { 0 };
 	ssize_t res = 0;
 
@@ -55,20 +57,23 @@ void test_CMD_RPRT_PUNIT(void)
 		goto out;
 
 	// Test that it reports the correct amount of chunks
-	assert(rprt[rprt_cur]->nchunks == geo->nchunk);
+	assert(rprt[rprt_cur]->ndescr == geo->nchunk);
 
-	// Get an arbitrary non-offline chunk in the punit
+	// Get an arbitrary free chunk in the punit
 	chunk_addr.val = punit_addr.val;
-	//for (size_t idx = geo->nchunk / 2; idx < geo->nchunk; ++idx) {
-	for (size_t idx = 0; idx < geo->nchunk; ++idx) {
-		if (rprt[rprt_cur]->descr[idx].chunk_state == NVM_RPRT_OFFLINE)
+	for (size_t idx = geo->nchunk / 2; idx < geo->nchunk; ++idx) {
+		if (rprt[rprt_cur]->descr[idx].chunk_state != NVM_RPRT_FREE)
 			continue;
 
 		chunk_addr.l.chunk = idx;
 		break;
 	}
 
-	// TODO: check the address...
+	// Test that the address found is valid
+	res = nvm_addr_check(chunk_addr, dev);
+	CU_ASSERT(!res);
+	if (res)
+		goto out;
 
 	// Create a vblk for the address
 	vblk = nvm_vblk_alloc(dev, &chunk_addr, 1);
@@ -76,39 +81,62 @@ void test_CMD_RPRT_PUNIT(void)
 	if (!vblk)
 		goto out;
 
+	// Write it partially and check that it changed state to OPEN and wp
+	{
+		buf_len = nvm_vblk_get_nbytes(vblk) / 2;
+		buf = nvm_buf_alloc(geo, buf_len);
+
+		res = nvm_vblk_write(vblk, buf, buf_len);
+		CU_ASSERT(res >= 0);
+		if (res < 0)
+			goto out;
+		
+		rprt[++rprt_cur] = nvm_cmd_rprt(dev, &punit_addr, 0x0, &ret);
+		CU_ASSERT_PTR_NOT_NULL(rprt[rprt_cur]);
+		if (!rprt[rprt_cur])
+			goto out;
+
+		CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_state == NVM_RPRT_OPEN);
+		CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_wptr == (buf_len / geo->nbytes));
+	}
+
+	// Write it fully and check that it changed state to CLOSED and wp
+	{
+		res = nvm_vblk_pad(vblk);
+		CU_ASSERT(res >= 0);
+		if (res < 0)
+			goto out;
+		
+		rprt[++rprt_cur] = nvm_cmd_rprt(dev, &punit_addr, 0x0, &ret);
+		CU_ASSERT_PTR_NOT_NULL(rprt[rprt_cur]);
+		if (!rprt[rprt_cur])
+			goto out;
+
+		CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_state == NVM_RPRT_CLOSED);
+		CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_wptr == geo->nsectr);
+	}
+
 	// Erase it and check that it is in state FREE
-	res = nvm_vblk_erase(vblk);
-	CU_ASSERT(res >= 0);
-	if (res < 0)
-		goto out;
+	{
+		res = nvm_vblk_erase(vblk);
+		CU_ASSERT(res >= 0);
+		if (res < 0)
+			goto out;
 
-	rprt[++rprt_cur] = nvm_cmd_rprt(dev, &punit_addr, 0x0, &ret);
-	CU_ASSERT_PTR_NOT_NULL(rprt[rprt_cur]);
-	if (!rprt[rprt_cur])
-		goto out;
+		rprt[++rprt_cur] = nvm_cmd_rprt(dev, &punit_addr, 0x0, &ret);
+		CU_ASSERT_PTR_NOT_NULL(rprt[rprt_cur]);
+		if (!rprt[rprt_cur])
+			goto out;
 
-	CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_state == NVM_RPRT_FREE);
-
-	nvm_addr_print(&chunk_addr, 1, dev);
-	nvm_vblk_pr(vblk);
-
-	// Write it and check that it changed state to closed
-	res = nvm_vblk_pad(vblk);
-	CU_ASSERT(res >= 0);
-	if (res < 0)
-		goto out;
-	
-	rprt[++rprt_cur] = nvm_cmd_rprt(dev, &punit_addr, 0x0, &ret);
-	CU_ASSERT_PTR_NOT_NULL(rprt[rprt_cur]);
-	if (!rprt[rprt_cur])
-		goto out;
-
-	CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_state == NVM_RPRT_CLOSED);
+		CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_state == NVM_RPRT_FREE);
+		CU_ASSERT(rprt[rprt_cur]->descr[chunk_addr.l.chunk].chunk_wptr == 0);
+	}
 
 out:
 	nvm_vblk_free(vblk);
 	for (int idx = 0; idx <= rprt_cur; ++idx)
 		nvm_buf_free(rprt[idx]);
+	nvm_buf_free(buf);
 }
 
 int main(int argc, char **argv)
