59eb72dccab8d00bebeee2548817c83a7e6d7261
Simon A. F. Lund <slund@cnexlabs.com>
2017-12-20 12:25:55 +0100

    nvm_cmd: added helpers nvm_cmd_{gbbt|rprt}_arbs

--- src/nvm_cmd.c
+++ src/nvm_cmd.c
@@ -36,6 +36,128 @@
 #include <nvm_utils.h>
 #include <nvm_debug.h>
 
+struct nvm_spec_idfy *nvm_cmd_idfy(struct nvm_dev *dev, struct nvm_ret *ret)
+{
+	return dev->be->idfy(dev, ret);
+}
+
+struct nvm_spec_rprt *nvm_cmd_rprt(struct nvm_dev *dev, struct nvm_addr *addr,
+				   int opt, struct nvm_ret *ret)
+{
+	return dev->be->rprt(dev, addr, opt, ret);
+}
+
+int nvm_cmd_rprt_arbs(struct nvm_dev *dev, int cs, int naddrs,
+		      struct nvm_addr *addrs)
+{
+	const struct nvm_geo *geo = nvm_dev_get_geo(dev);
+	int tpunit = geo->l.npugrp * geo->l.npunit;
+	const int arb = rand();
+
+	if ((naddrs < 0) || (naddrs > tpunit)) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	for (int idx = 0; idx < naddrs; ++idx) {
+		struct nvm_spec_rprt *rprt = NULL;
+		struct nvm_addr addr = { .val = 0 };
+		size_t cur = (idx + arb) % naddrs;
+		size_t des_idx;
+
+		addr.l.pugrp = cur % geo->l.npugrp;
+		addr.l.punit = (cur / geo->l.npugrp) % geo->l.npunit;
+
+		rprt = nvm_cmd_rprt(dev, &addr, 0x0, NULL);	// Grab RPRT
+		if (!(rprt && (rprt->ndescr == geo->l.nchunk))) {
+			free(rprt);
+			return -1;
+		}
+
+		for (des_idx = 0; des_idx < rprt->ndescr; ++des_idx) {
+			int des_cur = (des_idx + arb) % rprt->ndescr;
+			
+			if (rprt->descr[des_cur].chunk_state != cs)
+				continue;
+
+			addrs[idx].val = addr.val;
+			addrs[idx].l.chunk = des_cur;
+			break;
+		}
+
+		if (des_idx == rprt->ndescr) {			// No chunk !
+			free(rprt);
+			return -1;
+		}
+
+		free(rprt);
+	}
+
+	return 0;
+}
+
+struct nvm_spec_bbt *nvm_cmd_gbbt(struct nvm_dev *dev, struct nvm_addr addr,
+				  struct nvm_ret *ret)
+{
+	return dev->be->gbbt(dev, addr, ret);
+}
+
+int nvm_cmd_gbbt_arbs(struct nvm_dev *dev, int bs, int naddrs,
+		      struct nvm_addr *addrs)
+{
+	const struct nvm_geo *geo = nvm_dev_get_geo(dev);
+	int tluns = geo->g.nchannels * geo->g.nluns;
+	int arb = rand();
+
+	if ((naddrs < 0) || naddrs > tluns) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	for (size_t idx = 0; idx < (size_t)naddrs; ++idx) {
+		struct nvm_spec_bbt *bbt = NULL;
+		struct nvm_addr addr = { .val = 0 };
+		size_t cur = (idx + arb) % naddrs;
+		size_t blk_idx;
+
+		addr.g.ch = cur % geo->g.nchannels;
+		addr.g.lun = (cur / geo->g.nchannels) % geo->g.nluns;
+
+		bbt = nvm_cmd_gbbt(dev, addr, NULL);		// Grab BBT
+		if (!bbt)
+			return -1;
+
+		for (blk_idx = 0; idx < geo->g.nblocks; ++idx) {
+			size_t blk_cur = (blk_idx + arb) % geo->g.nblocks;
+			int state = 0;
+
+			// Accumulate plane state
+			for (uint32_t idx = blk_cur * geo->g.nplanes;
+			     idx < blk_cur * geo->g.nplanes + geo->g.nplanes;
+				++idx)
+				state |= bbt->blk[idx];
+
+			// Check state
+			if (bs != state)
+				continue;
+
+			addrs[idx].val = addr.val;
+			addrs[idx].g.blk = blk_cur;
+			break;
+		}
+		if (blk_idx == geo->g.nblocks)
+			return -1;				// No block!
+	}
+
+	return 0;
+}
+
+int nvm_cmd_sbbt(struct nvm_dev *dev, struct nvm_addr *addrs, int naddrs,
+		 uint16_t flags, struct nvm_ret *ret)
+{
+	return dev->be->sbbt(dev, addrs, naddrs, flags, ret);
+}
+
 int nvm_cmd_erase(struct nvm_dev *dev, struct nvm_addr addrs[], int naddrs,
 		  uint16_t flags, struct nvm_ret *ret)
 {
@@ -61,86 +183,5 @@ int nvm_cmd_copy(struct nvm_dev *dev, struct nvm_addr src[],
 		 struct nvm_ret *ret)
 {
 	return dev->be->copy(dev, src, dst, naddrs, flags, ret);
-}
-
-struct nvm_spec_idfy *nvm_cmd_idfy(struct nvm_dev *dev, struct nvm_ret *ret)
-{
-	return dev->be->idfy(dev, ret);
-}
-
-struct nvm_spec_rprt *nvm_cmd_rprt(struct nvm_dev *dev, struct nvm_addr *addr,
-				   int opt, struct nvm_ret *ret)
-{
-	return dev->be->rprt(dev, addr, opt, ret);
-}
-
-int nvm_cmd_rprt_arbc(struct nvm_dev *dev, int cs, struct nvm_addr *addr)
-{
-	struct nvm_spec_rprt *rprt = nvm_cmd_rprt(dev, NULL, 0x0, 0x0);
-
-	if (!rprt)
-		return -1;
-
-	for (size_t idx = rprt->ndescr / 2; idx < rprt->ndescr; ++idx) {
-		if (rprt->descr[idx].chunk_state != cs)
-			continue;
-	
-		*addr = nvm_addr_dev2gen(dev, rprt->descr[idx].chunk_addr);
-		free(rprt);
-		return 0;
-	}
-
-	free(rprt);
-	return -1;
-}
-
-struct nvm_spec_bbt *nvm_cmd_gbbt(struct nvm_dev *dev, struct nvm_addr addr,
-				  struct nvm_ret *ret)
-{
-	return dev->be->gbbt(dev, addr, ret);
-}
-
-int nvm_cmd_gbbt_arbc(struct nvm_dev *dev, int bs, struct nvm_addr *addr)
-{
-	const struct nvm_geo *geo = nvm_dev_get_geo(dev);
-	const size_t range = geo->g.nchannels * geo->g.nluns * geo->g.nblocks;
-
-	int arb = rand();
-
-	for (size_t i = 0; i < range; ++i) {
-		struct nvm_addr lun_addr = { .val = 0 };
-		size_t cur = (i + arb) % range;
-		struct nvm_spec_bbt *bbt = NULL;
-		int state = 0;
-
-		lun_addr.g.blk = cur % geo->g.nblocks;
-		lun_addr.g.lun = (cur / geo->g.nblocks) % geo->g.nluns;
-		lun_addr.g.ch = ((cur / geo->g.nblocks) / geo->g.nluns) \
-				 % geo->g.nchannels;
-
-		bbt = nvm_cmd_gbbt(dev, lun_addr, NULL);
-		if (!bbt)
-			return -1;
-
-		// Check the state
-		for (uint32_t idx = lun_addr.g.blk * geo->g.nplanes;
-		     idx < lun_addr.g.blk * geo->g.nplanes + geo->g.nplanes;
-			++idx) {
-			state |= bbt->blk[idx];
-		}
-
-		if (bs == state) {
-			addr->val = lun_addr.val;
-			return 0;
-		}
-	}
-
-	return -1;
-}
-
-int nvm_cmd_sbbt(struct nvm_dev *dev, struct nvm_addr *addrs, int naddrs,
-		 uint16_t flags, struct nvm_ret *ret)
-{
-	return dev->be->sbbt(dev, addrs, naddrs, flags, ret);
 }
 
