a497c092c4279e85bacb7051a1eb6fc93fa0c0c0
aravind <aravind.ramesh@wdc.com>
2018-07-16 13:26:00 +0200

    nvm_addr: cli: added terse output format

--- cli/cli_addr.c
+++ cli/cli_addr.c
@@ -8,6 +8,12 @@ static int cmd_fmt(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
 
+	if (cli->opts.terse) {
+		nvm_addr_pr((args->addrs[0]));
+		printf("\n");
+		return 0;
+	}
+
 	nvm_addr_prn(args->addrs, args->naddrs, args->dev);
 
 	return 0;
@@ -16,6 +22,14 @@ static int cmd_fmt(struct nvm_cli *cli)
 static int cmd_gen2dev(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
+
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->naddrs; ++i) {
+			printf("0x%016"PRIx64"\n",
+			       nvm_addr_gen2dev(args->dev, args->addrs[i]));
+		}
+		return 0;
+	}
 
 	for (int i = 0; i < args->naddrs; ++i) {
 		printf("- {gen: ");
@@ -31,6 +45,14 @@ static int cmd_gen2lba(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
 
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->naddrs; ++i) {
+			printf("%064"PRIu64"\n",
+			       nvm_addr_gen2lba(args->dev, args->addrs[i]));
+		}
+		return 0;
+	}
+
 	for (int i = 0; i < args->naddrs; ++i) {
 		printf("- {gen: ");
 		nvm_addr_pr(args->addrs[i]);
@@ -44,6 +66,14 @@ static int cmd_gen2lba(struct nvm_cli *cli)
 static int cmd_gen2off(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
+
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->naddrs; ++i) {
+			printf("%064"PRIu64"\n",
+			       nvm_addr_gen2off(args->dev, args->addrs[i]));
+		}
+		return 0;
+	}
 
 	for (int i = 0; i < args->naddrs; ++i) {
 		printf("- {gen: ");
@@ -59,6 +89,14 @@ static int cmd_gen2lpo(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
 
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->naddrs; ++i) {
+			printf("%064"PRIu64"\n",
+			       nvm_addr_gen2lpo(args->dev, args->addrs[i]));
+		}
+		return 0;
+	}
+
 	for (int i = 0; i < args->naddrs; ++i) {
 		printf("- {gen: ");
 		nvm_addr_pr(args->addrs[i]);
@@ -72,6 +110,16 @@ static int cmd_gen2lpo(struct nvm_cli *cli)
 static int cmd_dev2gen(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
+
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->nhex_vals; ++i) {
+			struct nvm_addr gen = { 0 };
+			gen = nvm_addr_dev2gen(args->dev, args->hex_vals[i]);
+			nvm_addr_pr(gen);
+			printf("\n");
+		}
+		return 0;
+	}
 
 	for (int i = 0; i < args->nhex_vals; ++i) {
 		struct nvm_addr gen = { 0 };
@@ -91,6 +139,16 @@ static int cmd_lba2gen(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
 
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->ndec_vals; ++i) {
+			struct nvm_addr gen = { 0 };
+			gen = nvm_addr_lba2gen(args->dev, args->dec_vals[i]);
+			nvm_addr_pr(gen);
+			printf("\n");
+		}
+		return 0;
+	}
+
 	for (int i = 0; i < args->ndec_vals; ++i) {
 		struct nvm_addr gen = { 0 };
 
@@ -109,6 +167,16 @@ static int cmd_off2gen(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
 
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->ndec_vals; ++i) {
+			struct nvm_addr gen = { 0 };
+			gen = nvm_addr_off2gen(args->dev, args->dec_vals[i]);
+			nvm_addr_pr(gen);
+			printf("\n");
+		}
+		return 0;
+	}
+
 	for (int i = 0; i < args->ndec_vals; ++i) {
 		struct nvm_addr gen = { 0 };
 
@@ -126,6 +194,16 @@ static int cmd_off2gen(struct nvm_cli *cli)
 static int cmd_lpo2gen(struct nvm_cli *cli)
 {
 	struct nvm_cli_cmd_args *args = &cli->args;
+
+	if (cli->opts.terse) {
+		for (int i = 0; i < args->ndec_vals; ++i) {
+			struct nvm_addr gen = { 0 };
+			gen = nvm_addr_lpo2gen(args->dev, args->dec_vals[i]);
+			nvm_addr_pr(gen);
+			printf("\n");
+		}
+		return 0;
+	}
 
 	for (int i = 0; i < args->ndec_vals; ++i) {
 		struct nvm_addr gen = { 0 };
@@ -150,20 +228,20 @@ static int cmd_lpo2gen(struct nvm_cli *cli)
 /* Define commands */
 static struct nvm_cli_cmd cmds[] = {
 
-	{"s12_to_gen",	cmd_fmt,	NVM_CLI_ARG_ADDR_S12, NVM_CLI_OPT_DEFAULT},
-	{"s20_to_gen",	cmd_fmt,	NVM_CLI_ARG_ADDR_S20, NVM_CLI_OPT_DEFAULT},
+	{"s12_to_gen",	cmd_fmt,	NVM_CLI_ARG_ADDR_S12, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
+	{"s20_to_gen",	cmd_fmt,	NVM_CLI_ARG_ADDR_S20, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
 
 	//{"from_hex",		cmd_fmt,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT},
 	
-	{"gen2dev",	cmd_gen2dev,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT},
-	{"gen2lba",	cmd_gen2lba,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT},
-	{"gen2off",	cmd_gen2off,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT},
-	{"gen2lpo",	cmd_gen2lpo,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT},
+	{"gen2dev",	cmd_gen2dev,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
+	{"gen2lba",	cmd_gen2lba,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
+	{"gen2off",	cmd_gen2off,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
+	{"gen2lpo",	cmd_gen2lpo,	NVM_CLI_ARG_ADDR_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
 
-	{"dev2gen",	cmd_dev2gen,	NVM_CLI_ARG_HEXVAL_LIST, NVM_CLI_OPT_DEFAULT},
-	{"lba2gen",	cmd_lba2gen,	NVM_CLI_ARG_DECVAL_LIST, NVM_CLI_OPT_DEFAULT},
-	{"off2gen",	cmd_off2gen,	NVM_CLI_ARG_DECVAL_LIST, NVM_CLI_OPT_DEFAULT},
-	{"lpo2gen",	cmd_lpo2gen,	NVM_CLI_ARG_DECVAL_LIST, NVM_CLI_OPT_DEFAULT},
+	{"dev2gen",	cmd_dev2gen,	NVM_CLI_ARG_HEXVAL_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
+	{"lba2gen",	cmd_lba2gen,	NVM_CLI_ARG_DECVAL_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
+	{"off2gen",	cmd_off2gen,	NVM_CLI_ARG_DECVAL_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
+	{"lpo2gen",	cmd_lpo2gen,	NVM_CLI_ARG_DECVAL_LIST, NVM_CLI_OPT_DEFAULT|NVM_CLI_OPT_TERSE},
 };
 
 /* Define the CLI */
