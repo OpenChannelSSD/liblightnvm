6892afbde19558d792221ea8a00974a85446bef2
Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>
2018-09-07 16:29:52 +0200

    nvm_be_ioctl: internal: Added get/set-feature

--- include/nvm_be_ioctl.h
+++ include/nvm_be_ioctl.h
@@ -183,6 +183,14 @@ struct nvm_spec_rprt *nvm_be_ioctl_rprt(struct nvm_dev *dev,
 					struct nvm_addr *addr, int opt,
 					struct nvm_ret *ret);
 
+int nvm_be_ioctl_gfeat(struct nvm_dev *dev, uint8_t id,
+		       union nvm_spec_feat *feat,
+		       struct nvm_ret *ret);
+
+int nvm_be_ioctl_sfeat(struct nvm_dev *dev, uint8_t id,
+		       const union nvm_spec_feat *feat,
+		       struct nvm_ret *ret);
+
 struct nvm_spec_bbt *nvm_be_ioctl_gbbt(struct nvm_dev *dev,
 				       struct nvm_addr addr,
 				       struct nvm_ret *ret);
