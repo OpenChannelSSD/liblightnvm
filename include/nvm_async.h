1c33445427f880637f5d2c10480d176ca3d44cdc
Simon A. F. Lund <slund@cnexlabs.com>
2018-09-13 16:24:43 +0200

    nvm_async: internal: Added internal header

---
+++ include/nvm_async.h
@@ -0,0 +1,40 @@
+/*
+ * nvm_async - Internal header for liblightnvm ASYNC CMD interface
+ *
+ * Copyright (C) Simon A. F. Lund <slund@cnexlabs.com>
+ * Copyright (C) Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __INTERNAL_NVM_ASYNC_H
+#define __INTERNAL_NVM_ASYNC_H
+
+struct nvm_async_ctx {
+	uint32_t depth;		///< IO depth of the ASYNC CTX
+	uint32_t outstanding;	///< Outstanding IO on the ASYNC CTX
+
+	// Lower-layer context, e.g. for the implementation of nvm_be_*_async_*
+	void *be_ctx;
+};
+
+#endif /* __INTERNAL_NVM_ASYNC_H */
