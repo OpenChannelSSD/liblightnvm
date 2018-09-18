5a5add45f2b8c9d2523dd334195ab5532a7eee8a
Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>
2018-09-18 13:25:32 +0200

    nvm_be_lbd: Added ASYNC CMD support for SCALAR write and read
    Signed-off-by: Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>

--- src/nvm_be_lbd.c
+++ src/nvm_be_lbd.c
@@ -4,6 +4,7 @@
  * Copyright (C) 2015-2017 Javier Gonzáles <javier@cnexlabs.com>
  * Copyright (C) 2015-2017 Matias Bjørling <matias@cnexlabs.com>
  * Copyright (C) 2015-2017 Simon A. F. Lund <slund@cnexlabs.com>
+ * Copyright (C) 2018 Klaus B. A. Jensen <klaus.jensen@cnexlabs.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -66,20 +67,172 @@ struct nvm_be nvm_be_lbd = {
 #include <fcntl.h>
 #include <errno.h>
 #include <linux/fs.h>
+#include <time.h>
 #include <sys/ioctl.h>
 #include <nvm_be_ioctl.h>
 #include <nvm_dev.h>
 #include <nvm_utils.h>
 #include <nvm_debug.h>
+#include <nvm_async.h>
+
+#ifdef HAVE_LIBAIO
+#include <libaio.h>
+#define NVM_BE_LBD_ASYNC_DEFAULT_IODEPTH 256
+
+typedef void (*io_prep_fn)(struct iocb *iocb, int fd, void *buf, size_t count, long long offset);
+
+struct nvm_be_lbd_async_state {
+	io_context_t aio_ctx;
+	struct io_event *aio_events;
+	struct iocb **iocbs;
+	uint32_t depth;
+};
+
+int nvm_be_lbd_async_rw(struct nvm_dev *dev, int naddrs, void *data,
+		        const off_t offset, struct nvm_ret *ret,
+			io_prep_fn prep_fn)
+{
+	struct nvm_async_ctx *ctx = ret->async.ctx;
+	struct nvm_be_lbd_async_state *state = ctx->be_ctx;
+
+	if (ctx->outstanding == state->depth) {
+		errno = EAGAIN;
+		return -1;
+	}
+
+	struct iocb *iocb = state->iocbs[ctx->outstanding++];
+
+	prep_fn(iocb, dev->fd, data, dev->geo.l.nbytes * naddrs, offset);
+	iocb->data = ret;
+
+	int r = io_submit(state->aio_ctx, 1, &iocb);
+	if (r < 0) {
+		errno = -r;
+		return -1;
+	}
+
+	return 0;
+}
+
+int nvm_be_lbd_async_getevents(struct nvm_async_ctx *ctx, unsigned int min,
+			       unsigned int max, struct timespec *timeout)
+{
+	struct nvm_be_lbd_async_state *state = ctx->be_ctx;
+
+	int r, nevents = 0;
+	while (ctx->outstanding) {
+		if (0 == (r = io_getevents(state->aio_ctx, min, max, state->aio_events, timeout))) {
+			break;
+		}
+
+		if (r < 0) return -r;
+
+		nevents += r;
+
+		for (int i = 0; i < r; i++) {
+			struct nvm_ret *ret = state->aio_events[i].data;
+
+			ret->async.cb(ret, ret->async.cb_arg);
+
+			state->iocbs[--(ctx->outstanding)] = state->aio_events[i].obj;
+		}
+	}
+
+	return nevents;
+}
+
+int nvm_be_lbd_async_poke(struct nvm_dev *NVM_UNUSED(dev), struct nvm_async_ctx *ctx, uint32_t max)
+{
+	struct nvm_be_lbd_async_state *state = ctx->be_ctx;
+
+	struct timespec timeout = { 0, 0 };
+	if (!max) {
+		max = state->depth;
+	}
+
+	return nvm_be_lbd_async_getevents(ctx, 0, max, &timeout);
+}
+
+
+int nvm_be_lbd_async_wait(struct nvm_dev *NVM_UNUSED(dev), struct nvm_async_ctx *ctx)
+{
+	struct nvm_be_lbd_async_state *state = ctx->be_ctx;
+
+	return nvm_be_lbd_async_getevents(ctx, ctx->outstanding, state->depth, NULL);
+}
+
+struct nvm_async_ctx *nvm_be_lbd_async_init(struct nvm_dev *NVM_UNUSED(dev), uint32_t depth, uint16_t NVM_UNUSED(flags))
+{
+	struct nvm_async_ctx *ctx = calloc(1, sizeof(*ctx));
+
+	struct nvm_be_lbd_async_state *state = calloc(1, sizeof(*state));
+
+	if (!depth) {
+		depth = NVM_BE_LBD_ASYNC_DEFAULT_IODEPTH;
+	}
+
+	state->depth = depth;
+	state->aio_events = calloc(depth, sizeof(struct io_event));
+	state->iocbs = calloc(depth, sizeof(struct iocb *));
+
+	for (unsigned int i = 0; i < depth; i++) {
+		state->iocbs[i] = calloc(1, sizeof(struct iocb));
+	}
+
+	ctx->be_ctx = state;
+
+	int err;
+	if (0 != (err = io_queue_init(depth, &state->aio_ctx))) {
+		errno = -err;
+		return NULL;
+	}
+
+	return ctx;
+}
+
+int nvm_be_lbd_async_term(struct nvm_dev *NVM_UNUSED(dev), struct nvm_async_ctx *ctx)
+{
+	struct nvm_be_lbd_async_state *state = ctx->be_ctx;
+
+	for (unsigned int i = 0; i < state->depth; i++) {
+		free(state->iocbs[i]);
+	}
+
+	free(state->aio_events);
+	free(state->iocbs);
+
+	int err;
+	if (0 != (err = io_queue_release(state->aio_ctx))) {
+		errno = -err;
+		return -1;
+	}
+
+	free(state);
+	free(ctx);
+
+	return 0;
+}
+#endif
 
 static int nvm_be_lbd_scalar_erase(struct nvm_dev *dev, struct nvm_addr addrs[],
-				   int naddrs, uint16_t NVM_UNUSED(flags),
+				   int naddrs, uint16_t flags,
 				   struct nvm_ret *NVM_UNUSED(ret))
 {
+#ifdef HAVE_LIBAIO
+	if (flags & NVM_CMD_ASYNC) {
+		NVM_DEBUG("FAILED: flag NVM_CMD_ASYNC not supported for lbd scalar erase");
+		errno = EINVAL;
+		return -1;
+	}
+#else
+	// silence gcc
+	(void)flags;
+#endif
+
 	for (int i = 0; i < naddrs; i++) {
 		uint64_t range[2];
 		int err;
-		
+
 		range[0] = nvm_addr_gen2off(dev, addrs[i]);
 		range[1] = dev->geo.l.nsectr << dev->ssw;
 
@@ -96,9 +249,8 @@ static int nvm_be_lbd_scalar_erase(struct nvm_dev *dev, struct nvm_addr addrs[],
 }
 
 int nvm_be_lbd_scalar_read(struct nvm_dev *dev, struct nvm_addr addr,
-			   int naddrs, void *data, void *meta,
-			   uint16_t NVM_UNUSED(flags),
-			   struct nvm_ret *NVM_UNUSED(ret))
+			   int naddrs, void *data, void *meta, uint16_t flags,
+			   struct nvm_ret *ret)
 {
 	const off_t offset = nvm_addr_gen2off(dev, addr);
 	ssize_t res;
@@ -108,6 +260,17 @@ int nvm_be_lbd_scalar_read(struct nvm_dev *dev, struct nvm_addr addr,
 		errno = ENOSYS;
 		return -1;
 	}
+
+#ifdef HAVE_LIBAIO
+	if (flags & NVM_CMD_ASYNC) {
+		return nvm_be_lbd_async_rw(dev, naddrs, data, offset,
+					   ret, io_prep_pread);
+	}
+#else
+	// silence gcc
+	(void)flags;
+	(void)ret;
+#endif
 
 	res = pread(dev->fd, data, dev->geo.l.nbytes * naddrs, offset);
 	if (res < 0) {
@@ -121,8 +284,7 @@ int nvm_be_lbd_scalar_read(struct nvm_dev *dev, struct nvm_addr addr,
 
 int nvm_be_lbd_scalar_write(struct nvm_dev *dev, struct nvm_addr addr,
 			    int naddrs, const void *data, const void *meta,
-			    uint16_t NVM_UNUSED(flags),
-			    struct nvm_ret *NVM_UNUSED(ret))
+			    uint16_t flags, struct nvm_ret *ret)
 {
 	const off_t offset = nvm_addr_gen2off(dev, addr);
 	ssize_t res;
@@ -132,6 +294,17 @@ int nvm_be_lbd_scalar_write(struct nvm_dev *dev, struct nvm_addr addr,
 		errno = ENOSYS;
 		return -1;
 	}
+
+#ifdef HAVE_LIBAIO
+	if (flags & NVM_CMD_ASYNC) {
+		return nvm_be_lbd_async_rw(dev, naddrs, (void*) data, offset,
+					   ret, io_prep_pwrite);
+	}
+#else
+	// silence gcc
+	(void)flags;
+	(void)ret;
+#endif
 
 	res = pwrite(dev->fd, data, dev->geo.l.nbytes * naddrs, offset);
 	if (res < 0) {
@@ -146,7 +319,7 @@ int nvm_be_lbd_scalar_write(struct nvm_dev *dev, struct nvm_addr addr,
 struct nvm_dev *nvm_be_lbd_open(const char *dev_path, int NVM_UNUSED(flags))
 {
 	struct nvm_dev *dev;
-	
+
 	dev = nvm_be_ioctl_open(dev_path, NVM_BE_IOCTL_WRITABLE);
 	if (!dev) {
 		NVM_DEBUG("FAILED: opening via IOCTL_WRITABLE");
@@ -180,9 +353,16 @@ struct nvm_be nvm_be_lbd = {
 	.vector_read = nvm_be_ioctl_vector_read,
 	.vector_copy = nvm_be_ioctl_vector_copy,
 
+#ifdef HAVE_LIBAIO
+	.async_init = nvm_be_lbd_async_init,
+	.async_term = nvm_be_lbd_async_term,
+	.async_poke = nvm_be_lbd_async_poke,
+	.async_wait = nvm_be_lbd_async_wait,
+#else
 	.async_init = nvm_be_nosys_async_init,
 	.async_term = nvm_be_nosys_async_term,
 	.async_poke = nvm_be_nosys_async_poke,
 	.async_wait = nvm_be_nosys_async_wait,
+#endif
 };
 #endif
