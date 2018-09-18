d99f96df72e2b05d95293b66f305d77c0ca6da67
Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>
2018-09-18 14:31:40 +0200

    nvm_cmd: Fixed errno of nvm_cmd_rprt_arbs
    nvm_cmd_rprt_arbs failed to set errno in some paths.

--- src/nvm_cmd.c
+++ src/nvm_cmd.c
@@ -69,6 +69,7 @@ int nvm_cmd_rprt_arbs(struct nvm_dev *dev, int cs, int naddrs,
 		rprt = nvm_cmd_rprt(dev, &addr, 0x0, NULL);	// Grab RPRT
 		if (!(rprt && (rprt->ndescr == geo->l.nchunk))) {
 			free(rprt);
+			errno = EINVAL;
 			return -1;
 		}
 
@@ -85,6 +86,7 @@ int nvm_cmd_rprt_arbs(struct nvm_dev *dev, int cs, int naddrs,
 
 		if (des_idx == rprt->ndescr) {			// No chunk !
 			free(rprt);
+			errno = EINVAL;
 			return -1;
 		}
 
