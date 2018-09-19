3821fcf798934c6764c70bbb3b56b5d1160b5981
Simon A. F. Lund <slund@cnexlabs.com>
2018-09-19 14:03:19 +0200

    tests: nvm_cmd_erase: Expanded and improved
    * Added tests of state changes and multiple addresses.
    
    The test did not check if the state change (i.e. CLOSED -> FREE) was
    correctly applied.
    
    The test now always gets a fresh report as the final step.
    
    Signed-off-by: Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>

--- tests/test_cmd_erase.c
+++ tests/test_cmd_erase.c
@@ -1,24 +1,26 @@
 #include "test_intf.c"
 
-static void erase_s20(int use_metadata, int erase_mode)
+static void erase_s20(int use_metadata, int erase_mode, int naddrs)
 {
 	struct nvm_ret ret;
-	struct nvm_addr chunk_addr = { .val = 0 };
+
+	struct nvm_spec_rprt *rprt = NULL;
+	struct nvm_spec_rprt_descr primes[naddrs];
+
+	struct nvm_addr chunk_addrs[naddrs];
+	struct nvm_addr lun_addr = { .val = 0 };
+
 	ssize_t res;
 
 	// get an abitrary free chunk
-	if (nvm_cmd_rprt_arbs(dev, NVM_CHUNK_STATE_FREE, 1, &chunk_addr)) {
+	if (nvm_cmd_rprt_arbs(dev, NVM_CHUNK_STATE_FREE, naddrs, chunk_addrs)) {
 		CU_FAIL("nvm_cmd_rprt_arbs");
 		return;
 	}
 
-	struct nvm_addr lun_addr = { .val = 0 };
-	struct nvm_spec_rprt_descr *descr = NULL;
-	struct nvm_spec_rprt *rprt = NULL;
-
-	if (use_metadata) {
-		lun_addr.l.pugrp = chunk_addr.l.pugrp;
-		lun_addr.l.punit = chunk_addr.l.punit;
+	for (int i = 0; i < naddrs; i++) {
+		lun_addr.l.pugrp = chunk_addrs[i].l.pugrp;
+		lun_addr.l.punit = chunk_addrs[i].l.punit;
 
 		// get report for all chunks in lun
 		if (NULL == (rprt = nvm_cmd_rprt(dev, &lun_addr, 0, NULL))) {
@@ -26,32 +28,95 @@ static void erase_s20(int use_metadata, int erase_mode)
 			return;
 		}
 
-		descr = nvm_buf_alloc(geo, sizeof(struct nvm_spec_rprt_descr));
+		primes[i] = rprt->descr[chunk_addrs[i].l.chunk];
+
+		CU_ASSERT(primes[i].cs == NVM_CHUNK_STATE_FREE);
 	}
 
+	struct nvm_vblk *vblk = nvm_vblk_alloc(dev, chunk_addrs, naddrs);
+	if (!vblk) {
+		CU_FAIL("FAILED: Allocating vblk");
+		return;
+	}
+	size_t nbytes = nvm_vblk_get_nbytes(vblk);
+
+	struct nvm_buf_set *bufs = nvm_buf_set_alloc(dev, nbytes, 0);
+	if (!bufs) {
+		CU_FAIL("FAILED: Allocating nvm_buf_set");
+		return;
+	}
+	nvm_buf_set_fill(bufs);
+
+	if (nvm_vblk_write(vblk, bufs->write, nbytes) < 0) {
+		CU_FAIL("FAILED: nvm_vblk_write");
+		return;
+	}
+
+	for (int i = 0; i < naddrs; i++) {
+		lun_addr.l.pugrp = chunk_addrs[i].l.pugrp;
+		lun_addr.l.punit = chunk_addrs[i].l.punit;
+
+		// get report for all chunks in lun
+		if (NULL == (rprt = nvm_cmd_rprt(dev, &lun_addr, 0, NULL))) {
+			CU_FAIL("nvm_cmd_rprt failed");
+			return;
+		}
+
+		primes[i] = rprt->descr[chunk_addrs[i].l.chunk];
+
+		CU_ASSERT(primes[i].cs == NVM_CHUNK_STATE_CLOSED);
+	}
+
+	struct nvm_spec_rprt_descr *updated = use_metadata ?
+		nvm_buf_alloc(geo, naddrs * sizeof(struct nvm_spec_rprt_descr)) : NULL;
+
 	// erase the chunk
-	res = nvm_cmd_erase(dev, &chunk_addr, 1, descr, erase_mode, &ret);
+	res = nvm_cmd_erase(dev, chunk_addrs, naddrs, updated, erase_mode, &ret);
 	if (res < 0) {
 		CU_FAIL("Erase failure");
 		return;
 	}
 
+	// verify the returned metadata
 	if (use_metadata) {
-		struct nvm_spec_rprt_descr orig = rprt->descr[chunk_addr.l.chunk];
-
-		CU_ASSERT(descr->cs == orig.cs);
-		CU_ASSERT(descr->ct == orig.ct);
-		CU_ASSERT(descr->wli >= orig.wli);
-		CU_ASSERT(descr->addr == orig.addr);
-		CU_ASSERT(descr->naddrs == orig.naddrs);
-		CU_ASSERT(descr->wp == 0);
-
-		nvm_buf_free(descr);
-		nvm_buf_free(rprt);
+		for (int i = 0; i < naddrs; i++) {
+			CU_ASSERT(updated[i].cs == NVM_CHUNK_STATE_FREE);
+			CU_ASSERT(updated[i].ct == primes[i].ct);
+			CU_ASSERT(updated[i].wli >= primes[i].wli);
+			CU_ASSERT(updated[i].addr == primes[i].addr);
+			CU_ASSERT(updated[i].naddrs == primes[i].naddrs);
+			CU_ASSERT(updated[i].wp == 0);
+		}
 	}
+
+	struct nvm_spec_rprt_descr *verify;
+
+	// verify with a fresh report
+	for (int i = 0; i < naddrs; i++) {
+		lun_addr.l.pugrp = chunk_addrs[i].l.pugrp;
+		lun_addr.l.punit = chunk_addrs[i].l.punit;
+
+		// get report for all chunks in lun
+		if (NULL == (rprt = nvm_cmd_rprt(dev, &lun_addr, 0, NULL))) {
+			CU_FAIL("nvm_cmd_rprt failed");
+			return;
+		}
+		verify = &rprt->descr[chunk_addrs[i].l.chunk];
+		CU_ASSERT(verify->cs == NVM_CHUNK_STATE_FREE);
+		CU_ASSERT(verify->ct == primes[i].ct);
+		CU_ASSERT(verify->wli >= primes[i].wli);
+		CU_ASSERT(verify->addr == primes[i].addr);
+		CU_ASSERT(verify->naddrs == primes[i].naddrs);
+		CU_ASSERT(verify->wp == 0);
+	}
+
+	nvm_buf_free(updated);
+	nvm_buf_free(rprt);
+
+	return;
 }
 
-void test_ERASE_VECTOR_S20_META1(void)
+void test_ERASE_VECTOR_S20_NADDRS_ONE_META(void)
 {
 	switch(nvm_dev_get_verid(dev)) {
 	case NVM_SPEC_VERID_12:
@@ -59,7 +124,7 @@ void test_ERASE_VECTOR_S20_META1(void)
 		break;
 
 	case NVM_SPEC_VERID_20:
-		erase_s20(1, NVM_CMD_VECTOR);
+		erase_s20(1, NVM_CMD_VECTOR, 1);
 		break;
 
 	default:
@@ -67,8 +132,7 @@ void test_ERASE_VECTOR_S20_META1(void)
 	}
 }
 
-
-void test_ERASE_VECTOR_S20_META0(void)
+void test_ERASE_VECTOR_S20_NADDRS_NPUNITS_META(void)
 {
 	switch(nvm_dev_get_verid(dev)) {
 	case NVM_SPEC_VERID_12:
@@ -76,7 +140,7 @@ void test_ERASE_VECTOR_S20_META0(void)
 		break;
 
 	case NVM_SPEC_VERID_20:
-		erase_s20(0, NVM_CMD_VECTOR);
+		erase_s20(1, NVM_CMD_VECTOR, geo->l.npunit);
 		break;
 
 	default:
@@ -84,7 +148,7 @@ void test_ERASE_VECTOR_S20_META0(void)
 	}
 }
 
-void test_ERASE_SCALAR_S20_NADDRS1(void)
+void test_ERASE_VECTOR_S20_NADDRS_ONE(void)
 {
 	switch(nvm_dev_get_verid(dev)) {
 	case NVM_SPEC_VERID_12:
@@ -92,7 +156,55 @@ void test_ERASE_SCALAR_S20_NADDRS1(void)
 		break;
 
 	case NVM_SPEC_VERID_20:
-		erase_s20(0, NVM_CMD_SCALAR);
+		erase_s20(0, NVM_CMD_VECTOR, 1);
+		break;
+
+	default:
+		CU_FAIL("invalid verid");
+	}
+}
+
+void test_ERASE_VECTOR_S20_NADDRS_NPUNITS(void)
+{
+	switch(nvm_dev_get_verid(dev)) {
+	case NVM_SPEC_VERID_12:
+		CU_PASS("nothing to test");
+		break;
+
+	case NVM_SPEC_VERID_20:
+		erase_s20(0, NVM_CMD_VECTOR, geo->l.npunit);
+		break;
+
+	default:
+		CU_FAIL("invalid verid");
+	}
+}
+
+void test_ERASE_SCALAR_S20_NADDRS_ONE(void)
+{
+	switch(nvm_dev_get_verid(dev)) {
+	case NVM_SPEC_VERID_12:
+		CU_PASS("nothing to test");
+		break;
+
+	case NVM_SPEC_VERID_20:
+		erase_s20(0, NVM_CMD_SCALAR, 1);
+		break;
+
+	default:
+		CU_FAIL("invalid verid");
+	}
+}
+
+void test_ERASE_SCALAR_S20_NADDRS_NPUNITS(void)
+{
+	switch(nvm_dev_get_verid(dev)) {
+	case NVM_SPEC_VERID_12:
+		CU_PASS("nothing to test");
+		break;
+
+	case NVM_SPEC_VERID_20:
+		erase_s20(0, NVM_CMD_SCALAR, geo->l.npunit);
 		break;
 
 	default:
@@ -109,14 +221,22 @@ int main(int argc, char **argv)
 	if (!pSuite)
 		goto out;
 
-	if (!CU_add_test(pSuite, "ERASE_VECTOR_S20_META1", test_ERASE_VECTOR_S20_META1))
+	if (!CU_add_test(pSuite, "ERASE_SCALAR_S20_NADDRS_ONE", test_ERASE_SCALAR_S20_NADDRS_ONE))
 		goto out;
-	if (!CU_add_test(pSuite, "ERASE_VECTOR_S20_META0", test_ERASE_VECTOR_S20_META0))
+	if (!CU_add_test(pSuite, "ERASE_SCALAR_S20_NADDRS_NPUNITS", test_ERASE_SCALAR_S20_NADDRS_NPUNITS))
 		goto out;
 
-	if (!CU_add_test(pSuite, "ERASE_SCALAR_S20_NADDRS1", test_ERASE_SCALAR_S20_NADDRS1))
+	if (!CU_add_test(pSuite, "ERASE_VECTOR_S20_NADDRS_ONE", test_ERASE_VECTOR_S20_NADDRS_ONE))
 		goto out;
-	/*
+	if (!CU_add_test(pSuite, "ERASE_VECTOR_S20_NADDRS_ONE_META", test_ERASE_VECTOR_S20_NADDRS_ONE_META))
+		goto out;
+
+	if (!CU_add_test(pSuite, "ERASE_VECTOR_S20_NADDRS_NPUNITS", test_ERASE_VECTOR_S20_NADDRS_NPUNITS))
+		goto out;
+	if (!CU_add_test(pSuite, "ERASE_VECTOR_S20_NADDRS_NPUNITS_META", test_ERASE_VECTOR_S20_NADDRS_NPUNITS_META))
+		goto out;
+
+/*
 	// Some arbitrary size in the range [1,MAX]
 	if (!CU_add_test(pSuite, "ERASE_SCALAR_S20_NADDRS_ARB", test_ERASE_SCALAR_S20_NADDRS_ARB))
 		goto out;
@@ -129,8 +249,7 @@ int main(int argc, char **argv)
 	// fail with EINVAL
 	if (!CU_add_test(pSuite, "ERASE_SCALAR_S20_NADDRS_BAD", test_ERASE_SCALAR_S20_NADDRS_BAD))
 		goto out;
-	*/
-
+*/
 	switch(rmode) {
 	case NVM_TEST_RMODE_AUTO:
 		CU_automated_run_tests();
