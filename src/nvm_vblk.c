c6c511626b42a89ede8a218d5e1fc31e3293d1a1
Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>
2018-09-22 15:49:43 +0200

    nvm_vblk: Added ASYNC and SCALAR CMD option and fixed stripe width
    * Ensure that WS_OPT is used as stripe size width consistently
    * Added handling of ASYNC and SCALAR CMD options
    
    Signed-off-by: Klaus Birkelund Abildgaard Jensen <klaus.jensen@cnexlabs.com>

--- src/nvm_vblk.c
+++ src/nvm_vblk.c
@@ -36,6 +36,51 @@
 
 #define NVM_VBLK_CMD_OPTS (NVM_CMD_SYNC | NVM_CMD_VECTOR | NVM_CMD_PRP)
 
+int nvm_vblk_set_async(struct nvm_vblk *vblk, uint32_t depth)
+{
+	vblk->flags &= ~NVM_CMD_SYNC;
+	vblk->flags |= NVM_CMD_ASYNC;
+
+	if (!vblk->async_ctx) {
+		if (NULL == (vblk->async_ctx = nvm_async_init(vblk->dev, depth, 0))) {
+			NVM_DEBUG("FAILED: nvm_async_init");
+			return -1;
+		}
+
+		if (depth == 0) {
+			depth = nvm_async_get_depth(vblk->async_ctx);
+		}
+
+		vblk->rets = calloc(depth, sizeof(struct nvm_ret *));
+		for (uint32_t i = 0; i < depth; i++)
+			vblk->rets[i] = calloc(1, sizeof(struct nvm_ret));
+
+	}
+
+	return 0;
+}
+
+int nvm_vblk_set_scalar(struct nvm_vblk *vblk)
+{
+	vblk->flags &= ~NVM_CMD_VECTOR;
+	vblk->flags |= NVM_CMD_SCALAR;
+
+	return 0;
+}
+
+static void vblk_async_callback(struct nvm_ret *ret, void *opaque)
+{
+	struct nvm_vblk_async_cb_state *state = opaque;
+	struct nvm_vblk *vblk = state->vblk;
+
+	if (ret->status) {
+		(*state->nerr)++;
+	}
+
+	memset(ret, 0, sizeof(*ret));
+	vblk->rets[--(vblk->retsp)] = ret;
+}
+
 struct nvm_vblk* nvm_vblk_alloc(struct nvm_dev *dev, struct nvm_addr addrs[],
 				int naddrs)
 {
@@ -53,7 +98,7 @@ struct nvm_vblk* nvm_vblk_alloc(struct nvm_dev *dev, struct nvm_addr addrs[],
 		return NULL;
 	}
 
-	vblk = malloc(sizeof(*vblk));
+	vblk = calloc(1, sizeof(*vblk));
 	if (!vblk) {
 		errno = ENOMEM;
 		return NULL;
@@ -74,6 +119,7 @@ struct nvm_vblk* nvm_vblk_alloc(struct nvm_dev *dev, struct nvm_addr addrs[],
 	vblk->dev = dev;
 	vblk->pos_write = 0;
 	vblk->pos_read = 0;
+	vblk->flags = NVM_VBLK_CMD_OPTS;
 
 	switch (nvm_dev_get_verid(dev)) {
 	case NVM_SPEC_VERID_12:
@@ -272,6 +318,433 @@ static inline int _cmd_nspages(int nblks, int cmd_nspages_max)
 	return cmd_nspages;
 }
 
+static inline int _vblk_async_greedy_reap(struct nvm_vblk *vblk)
+{
+	int r, nevents = 0;
+
+	// spin until something can be reaped
+	do {
+		if (-1 == (nevents = nvm_async_poke(vblk->dev, vblk->async_ctx, 0)))
+			return -1;
+	} while (nevents == 0);
+
+	// reap until empty
+	do {
+		if (-1 == (r = nvm_async_poke(vblk->dev, vblk->async_ctx, 0)))
+			return -1;
+
+		nevents += r;
+	} while (r != 0);
+
+	return nevents;
+}
+
+static inline int vblk_io_async(struct nvm_vblk *vblk, const size_t vsectr_bgn,
+	const size_t count, void *buf, void *meta_buf, char *pad_buf,
+	int write)
+{
+	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
+
+	const size_t sectr_nbytes = geo->l.nbytes;
+	const size_t nsectrs = count / sectr_nbytes;
+	const size_t stripe_nsectrs = nvm_dev_get_ws_opt(vblk->dev);
+	const size_t nstripes = nsectrs / stripe_nsectrs;
+
+	int err;
+	uint64_t nerr = 0;
+
+	struct nvm_vblk_async_cb_state state = {
+		.nerr = &nerr,
+		.vblk = vblk,
+	};
+
+	size_t cnk_bgn = (vsectr_bgn / stripe_nsectrs) % vblk->nblks;
+	NVM_DEBUG("cnk_bgn: %ld", cnk_bgn);
+	for (size_t stripe = 0; stripe < nstripes; stripe++) {
+		size_t cnk_idx = (cnk_bgn + stripe) % vblk->nblks;
+		size_t cnk_off = (stripe / vblk->nblks) * stripe_nsectrs;
+
+		char *bufp = pad_buf ? pad_buf :
+			(char *)buf + (sectr_nbytes * stripe_nsectrs * stripe);
+
+		struct nvm_addr addrs[stripe_nsectrs];
+		for (size_t i = 0; i < stripe_nsectrs; i++) {
+			addrs[i].val = vblk->blks[cnk_idx].val;
+			addrs[i].l.sectr = cnk_off + i;
+		}
+
+		// this basically makes sure we never hit an EAGAIN below in
+		// the nvm_cmd_read/write call.
+		if (vblk->retsp == (nvm_async_get_depth(vblk->async_ctx) - 1)) {
+			if (_vblk_async_greedy_reap(vblk) < 0) {
+				NVM_DEBUG("FAILED: _vblk_async_greedy_reap: %d", errno);
+				return -1;
+			}
+		}
+
+		struct nvm_ret *ret = vblk->rets[vblk->retsp++];
+		if (!ret) {
+			NVM_DEBUG("should not happen; retsp = %d", vblk->retsp);
+			errno = ENOMEM;
+			return -1;
+		}
+
+		ret->async.ctx = vblk->async_ctx;
+		ret->async.cb = vblk_async_callback;
+		ret->async.cb_arg = &state;
+
+		while(1) {
+			err = write ?
+				nvm_cmd_write(vblk->dev, addrs, stripe_nsectrs,
+					      bufp, meta_buf, vblk->flags,
+					      ret) :
+				nvm_cmd_read(vblk->dev, addrs, stripe_nsectrs,
+					     bufp, meta_buf, vblk->flags,
+					     ret);
+
+			if (err < 0) {
+				if (errno == EAGAIN) {
+					if (_vblk_async_greedy_reap(vblk) < 0) {
+						NVM_DEBUG("FAILED: _vblk_async_greedy_reap: %d", errno);
+						return -1;
+					}
+
+					continue;
+				}
+
+				// propagate errno
+				return -1;
+			}
+
+			break;
+		}
+
+
+		if (((stripe + 1) % vblk->nblks) == 0) {
+			if (nvm_async_wait(vblk->dev, vblk->async_ctx) < 0) {
+				NVM_DEBUG("FAILED: nvm_async_wait");
+				return -1;
+			}
+		}
+	}
+
+	err = nvm_async_wait(vblk->dev, vblk->async_ctx);
+	if (err < 0) {
+		return -1;
+	}
+
+	return nerr;
+}
+
+static inline ssize_t vblk_async_pwrite_s20(struct nvm_vblk *vblk,
+					    const void *buf,
+					    size_t count, size_t offset)
+{
+	size_t nerr = 0;
+
+	const uint32_t WS_OPT = nvm_dev_get_ws_opt(vblk->dev);
+
+	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
+
+	const size_t sectr_nbytes = geo->l.nbytes;
+	const size_t nsectr = count / sectr_nbytes;
+
+	const size_t vsectr_bgn = offset / sectr_nbytes;
+
+	const size_t cmd_nsectr = WS_OPT;
+
+	const size_t meta_tbytes = cmd_nsectr * geo->l.nbytes_oob;
+	char *meta_buf = NULL;
+
+	const size_t pad_nbytes = cmd_nsectr * nsectr * geo->l.nbytes;
+	char *pad_buf = NULL;
+
+	const int meta_mode = nvm_dev_get_meta_mode(vblk->dev);
+
+	if (nsectr % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned nsectr: %zu", nsectr);
+		errno = EINVAL;
+		return -1;
+	}
+	if (vsectr_bgn % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned vsectr_bgn: %zu", vsectr_bgn);
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (!buf) {	// Allocate and use a padding buffer
+		pad_buf = nvm_buf_alloc(geo, pad_nbytes);
+		if (!pad_buf) {
+			NVM_DEBUG("FAILED: nvm_buf_alloc(pad)");
+			errno = ENOMEM;
+			return -1;
+		}
+
+		nvm_buf_fill(pad_buf, pad_nbytes);
+	}
+
+	if (meta_mode != NVM_META_MODE_NONE) {	// Meta
+		meta_buf = nvm_buf_alloc(geo, meta_tbytes);	// Alloc buf
+		if (!meta_buf) {
+			nvm_buf_free(pad_buf);
+			NVM_DEBUG("FAILED: nvm_buf_alloc(meta)");
+			errno = ENOMEM;
+			return -1;
+		}
+
+		switch(meta_mode) {			// Fill it
+			case NVM_META_MODE_ALPHA:
+				nvm_buf_fill(meta_buf, meta_tbytes);
+				break;
+			case NVM_META_MODE_CONST:
+				for (size_t i = 0; i < meta_tbytes; ++i)
+					meta_buf[i] = 65 + (meta_tbytes % 20);
+				break;
+			case NVM_META_MODE_NONE:
+				break;
+		}
+	}
+
+	nerr = vblk_io_async(vblk, vsectr_bgn, count, (void *) buf, meta_buf,
+			     pad_buf, 1 /* write */);
+
+	nvm_buf_free(pad_buf);
+	nvm_buf_free(meta_buf);
+
+	if (nerr) {
+		NVM_DEBUG("FAILED: nvm_cmd_write, nerr(%zu)", nerr);
+		errno = EIO;
+		return -1;
+	}
+
+	return count;
+}
+
+static inline ssize_t vblk_async_pread_s20(struct nvm_vblk *vblk, void *buf,
+					   size_t count, size_t offset)
+{
+	size_t nerr = 0;
+
+	const uint32_t WS_OPT = nvm_dev_get_ws_opt(vblk->dev);
+
+	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
+
+	const size_t sectr_nbytes = geo->l.nbytes;
+	const size_t nsectr = count / sectr_nbytes;
+
+	const size_t vsectr_bgn = offset / sectr_nbytes;
+
+	if (nsectr % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned nsectr: %zu", nsectr);
+		errno = EINVAL;
+		return -1;
+	}
+	if (vsectr_bgn % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned vsectr_bgn: %zu", vsectr_bgn);
+		errno = EINVAL;
+		return -1;
+	}
+
+	nerr = vblk_io_async(vblk, vsectr_bgn, count, buf, NULL, NULL,
+			     0 /* write */);
+	if (nerr) {
+		NVM_DEBUG("FAILED: nvm_cmd_read, nerr(%zu)", nerr);
+		errno = EIO;
+		return -1;
+	}
+
+	return count;
+}
+
+static inline ssize_t vblk_sync_pread_s20(struct nvm_vblk *vblk, void *buf,
+					  size_t count, size_t offset)
+{
+	size_t nerr = 0;
+
+	const uint32_t WS_OPT = nvm_dev_get_ws_opt(vblk->dev);
+
+	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
+	const size_t nchunks = vblk->nblks;
+
+	const size_t sectr_nbytes = geo->l.nbytes;
+	const size_t nsectr = count / sectr_nbytes;
+
+	const size_t sectr_bgn = offset / sectr_nbytes;
+	const size_t sectr_end = sectr_bgn + (count / sectr_nbytes) - 1;
+
+	const size_t cmd_nsectr = vblk->flags & NVM_CMD_VECTOR ? NVM_NADDR_MAX : WS_OPT;
+
+	const int NTHREADS = NVM_MIN(nchunks, nsectr / WS_OPT);
+
+	if (nsectr % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned nsectr: %zu", nsectr);
+		errno = EINVAL;
+		return -1;
+	}
+	if (sectr_bgn % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned sectr_bgn: %zu", sectr_bgn);
+		errno = EINVAL;
+		return -1;
+	}
+
+	const int VBLK_FLAGS = vblk->flags;
+
+	#pragma omp parallel for num_threads(NTHREADS) schedule(static,1) reduction(+:nerr) ordered if(NTHREADS>1)
+	for (size_t sectr_ofz = sectr_bgn; sectr_ofz <= sectr_end; sectr_ofz += cmd_nsectr) {
+		struct nvm_addr addrs[cmd_nsectr];
+		char *buf_off = (char*)buf + (sectr_ofz - sectr_bgn) * sectr_nbytes;
+
+		for (size_t idx = 0; idx < cmd_nsectr; ++idx) {
+			const size_t sectr = sectr_ofz + idx;
+			const size_t wunit = sectr / WS_OPT;
+			const size_t rnd = wunit / nchunks;
+
+			const size_t chunk = wunit % nchunks;
+			const size_t chunk_sectr = sectr % WS_OPT + rnd * WS_OPT;
+
+			addrs[idx].val = vblk->blks[chunk].val;
+			addrs[idx].l.sectr = chunk_sectr;
+
+			if (VBLK_FLAGS & NVM_CMD_SCALAR) break;
+		}
+
+		const ssize_t err = nvm_cmd_read(vblk->dev, addrs, cmd_nsectr,
+						 buf_off, NULL,
+						 VBLK_FLAGS, NULL);
+		if (err)
+			++nerr;
+	}
+
+	if (nerr) {
+		NVM_DEBUG("FAILED: nvm_cmd_read, nerr(%zu)", nerr);
+		errno = EIO;
+		return -1;
+	}
+
+	return count;
+}
+
+static inline ssize_t vblk_sync_pwrite_s20(struct nvm_vblk *vblk,
+					   const void *buf, size_t count,
+					   size_t offset)
+{
+	size_t nerr = 0;
+
+	const uint32_t WS_OPT = nvm_dev_get_ws_opt(vblk->dev);
+
+	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
+	const size_t nchunks = vblk->nblks;
+
+	const size_t sectr_nbytes = geo->l.nbytes;
+	const size_t nsectr = count / sectr_nbytes;
+
+	const size_t sectr_bgn = offset / sectr_nbytes;
+	const size_t sectr_end = sectr_bgn + (count / sectr_nbytes) - 1;
+
+	const size_t cmd_nsectr = WS_OPT;
+
+	const size_t meta_tbytes = cmd_nsectr * geo->l.nbytes_oob;
+	char *meta_buf = NULL;
+
+	const size_t pad_nbytes = cmd_nsectr * nsectr * geo->l.nbytes;
+	char *pad_buf = NULL;
+
+	const int NTHREADS = NVM_MIN(nchunks, nsectr / WS_OPT);
+
+	const int meta_mode = nvm_dev_get_meta_mode(vblk->dev);
+
+	if (nsectr % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned nsectr: %zu", nsectr);
+		errno = EINVAL;
+		return -1;
+	}
+	if (sectr_bgn % WS_OPT) {
+		NVM_DEBUG("FAILED: unaligned sectr_bgn: %zu", sectr_bgn);
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (!buf) {	// Allocate and use a padding buffer
+		pad_buf = nvm_buf_alloc(geo, pad_nbytes);
+		if (!pad_buf) {
+			NVM_DEBUG("FAILED: nvm_buf_alloc(pad)");
+			errno = ENOMEM;
+			return -1;
+		}
+
+		nvm_buf_fill(pad_buf, pad_nbytes);
+	}
+
+	if (meta_mode != NVM_META_MODE_NONE) {	// Meta
+		meta_buf = nvm_buf_alloc(geo, meta_tbytes);	// Alloc buf
+		if (!meta_buf) {
+			nvm_buf_free(pad_buf);
+			NVM_DEBUG("FAILED: nvm_buf_alloc(meta)");
+			errno = ENOMEM;
+			return -1;
+		}
+
+		switch(meta_mode) {			// Fill it
+			case NVM_META_MODE_ALPHA:
+				nvm_buf_fill(meta_buf, meta_tbytes);
+				break;
+			case NVM_META_MODE_CONST:
+				for (size_t i = 0; i < meta_tbytes; ++i)
+					meta_buf[i] = 65 + (meta_tbytes % 20);
+				break;
+			case NVM_META_MODE_NONE:
+				break;
+		}
+	}
+
+	const int VBLK_FLAGS = vblk->flags;
+
+	#pragma omp parallel for num_threads(NTHREADS) schedule(static,1) reduction(+:nerr) ordered if(NTHREADS>1)
+	for (size_t sectr_ofz = sectr_bgn; sectr_ofz <= sectr_end; sectr_ofz += cmd_nsectr) {
+		struct nvm_ret ret = { 0 };
+
+		struct nvm_addr addrs[cmd_nsectr];
+		char *buf_off;
+
+		if (pad_buf)
+			buf_off = pad_buf;
+		else
+			buf_off = (char*)buf + (sectr_ofz - sectr_bgn) * sectr_nbytes;
+
+		for (size_t idx = 0; idx < cmd_nsectr; ++idx) {
+			const size_t sectr = sectr_ofz + idx;
+			const size_t wunit = sectr / WS_OPT;
+			const size_t rnd = wunit / nchunks;
+
+			const size_t chunk = wunit % nchunks;
+			const size_t chunk_sectr = sectr % WS_OPT + rnd * WS_OPT;
+
+			addrs[idx].ppa = vblk->blks[chunk].ppa;
+			addrs[idx].l.sectr = chunk_sectr;
+		}
+
+		const ssize_t err = nvm_cmd_write(vblk->dev, addrs, cmd_nsectr,
+						  buf_off, meta_buf,
+						  VBLK_FLAGS, &ret);
+		if (err)
+			++nerr;
+
+		#pragma omp ordered
+		{}
+	}
+
+	nvm_buf_free(pad_buf);
+	nvm_buf_free(meta_buf);
+
+	if (nerr) {
+		NVM_DEBUG("FAILED: nvm_cmd_write, nerr(%zu)", nerr);
+		errno = EIO;
+		return -1;
+	}
+
+	return count;
+}
+
 static inline ssize_t vblk_pwrite_s12(struct nvm_vblk *vblk, const void *buf,
 				      size_t count, size_t offset)
 {
@@ -385,123 +858,6 @@ static inline ssize_t vblk_pwrite_s12(struct nvm_vblk *vblk, const void *buf,
 	return count;
 }
 
-static inline ssize_t vblk_pwrite_s20(struct nvm_vblk *vblk, const void *buf,
-				      size_t count, size_t offset)
-{
-	size_t nerr = 0;
-
-	const uint32_t WS_OPT = nvm_dev_get_ws_min(vblk->dev);
-
-	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
-	const size_t nchunks = vblk->nblks;
-
-	const size_t sectr_nbytes = geo->l.nbytes;
-	const size_t nsectr = count / sectr_nbytes;
-
-	const size_t sectr_bgn = offset / sectr_nbytes;
-	const size_t sectr_end = sectr_bgn + (count / sectr_nbytes) - 1;
-
-	const size_t cmd_nsectr = WS_OPT;
-
-	const size_t meta_tbytes = cmd_nsectr * geo->l.nbytes_oob;
-	char *meta_buf = NULL;
-
-	const size_t pad_nbytes = cmd_nsectr * nsectr * geo->l.nbytes;
-	char *pad_buf = NULL;
-
-	const int NTHREADS = NVM_MIN(nchunks, nsectr / WS_OPT);
-
-	const int meta_mode = nvm_dev_get_meta_mode(vblk->dev);
-
-	if (nsectr % WS_OPT) {
-		NVM_DEBUG("FAILED: unaligned nsectr: %zu", nsectr);
-		errno = EINVAL;
-		return -1;
-	}
-	if (sectr_bgn % WS_OPT) {
-		NVM_DEBUG("FAILED: unaligned sectr_bgn: %zu", sectr_bgn);
-		errno = EINVAL;
-		return -1;
-	}
-
-	if (!buf) {	// Allocate and use a padding buffer
-		pad_buf = nvm_buf_alloc(geo, pad_nbytes);
-		if (!pad_buf) {
-			NVM_DEBUG("FAILED: nvm_buf_alloc(pad)");
-			errno = ENOMEM;
-			return -1;
-		}
-
-		nvm_buf_fill(pad_buf, pad_nbytes);
-	}
-
-	if (meta_mode != NVM_META_MODE_NONE) {	// Meta
-		meta_buf = nvm_buf_alloc(geo, meta_tbytes);	// Alloc buf
-		if (!meta_buf) {
-			nvm_buf_free(pad_buf);
-			NVM_DEBUG("FAILED: nvm_buf_alloc(meta)");
-			errno = ENOMEM;
-			return -1;
-		}
-
-		switch(meta_mode) {			// Fill it
-			case NVM_META_MODE_ALPHA:
-				nvm_buf_fill(meta_buf, meta_tbytes);
-				break;
-			case NVM_META_MODE_CONST:
-				for (size_t i = 0; i < meta_tbytes; ++i)
-					meta_buf[i] = 65 + (meta_tbytes % 20);
-				break;
-			case NVM_META_MODE_NONE:
-				break;
-		}
-	}
-
-	#pragma omp parallel for num_threads(NTHREADS) schedule(static,1) reduction(+:nerr) ordered if(NTHREADS>1)
-	for (size_t sectr_ofz = sectr_bgn; sectr_ofz <= sectr_end; sectr_ofz += cmd_nsectr) {
-		struct nvm_ret ret = { 0 };
-
-		struct nvm_addr addrs[cmd_nsectr];
-		char *buf_off;
-
-		if (pad_buf)
-			buf_off = pad_buf;
-		else
-			buf_off = (char*)buf + (sectr_ofz - sectr_bgn) * sectr_nbytes;
-
-		for (size_t idx = 0; idx < cmd_nsectr; ++idx) {
-			const size_t sectr = sectr_ofz + idx;
-			const size_t wunit = sectr / WS_OPT;
-			const size_t rnd = wunit / nchunks;
-
-			const size_t chunk = wunit % nchunks;
-			const size_t chunk_sectr = sectr % WS_OPT + rnd * WS_OPT;
-
-			addrs[idx].ppa = vblk->blks[chunk].ppa;
-			addrs[idx].l.sectr = chunk_sectr;
-		}
-
-		const ssize_t err = nvm_cmd_write(vblk->dev, addrs, cmd_nsectr,
-						  buf_off, meta_buf,
-						  NVM_VBLK_CMD_OPTS, &ret);
-		if (err)
-			++nerr;
-
-		#pragma omp ordered
-		{}
-	}
-
-	nvm_buf_free(pad_buf);
-	nvm_buf_free(meta_buf);
-
-	if (nerr) {
-		NVM_DEBUG("FAILED: nvm_cmd_write, nerr(%zu)", nerr);
-		errno = EIO;
-		return -1;
-	}
-
-	return count;
-}
 
 ssize_t nvm_vblk_pwrite(struct nvm_vblk *vblk, const void *buf, size_t count,
 			size_t offset)
@@ -513,7 +869,11 @@ ssize_t nvm_vblk_pwrite(struct nvm_vblk *vblk, const void *buf, size_t count,
 		return vblk_pwrite_s12(vblk, buf, count, offset);
 
 	case NVM_SPEC_VERID_20:
-		return vblk_pwrite_s20(vblk, buf, count, offset);
+		if (vblk->flags & NVM_CMD_ASYNC) {
+			return vblk_async_pwrite_s20(vblk, buf, count, offset);
+		} else {
+			return vblk_sync_pwrite_s20(vblk, buf, count, offset);
+		}
 
 	default:
 		NVM_DEBUG("FAILED: unsupported verid: %d", verid);
@@ -606,70 +966,6 @@ static inline ssize_t vblk_pread_s12(struct nvm_vblk *vblk, void *buf,
 	return count;
 }
 
-static inline ssize_t vblk_pread_s20(struct nvm_vblk *vblk, void *buf,
-				     size_t count, size_t offset)
-{
-	size_t nerr = 0;
-
-	const uint32_t WS_OPT = nvm_dev_get_ws_min(vblk->dev);
-
-	const struct nvm_geo *geo = nvm_dev_get_geo(vblk->dev);
-	const size_t nchunks = vblk->nblks;
-
-	const size_t sectr_nbytes = geo->l.nbytes;
-	const size_t nsectr = count / sectr_nbytes;
-
-	const size_t sectr_bgn = offset / sectr_nbytes;
-	const size_t sectr_end = sectr_bgn + (count / sectr_nbytes) - 1;
-
-	const size_t cmd_nsectr = (NVM_NADDR_MAX / WS_OPT) * WS_OPT;
-
-	const int NTHREADS = NVM_MIN(nchunks, nsectr / WS_OPT);
-
-	if (nsectr % WS_OPT) {
-		NVM_DEBUG("FAILED: unaligned nsectr: %zu", nsectr);
-		errno = EINVAL;
-		return -1;
-	}
-	if (sectr_bgn % WS_OPT) {
-		NVM_DEBUG("FAILED: unaligned sectr_bgn: %zu", sectr_bgn);
-		errno = EINVAL;
-		return -1;
-	}
-
-	#pragma omp parallel for num_threads(NTHREADS) schedule(static,1) reduction(+:nerr) ordered if(NTHREADS>1)
-	for (size_t sectr_ofz = sectr_bgn; sectr_ofz <= sectr_end; sectr_ofz += cmd_nsectr) {
-		struct nvm_addr addrs[cmd_nsectr];
-		char *buf_off = (char*)buf + (sectr_ofz - sectr_bgn) * sectr_nbytes;
-
-		for (size_t idx = 0; idx < cmd_nsectr; ++idx) {
-			const size_t sectr = sectr_ofz + idx;
-			const size_t wunit = sectr / WS_OPT;
-			const size_t rnd = wunit / nchunks;
-
-			const size_t chunk = wunit % nchunks;
-			const size_t chunk_sectr = sectr % WS_OPT + rnd * WS_OPT;
-
-			addrs[idx].val = vblk->blks[chunk].val;
-			addrs[idx].l.sectr = chunk_sectr;
-		}
-
-		const ssize_t err = nvm_cmd_read(vblk->dev, addrs, cmd_nsectr,
-						 buf_off, NULL,
-						 NVM_VBLK_CMD_OPTS, NULL);
-		if (err)
-			++nerr;
-	}
-
-	if (nerr) {
-		NVM_DEBUG("FAILED: nvm_cmd_read, nerr(%zu)", nerr);
-		errno = EIO;
-		return -1;
-	}
-
-	return count;
-}
-
 ssize_t nvm_vblk_pread(struct nvm_vblk *vblk, void *buf, size_t count,
 		       size_t offset)
 {
@@ -680,7 +976,11 @@ ssize_t nvm_vblk_pread(struct nvm_vblk *vblk, void *buf, size_t count,
 		return vblk_pread_s12(vblk, buf, count, offset);
 
 	case NVM_SPEC_VERID_20:
-		return vblk_pread_s20(vblk, buf, count, offset);
+		if (vblk->flags & NVM_CMD_ASYNC) {
+			return vblk_async_pread_s20(vblk, buf, count, offset);
+		} else {
+			return vblk_sync_pread_s20(vblk, buf, count, offset);
+		}
 
 	default:
 		NVM_DEBUG("FAILED: unsupported verid: %d", verid);
@@ -875,5 +1175,6 @@ void nvm_vblk_pr(struct nvm_vblk *vblk)
 	printf("  nmbytes: %zu\n", vblk->nbytes >> 20);
 	printf("  pos_write: %zu\n", vblk->pos_write);
 	printf("  pos_read: %zu\n", vblk->pos_read);
+	printf("  flags: 0x08%x\n", vblk->flags);
         nvm_addr_prn(vblk->blks, vblk->nblks, vblk->dev);
 }
