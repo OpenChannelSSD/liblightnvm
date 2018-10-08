4e7997f3abad8e9d6d0d4b5a4d63537780757e2c
Simon A. F. Lund <slund@cnexlabs.com>
2018-10-08 14:15:07 +0200

    tests: Added test of 'nvm_buf'
    Verifies whether allocations via nvm_buf up to 2GB are possible

---
+++ tests/test_buf.c
@@ -0,0 +1,85 @@
+#include "test_intf.c"
+
+#define KB (0x1ull << 10)
+#define MB (KB * 1024)
+#define GB (MB * 1024)
+
+#define PSEUDO_ALIGN KB * 4
+
+static size_t bsizes[] = {
+	KB, 4 * KB, 256 * KB,
+	MB, 128 * MB, 512 * MB,
+	GB, 2 * GB,
+};
+static size_t nbsizes = sizeof(bsizes) / sizeof(bsizes[0]);
+
+static void test_BUF_VIRT(void) {
+	for (size_t i = 0; i < nbsizes; ++i) {
+		void *buf = NULL;
+
+		buf = nvm_buf_virt_alloc(PSEUDO_ALIGN, bsizes[i]);
+		CU_ASSERT_PTR_NOT_NULL(buf);
+		nvm_buf_virt_free(buf);
+	}
+}
+
+static void test_BUF(void) {
+	for (size_t i = 0; i < nbsizes; ++i) {
+		void *buf = NULL;
+
+		buf = nvm_buf_alloc(dev, bsizes[i], NULL);
+		CU_ASSERT_PTR_NOT_NULL(buf);
+		nvm_buf_free(dev, buf);
+	}
+}
+
+static void test_BUF_SET(void) {
+
+	for (size_t i = 0; i < nbsizes; ++i) {
+		struct nvm_buf_set *bufs = NULL;
+
+		bufs = nvm_buf_set_alloc(dev, bsizes[i], 0);
+		CU_ASSERT_PTR_NOT_NULL(bufs);
+		nvm_buf_set_free(bufs);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int err = 0;
+
+	CU_pSuite pSuite = suite_create("nvm_cmd_erase_vector",
+					argc, argv);
+	if (!pSuite)
+		goto out;
+
+	if (!CU_add_test(pSuite, "BUF_VIRT", test_BUF_VIRT))
+		goto out;
+
+	if (!CU_add_test(pSuite, "BUF", test_BUF))
+		goto out;
+
+	if (!CU_add_test(pSuite, "BUF_SET", test_BUF_SET))
+		goto out;
+
+	switch(rmode) {
+	case NVM_TEST_RMODE_AUTO:
+		CU_automated_run_tests();
+		break;
+
+	default:
+		CU_basic_set_mode(rmode);
+		CU_basic_run_tests();
+		break;
+	}
+
+out:
+	err = CU_get_error() || \
+	      CU_get_number_of_suites_failed() || \
+	      CU_get_number_of_tests_failed() || \
+	      CU_get_number_of_failures();
+
+	CU_cleanup_registry();
+
+	return err;
+}
