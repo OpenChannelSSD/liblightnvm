2d392ff7604e3fe317688698cd638a30468274b9
Simon A. F. Lund <slund@cnexlabs.com>
2019-02-15 11:15:54 +0100

    tests: nvm_dev expanded open/close testing

--- tests/test_dev.c
+++ tests/test_dev.c
@@ -1,5 +1,6 @@
 #include "test_intf.c"
 
+// Verify that the device can be opened
 void test_DEV_OPEN_CLOSE(void)
 {
 	struct nvm_dev *dev;
@@ -10,7 +11,24 @@ void test_DEV_OPEN_CLOSE(void)
 	nvm_dev_close(dev);
 }
 
+// Verify that the device can be opened more than once
 void test_DEV_OPEN_CLOSE_N(void)
+{
+	struct nvm_dev *dev[] = {0,0,0,0,0,0,0,0,0,0};
+	int ndev = sizeof(dev) / sizeof(dev[0]);
+
+	for (int i = 0; i < ndev; ++i) {
+		dev[i] = nvm_dev_open(NVM_DEV_PATH);
+		CU_ASSERT_PTR_NOT_NULL(dev[i]);
+		if (!dev[i])
+			break;
+
+		nvm_dev_close(dev[i]);
+	}
+}
+
+// Verify that the device can be opened more than once at the same time
+void test_DEV_OPEN_CLOSE_MULTI_N(void)
 {
 	struct nvm_dev *dev[] = {0,0,0,0,0,0,0,0,0,0};
 	int ndev = sizeof(dev) / sizeof(dev[0]);
@@ -33,14 +51,15 @@ int main(int argc, char **argv)
 {
 	int err = 0;
 
-	CU_pSuite pSuite = suite_create("nvm_dev_*",
-					argc, argv);
+	CU_pSuite pSuite = suite_create_nosetup("nvm_dev_*", argc, argv);
 	if (!pSuite)
 		goto out;
 
 	if (!CU_add_test(pSuite, "nvm_dev_[open|close]", test_DEV_OPEN_CLOSE))
 		goto out;
 	if (!CU_add_test(pSuite, "nvm_dev_[open|close] n", test_DEV_OPEN_CLOSE_N))
+		goto out;
+	if (!CU_add_test(pSuite, "nvm_dev_[open|close] multi-n", test_DEV_OPEN_CLOSE_MULTI_N))
 		goto out;
 
 	switch(RMODE) {
